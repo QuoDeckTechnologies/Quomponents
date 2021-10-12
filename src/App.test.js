5eb8acb1f42ecebecd31afe2afe7a1b7f9886ffd
Arijit Lahiri <arijit@ptotem.com>
2021-10-12 19:03:22 +0530

    First commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,8 @@
 import { render, screen } from '@testing-library/react';
+import initStoryshots from '@storybook/addon-storyshots';
 import App from './App';
+
+initStoryshots();
 
 test('renders learn react link', () => {
   render(<App />);
