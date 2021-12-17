b3a82e5bf2529e9a0a5078bf51ee190dfab2586b
unknown <udhav.varma@quodeck.com>
2021-12-17 19:36:30 +0530

    udhav commit

---
+++ src/components/Buttons/IconLink/IconLink.react.jsx
@@ -0,0 +1,220 @@
+// Import npm packages
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { motion } from "framer-motion";
+import {
+    getQuommons,
+    getTranslation,
+    getAnimation,
+} from "../../../common/javascripts/helpers";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "../../../common/stylesheets/common.css";
+import './IconLink.scss'
+import "../../../common/stylesheets/overrule.scss";
+
+Button.propTypes = {
+    //=======================================
+    // Component Specific props
+    //=======================================
+
+    /**
+    Button Text has to be in content or passed as children to the component. Is optional if you only want an icon.
+    */
+    content: PropTypes.string,
+    /**
+    Set action emphasis in increasing order 
+    */
+    asEmphasis: PropTypes.oneOf(["text", "outlined", "contained"]),
+    /**
+    Use for rounded corners or circular icon button 
+    */
+    isCircular: PropTypes.bool,
+
+    // Quommon props
+    //=======================================
+
+    /**
+    Use to define standard component type
+    */
+    asVariant: PropTypes.oneOf([
+        "primary",
+        "secondary",
+        "success",
+        "warning",
+        "error",
+    ]),
+    /**
+    Use to define component text size in increasing order
+    */
+    asSize: PropTypes.oneOf([
+        "tiny",
+        "small",
+        "normal",
+        "big",
+        "huge",
+        "massive",
+    ]),
+    /**
+    Use to define component padding in increasing order
+    */
+    asPadded: PropTypes.oneOf(["fitted", "compact", "normal", "relaxed"]),
+    /**
+    Use to float the component in parent container
+    */
+    asFloated: PropTypes.oneOf(["left", "right", "none", "inline"]),
+    /**
+    Use to align content within the component container
+    */
+    asAligned: PropTypes.oneOf(["left", "right", "center"]),
+
+    /**
+    Use to override component colors and behavior
+    */
+    withColor: PropTypes.shape({
+        backgroundColor: PropTypes.string,
+        accentColor: PropTypes.string,
+        textColor: PropTypes.string,
+        hoverBackgroundColor: PropTypes.string,
+        hoverTextColor: PropTypes.string,
+    }),
+    /**
+    Use to add an icon to the component
+    */
+    withIcon: PropTypes.shape({
+        icon: PropTypes.string,
+        size: PropTypes.string,
+        position: PropTypes.oneOf(["left", "right"]),
+    }),
+    /**
+    Use to add a heading label, a footer caption or a title popover to the component
+    */
+    withLabel: PropTypes.shape({
+        format: PropTypes.oneOf(["label", "caption", "popover"]),
+        content: PropTypes.string,
+        textColor: PropTypes.string,
+    }),
+    /**
+    Use to define the entry animation of the component
+    */
+    withAnimation: PropTypes.shape({
+        animation: PropTypes.oneOf([
+            "zoom",
+            "collapse",
+            "fade",
+            "slideDown",
+            "slideUp",
+            "slideLeft",
+            "slideRight",
+            ""
+        ]),
+        duration: PropTypes.number,
+        delay: PropTypes.number,
+    }),
+    /**
+    Use to show a translated version of the component text. Dictionary must be valid JSON. 
+    */
+    withTranslation: PropTypes.shape({
+        lang: PropTypes.string,
+        tgt: PropTypes.string,
+        dictionary: PropTypes.string,
+    }),
+
+    /**
+    Use to show/hide the component
+    */
+    isHidden: PropTypes.bool,
+    /**
+    Use to enable/disable the component
+    */
+    isDisabled: PropTypes.bool,
+    /**
+    Use to toggle the component taking the full width of the parent container
+    */
+    isFluid: PropTypes.bool,
+    /**
+    Use to toggle a loading state for the component
+    */
+    isLoading: PropTypes.bool,
+
+    /**
+    Button component must have the onClick function passed as props
+    */
+    onClick: PropTypes.func.isRequired,
+};
+
+Button.defaultProps = {
+    // Component Specific props
+    //=======================================
+    asEmphasis: "contained",
+    isCircular: false,
+
+    // Quommon props
+    //=======================================
+    asVariant: "primary",
+    asSize: "normal",
+    asPadded: "normal",
+    asFloated: "none",
+    asAligned: "center",
+
+    withColor: null,
+    withIcon: null,
+    withLabel: null,
+    withAnimation: null,
+    withTranslation: null,
+
+    isHidden: false,
+    isDisabled: false,
+    isFluid: false,
+    isLoading: false,
+};
+
+
+export default function Button(props) {
+    const [ tilt,setTilt ] = useState(false)
+
+    
+    const handleTilt = (e) => {
+        setTilt(true)
+        setTimeout(()=>{
+            setTilt(false)
+        },300)
+        props.onClick(e)
+    }
+
+    //-------------------------------------------------------------------
+    // 1. Set the classes
+    //-------------------------------------------------------------------
+
+    let quommonClasses = getQuommons(props);
+    if (props.isCircular){
+        quommonClasses.childClasses += ` is-circular ${
+        props.content === "" && props.withIcon ? "is-only-icon" : ""}`;
+    }
+    quommonClasses.childClasses += ` emp-${props.asEmphasis}`;
+
+    //-------------------------------------------------------------------
+    // 2. Get animation of the component
+    //-------------------------------------------------------------------
+    
+    const animate = getAnimation(props.withAnimation);
+    
+    return (
+        <motion.div
+            initial={animate.from}
+            animate={animate.to}
+            className='qui'
+        >
+            <div onClick={(e)=>handleTilt(e)} className={`qui qui-btn ${quommonClasses.childClasses} ${quommonClasses.parentClasses}`} style={props.withColor}>
+                <div  className={`${tilt ? 'tilt' : 'notilt'} ${quommonClasses.childClasses}`}>
+                    { !props.isLoading && <i className={props.withIcon.icon} style={{color:props.withColor.textColor}}></i> }
+                    { props.isLoading && <div className="qui loading">
+                    <i className="fas fa-spinner"></i>PLEASE WAIT...
+                    </div> }
+                </div>
+                { !props.isLoading && <div className={quommonClasses.childClasses}>
+                    <p className="qui-text" style={{color:props.withColor.textColor}}>{props.content}</p>
+                </div> }
+            </div>
+        </motion.div>
+    )
+}
---
+++ src/stories/IconLink.stories.js
@@ -0,0 +1,234 @@
+import React from "react";
+import IconLink from '../components/Buttons/IconLink/IconLink.react'
+
+const dictionary = JSON.stringify({
+    // en: {
+    //     loading: "Please wait...",
+    //     button: {
+    //         text: "Button",
+    //         label: "Do not press this repeatedly...",
+    //     },
+    // },
+    hi: {
+        loading: "बस एक मिनट...",
+        button: { text: "बटन", label: "इसे बार-बार न दबाएं..." },
+    },
+});
+
+
+export default {
+    title: "Design System/Buttons/IconLink",
+    component: IconLink,
+    argTypes: {
+        content: "Books",
+        asEmphasis: {
+            control: "select",
+            options: ["text", "outlined", "contained"],
+            table: {
+                category: "as-Flags",
+            },
+        },
+        isCircular: {
+            table: {
+                category: "is-Toggles",
+                defaultValue: false,
+            },
+        },
+
+        asVariant: {
+            control: "select",
+            // options: ["primary", "secondary", "success", "warning", "error"],
+            options: ["primary", "secondary", "warning"],
+            table: {
+                category: "as-Flags",
+            },
+        },
+        asSize: {
+            control: "select",
+            options: ["tiny", "small", "normal", "big", "huge", "massive"],
+            table: {
+                category: "as-Flags",
+            },
+        },
+        asPadded: {
+            control: "select",
+            options: ["fitted", "compact", "normal", "relaxed"],
+            table: {
+                category: "as-Flags",
+            },
+        },
+        asFloated: {
+            control: "select",
+            options: ["left", "right", "none", "inline"],
+            table: {
+                category: "as-Flags",
+            },
+        },
+        asAligned: {
+            control: "select",
+            options: ["left", "right", "center"],
+            table: {
+                category: "as-Flags",
+            },
+        },
+
+        withColor: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    backgroundColor: "",
+                    accentColor: "",
+                    textColor: "",
+                    hoverBackgroundColor: "",
+                    hoverTextColor: "",
+                },
+            },
+        },
+        withIcon: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    icon: "",
+                    size: "",
+                    position: "left",
+                },
+            },
+        },
+        withLabel: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    format: "label",
+                    content: "",
+                    textColor: "",
+                },
+            },
+        },
+        withAnimation: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    animation: "",
+                    duration: 0,
+                    delay: 0,
+                },
+            },
+        },
+        withTranslation: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    lang: "",
+                    tgt: "",
+                    dictionary: "",
+                },
+            },
+        },
+
+        isHidden: {
+            table: {
+                category: "is-Toggles",
+                defaultValue: false,
+            },
+        },
+        isDisabled: {
+            table: {
+                category: "is-Toggles",
+                defaultValue: false,
+            },
+        },
+        isFluid: {
+            table: {
+                category: "is-Toggles",
+                defaultValue: false,
+            },
+        },
+        isLoading: {
+            table: {
+                category: "is-Toggles",
+                defaultValue: false,
+            },
+        },
+
+        onClick: {
+            action: 'clicked',
+            table: {
+                category: "Events",
+                defaultValue: null,
+            },
+        },
+    },
+    decorators: [
+        (story) => (
+            <div
+                style={{
+                    width: "100%",
+                    textAlign: "center",
+                    fontSize: "1.25em",
+                }}
+            >
+                {story()}
+            </div>
+        ),
+    ],
+    parameters: {
+        componentSubtitle: "Displays a button",
+        a11y: { disable: true },
+        // controls: { expanded: true }
+    },
+};
+
+// -------------------------------------------------------------
+// Default
+// -------------------------------------------------------------
+const Template = (args) => <IconLink {...args} />;
+export const Default = Template.bind({});
+Default.args = {
+    content: "Home",
+    asEmphasis: "text",
+    isCircular: false,
+
+    asVariant: "primary",
+    asSize: "normal",
+    asFloated: "inline",
+    asPadded: "normal",
+    asAligned: "center",
+
+ 
+    withIcon: { icon: "fas fa-home", size: "1em", position: "left" },
+    withColor: {
+        backgroundColor: "",
+        accentColor: "",
+        textColor: "",
+        hoverBackgroundColor: "",
+        hoverTextColor: "",
+    },
+    withAnimation: {
+        animation: "zoom",
+        duration: 0.5,
+        delay: 0,
+    },
+    withTranslation: {
+        lang: "en",
+        tgt: "button",
+        dictionary: dictionary,
+    },
+    withLabel: {
+        format: "caption",
+        content: "Home",
+        textColor: "#000000",
+    },
+
+    isDisabled: false,
+    isLoading: false,
+    isHidden: false,
+    isFluid: false,
+
+};
+Default.parameters = {
+    docs: {
+        source: {
+            code: `<Button {...${JSON.stringify(Default.args, null, 2)}}/>`,
+        },
+    },
+};
