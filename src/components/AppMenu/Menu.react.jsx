18d950b434be38c0a78358ba16fd318e444daba0
mohdnawab1 <mohd.nawab@quodeck>
2021-12-23 14:00:27 +0530

    first commit

--- src/components/AppMenu/Menu.react.jsx
+++ src/components/AppMenu/Menu.react.jsx
@@ -1,7 +1,7 @@
 // Import npm packages
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { motion } from "framer-motion";
+
 import {
     getQuommons,
     getTranslation,
