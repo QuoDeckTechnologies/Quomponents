168259879f5200980655964c49913865510b3c7b
mohdnawab1 <mohd.nawab@quodeck>
2021-12-24 11:48:18 +0530

    Created MenuBlock

---
+++ src/stories/MenuBlock.stories.js
@@ -0,0 +1,211 @@
+import React from "react";
+import MenuBlock from "../components/AppMenu/MenuBlock.react";
+
+// const dictionary = JSON.stringify({
+//     en: {
+//         loading: "Please wait...",
+//         button: {
+//             text: "Button",
+//             label: "Do not press this repeatedly...",
+//         },
+//     },
+//     hi: {
+//         loading: "बस एक मिनट...",
+//         button: { text: "होम", label: "होम" },
+//     },
+// });
+
+export default {
+    title: "Design System/AppMenu/MenuBlock",
+    component: MenuBlock,
+    argTypes: {
+        content: "Icon",
+        asEmphasis: {
+            control: "select",
+            options: ["text", "outlined", "contained"],
+            table: {
+                category: "as-Flags",
+            },
+        },
+        isCircular: {
+            table: {
+                category: "is-Toggles",
+                defaultValue: false,
+            },
+        },
+
+        asVariant: {
+            control: "select",
+            options: ["primary", "secondary", "warning"],
+            table: {
+                category: "as-Flags",
+            },
+        },
+        asSize: {
+            control: "select",
+            options: ["tiny", "small", "normal", "big", "huge", "massive"],
+            table: {
+                category: "as-Flags",
+            },
+        },
+        asPadded: {
+            control: "select",
+            options: ["fitted", "compact", "normal", "relaxed"],
+            table: {
+                category: "as-Flags",
+            },
+        },
+        asFloated: {
+            control: "select",
+            options: ["left", "right", "inline"],
+            table: {
+                category: "as-Flags",
+            },
+        },
+        asAligned: {
+            control: "select",
+            options: ["left", "right", "center"],
+            table: {
+                category: "as-Flags",
+            },
+        },
+
+        withColor: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    backgroundColor: "#ED6E6E",
+                    accentColor: "",
+                    textColor: "",
+                    hoverBackgroundColor: "",
+                    hoverTextColor: "",
+                },
+            },
+        },
+        withIcon: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    icon: "",
+                    size: "",
+                    position: "left",
+                },
+            },
+        },
+        withLabel: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    format: "label",
+                    content: "",
+                    textColor: "",
+                },
+            },
+        },
+        withAnimation: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    animation: "",
+                    duration: 0,
+                    delay: 0,
+                },
+            },
+        },
+        withTranslation: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    lang: "",
+                    tgt: "",
+                    dictionary: "",
+                },
+            },
+        },
+
+        isHidden: {
+            table: {
+                category: "is-Toggles",
+                defaultValue: false,
+            },
+        },
+        isDisabled: {
+            table: {
+                category: "is-Toggles",
+                defaultValue: false,
+            },
+        },
+        isFluid: {
+            table: {
+                category: "is-Toggles",
+                defaultValue: false,
+            },
+        },
+
+        onClick: {
+            table: {
+                category: "Events",
+                defaultValue: null,
+            },
+        },
+    },
+    decorators: [
+        (story) => (
+            <div
+                style={{
+                    width: "100%",
+                    textAlign: "center",
+                    fontSize: "1.25em",
+                }}
+            >
+                {story()}
+            </div>
+        ),
+    ],
+    parameters: {
+        componentSubtitle: "Displays a icon with a link",
+        a11y: { disable: true },
+    }, 
+};
+//----------------------------------------------------------
+// MenuBlock  Icon
+//---------------------------------------------------------
+const Template = (args) => <MenuBlock {...args} />;
+export const MenuBlockIcon = Template.bind({});
+MenuBlockIcon.args = {
+    content: "MenuBlockIcon",
+    asEmphasis: "contained",
+    isCircular: true,
+
+    asVariant: "primary",
+    asSize: "normal",
+    asFloated: "inline",
+    asPadded: "normal",
+    asAligned: "center",
+
+    withIcon: { icon: "fas fa-ellipsis-v", size: "1em", position: "left" },
+    withColor: {
+        backgroundColor: "#ED6E6E",
+        accentColor: "",
+        textColor: "",
+        hoverBackgroundColor: "",
+        hoverTextColor: "",
+    },
+
+    withAnimation: {
+        animation: "zoom",
+        duration: 0.5,
+        delay: 0,
+    },
+
+    isDisabled: false,
+    isHidden: false,
+    isFluid: false,
+};
+MenuBlockIcon.parameters = {
+    docs: {
+        source: {
+            code: `<MenuBlock {...${JSON.stringify(MenuBlockIcon.args, null, 2)}}/>`,
+        },
+    },
+};
