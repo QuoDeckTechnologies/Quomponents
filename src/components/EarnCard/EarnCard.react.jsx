7ee66a63cc4a66c086d4c545b9306619dab6b5ee
Udhav <udhav.varma@quodeck.com>
2022-01-07 15:13:03 +0530

    commit

---
+++ src/components/EarnCard/EarnCard.react.jsx
@@ -0,0 +1,177 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { motion } from "framer-motion";
+import _ from 'lodash'
+import BannerCard from "../Carousel/BannerCard/BannerCard.react";
+
+import {
+    getQuommons,
+    getTranslation,
+    getAnimation,
+} from "../../common/javascripts/helpers";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "../../common/stylesheets/common.css";
+import "../Carousel/BannerCard/BannerCard.scss";
+import './EarnCard.scss'
+import "../../common/stylesheets/overrule.scss";
+
+EarnCard.propTypes = {
+    //=======================================
+    // Component Specific props
+    //=======================================
+    /**
+    Banner Card data should be passed in content field and it is required field
+    */
+    content: PropTypes.shape({
+        image: PropTypes.string,
+        tag: PropTypes.oneOf([
+            "new",
+            "premium",
+            "restricted",
+            "free"
+        ]),
+        header: PropTypes.string,
+        content: PropTypes.string
+    }).isRequired,
+
+    //=======================================
+    // Quommon props
+    //=======================================
+    /**
+    Use to define standard component type
+    */
+    asVariant: PropTypes.oneOf([
+        "primary",
+        "secondary",
+        "success",
+        "warning",
+        "error",
+    ]),
+
+    /**
+    Use to override component colors and behavior
+    */
+    withColor: PropTypes.shape({
+        backgroundColor: PropTypes.string,
+        accentColor: PropTypes.string,
+        textColor: PropTypes.string,
+    }),
+
+    /**
+    Use to define the entry animation of the component
+    */
+    withAnimation: PropTypes.shape({
+        animation: PropTypes.oneOf([
+            "zoom",
+            "collapse",
+            "fade",
+            "slideDown",
+            "slideUp",
+            "slideLeft",
+            "slideRight",
+            ""
+        ]),
+        duration: PropTypes.number,
+        delay: PropTypes.number,
+    }),
+
+    /**
+    Use to show a translated version of the component text. Dictionary must be valid JSON. 
+    */
+    withTranslation: PropTypes.shape({
+        lang: PropTypes.string,
+        tgt: PropTypes.string,
+        dictionary: PropTypes.string,
+    }),
+
+    /**
+    Use to enable/disable the component
+    */
+    isDisabled: PropTypes.bool,
+    /**
+    Use to show/hide the component
+    */
+    isHidden: PropTypes.bool,
+
+    /**
+    Button component must have the onClick function passed as props
+    */
+    onClick: PropTypes.func.isRequired,
+};
+
+EarnCard.defaultProps = {
+    // Component Specific props
+    //=======================================
+    content: {},
+    // Quommon props
+    //=======================================
+    asVariant: "primary",
+
+    withColor: null,
+    withAnimation: null,
+    withTranslation: null,
+
+    isDisabled: false,
+    isHidden: false
+};
+
+/**
+## Notes
+- The design system used for this component is Material UI (@mui/material)
+- The animation system used for this component is Framer Motion (framer-motion)
+- Pass inline styles to the component to override any of the component css
+- Or add custom css in overrule.scss to override the component css
+- MUI props are not being passed to the button. Please speak to the admin to handle any new MUI prop.
+**/
+
+
+
+export default function EarnCard(props) {
+    let { courseContent } = props
+    //-------------------------------------------------------------------
+    // 1. Set the classes
+    //-------------------------------------------------------------------
+    let quommonClasses = getQuommons(props, "button");
+    if (props.isCircular)
+        quommonClasses.childClasses += ` is-circular ${props.content === "" && props.withIcon ? "is-only-icon" : ""
+            }`;
+
+    quommonClasses.childClasses += ` emp-${props.asEmphasis}`;
+
+    return (
+        <div className="qui qui-EarnCard">
+        <div className="qui-leftSide" >
+            <BannerCard {...props} />
+            <div className={`qui-leftLower size-${props.asSize} variant-${props.asVariant}-text`}>
+                <div className="qui-leftLowerTop">
+                    <div className="qui-checkbox">
+                        {_.map(courseContent.topics ,(topics,index) => {
+                            return (
+                                <h1 className={`${topics.checked ? 'fas fa-check-square' : 'far fa-square'}`} key={topics.name}></h1>
+                            )
+                        })}
+                    </div>
+                    <div className="qui-courseDate">
+                        <h2>{props.courseContent.dates.start_date}</h2>
+                        <h2>&nbsp;{`-`}&nbsp;</h2>
+                        <h2>{props.courseContent.dates.end_date}</h2>
+                    </div>
+                </div>
+                <div className="qui-EarnCardIcon">
+                    <i className={props.courseContent.icon}></i>
+                </div>
+            </div>
+        </div>
+        <div className="qui-rightSide">
+                <div className={`qui-courseHeader size-${props.asSize}`}>
+                    <h1>{props.courseContent.title}</h1>
+                    <div className="courseBanner"></div>
+                </div>
+            <div className="qui-courseDescription">
+                    <p>{props.courseContent.description}</p>
+            </div>
+        </div>
+        </div>
+    );
+}
---
+++ src/stories/EarnCard.stories.js
@@ -0,0 +1,195 @@
+import React from "react";
+import EarnCard from "../components/EarnCard/EarnCard.react";
+
+const dictionary = JSON.stringify({
+    hi: {
+        loading: "बस एक मिनट...",
+        bannercard: { header: "बातचीत कक्ष", content: "प्रतियोगिता खेलें और फ्लिपकार्ट वाउचर अर्जित करने के लिए जीतें।" },
+        ribbon: {
+            new: "नया",
+            restricted: "प्रतिबंधित",
+            premium: "अधिमूल्य",
+            free: "नि: शुल्क"
+        }
+    },
+});
+
+export default {
+    title: "Design System/EarnCard/EarnCard",
+    component: EarnCard,
+    argTypes: {
+        content: {
+            image: "",
+            tag: "",
+        },
+        asVariant: {
+            control: "select",
+            options: ["primary", "secondary", "success", "warning", "error"],
+            table: {
+                category: "as-Flags",
+            },
+        },
+        asSize: {
+            control: "select",
+            options: ["tiny", "small", "normal", "big", "huge", "massive"],
+            table: {
+                category: "as-Flags",
+            },
+        },
+        withColor: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    backgroundColor: "",
+                    accentColor: "",
+                    textColor: "",
+                    hoverBackgroundColor: "",
+                    hoverTextColor: "",
+                },
+            },
+        },
+        withAnimation: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    animation: "",
+                    duration: 0,
+                    delay: 0,
+                },
+            },
+        },
+        withTranslation: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    lang: "",
+                    tgt: "",
+                    dictionary: "",
+                },
+            },
+        },
+        isDisabled: {
+            table: {
+                category: "is-Toggles",
+                defaultValue: false,
+            },
+        },
+        isHidden: {
+            table: {
+                category: "is-Toggles",
+                defaultValue: false,
+            },
+        },
+        onClick: {
+            table: {
+                category: "Events",
+                defaultValue: null,
+            },
+        },
+    },
+    decorators: [
+        (story) => (
+            <div
+                style={{
+                    width: "100%",
+                    textAlign: "center",
+                }}
+            >
+                {story()}
+            </div>
+        ),
+    ],
+    parameters: {
+        componentSubtitle: "Displays a banner card with box for text and tag",
+        a11y: { disable: true },
+        docs: {
+            iframeHeight: 600,
+        },
+    },
+};
+
+// -------------------------------------------------------------
+// Default
+// -------------------------------------------------------------
+const Template = (args) => {
+    return (
+        <EarnCard {...args} />
+    );
+}
+
+export const Default = Template.bind({});
+Default.args = {
+    content: {
+        image: "https://us.123rf.com/450wm/microone/microone1909/microone190900839/130722932-chaos-in-workplace-sleepy-lazy-unorganized-employees-in-office-bad-organization-control-business-cor.jpg",
+        tag: "restricted",
+    },
+    courseContent : {
+        title : 'QuoDeck Emerging Leadership Program',
+        description : 'Win a chance to apply for this exclusive opportunity for taking your career to the stars',
+        icon : 'fas fa-book-reader',
+        dates: {
+            end_date: "3rd May",
+            start_date: "28th Feb"
+          },
+        topics : [
+            {
+                name : 'Name One',
+                contentList : [],
+                checked : true
+            },
+            {
+                name : 'Name Two',
+                contentList : [],
+                checked : false
+            },
+            {
+                name : 'Name Three',
+                contentList : [],
+                checked : false
+            },
+            {
+                name : 'Name Four',
+                contentList : [],
+                checked : false
+            },
+            {
+                name : 'Name Five',
+                contentList : [],
+                checked : true
+            },
+            {
+                name : 'Name Six',
+                contentList : [],
+                checked : false
+            },
+        ]
+    },
+    asVariant: "primary",
+    withColor: {
+        backgroundColor: "",
+        accentColor: "",
+        textColor: "",
+        hoverBackgroundColor: "",
+        hoverTextColor: "",
+    },
+    withAnimation: {
+        animation: "zoom",
+        duration: 0.5,
+        delay: 0,
+    },
+    withTranslation: {
+        lang: "en",
+        tgt: "button",
+        dictionary: dictionary,
+    },
+
+    isDisabled: false,
+    isHidden: false,
+};
+Default.parameters = {
+    docs: {
+        source: {
+            code: `<BannerCard {...${JSON.stringify(Default.args, null, 2)}}/>`,
+        },
+    },
+};
