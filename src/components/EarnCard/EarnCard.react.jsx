979facc1b44ae6da0c8f556322ccf4a0136f4a26
Udhav <udhav.varma@quodeck.com>
2022-01-12 10:52:38 +0530

    Banner height change remaining

--- src/components/EarnCard/EarnCard.react.jsx
+++ src/components/EarnCard/EarnCard.react.jsx
@@ -1,137 +1,139 @@
+// Import npm packages
 import React from "react";
 import PropTypes from "prop-types";
 import { motion } from "framer-motion";
-import _ from 'lodash'
-import {
-    getAnimation,
-} from "../../common/javascripts/helpers";
-
+import _ from "lodash";
+import { getAnimation } from "../../common/javascripts/helpers";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../../common/stylesheets/common.css";
-import './EarnCard.scss'
+import "./EarnCard.scss";
 import "../../common/stylesheets/overrule.scss";
 import BannerCard from "../Carousel/BannerCard/BannerCard.react";
 
 EarnCard.propTypes = {
-    //=======================================
-    // Component Specific props
-    //=======================================
-    /**
-    EarnCard data should be passed in content field and it is required field
+  //=======================================
+  // Component Specific props
+  //=======================================
+  /**
+    EarnCard data should be passed in content field and it is a required field
     */
-    content: PropTypes.shape({
-        image: PropTypes.string,
-        tag: PropTypes.oneOf([
-            "new",
-            "premium",
-            "restricted",
-            "free"
-        ]),
-        header: PropTypes.string,
-        content: PropTypes.string,
-        title: PropTypes.string,
-        description: PropTypes.string,
-        icon: PropTypes.string,
-        dates: PropTypes.shape({
-            end_date: PropTypes.string,
-            start_date: PropTypes.string,
-        }),
-        topics: PropTypes.arrayOf(
-            PropTypes.shape({
-                name: PropTypes.string,
-                contentList: PropTypes.arrayOf(
-                    PropTypes.shape({})
-                ),
-                checked: PropTypes.bool,
-            }),
-        )
-    }).isRequired,
-    //=======================================
-    // Quommon props
-    //=======================================
-    /**
+  content: PropTypes.shape({
+    image: PropTypes.string,
+    tag: PropTypes.oneOf(["new", "premium", "restricted", "free"]),
+    header: PropTypes.string,
+    content: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    icon: PropTypes.string,
+    dates: PropTypes.shape({
+      end_date: PropTypes.string,
+      start_date: PropTypes.string,
+    }),
+    topics: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string,
+        contentList: PropTypes.arrayOf(PropTypes.shape({})),
+        checked: PropTypes.bool,
+      })
+    ),
+  }).isRequired,
+  //=======================================
+  // Quommon props
+  //=======================================
+  /**
     Use to define standard component type
     */
-    asVariant: PropTypes.oneOf([
-        "primary",
-        "secondary",
-        "success",
-        "warning",
-        "error",
-    ]),
-
-    /**
+  asVariant: PropTypes.oneOf([
+    "primary",
+    "secondary",
+    "success",
+    "warning",
+    "error",
+  ]),
+  /**
+    Use to define component size in increasing order
+    */
+  asSize: PropTypes.oneOf([
+    "tiny",
+    "small",
+    "normal",
+    "big",
+    "huge",
+    "massive",
+  ]),
+  /**
     Use to override component colors and behavior
     */
-    withColor: PropTypes.shape({
-        backgroundColor: PropTypes.string,
-        accentColor: PropTypes.string,
-        textColor: PropTypes.string,
-    }),
+  withColor: PropTypes.shape({
+    backgroundColor: PropTypes.string,
+    accentColor: PropTypes.string,
+    textColor: PropTypes.string,
+  }),
 
-    /**
+  /**
     Use to define the entry animation of the component
     */
-    withAnimation: PropTypes.shape({
-        animation: PropTypes.oneOf([
-            "zoom",
-            "collapse",
-            "fade",
-            "slideDown",
-            "slideUp",
-            "slideLeft",
-            "slideRight",
-            ""
-        ]),
-        duration: PropTypes.number,
-        delay: PropTypes.number,
-    }),
-    /**
+  withAnimation: PropTypes.shape({
+    animation: PropTypes.oneOf([
+      "zoom",
+      "collapse",
+      "fade",
+      "slideDown",
+      "slideUp",
+      "slideLeft",
+      "slideRight",
+      "",
+    ]),
+    duration: PropTypes.number,
+    delay: PropTypes.number,
+  }),
+  /**
     Use to enable/disable the component
     */
-    isDisabled: PropTypes.bool,
-    /**
+  isDisabled: PropTypes.bool,
+  /**
     Use to show/hide the component
     */
-    isHidden: PropTypes.bool,
-    /**
+  isHidden: PropTypes.bool,
+  /**
     Button component must have the onClick function passed as props
     */
-    onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 EarnCard.defaultProps = {
-    // Component Specific props
-    //=======================================
-    content: {},
+  //=======================================
+  // Component Specific props
+  //=======================================
+  content: {},
+  //=======================================
+  // Quommon props
+  //=======================================
+  asVariant: "primary",
+  asSize: "normal",
+  withColor: null,
+  withAnimation: null,
 
-    // Quommon props
-    //=======================================
-    asVariant: "primary",
-
-    withColor: null,
-    withAnimation: null,
-
-    isDisabled: false,
-    isHidden: false
+  isDisabled: false,
+  isHidden: false,
 };
 
 function getColors(colors) {
-    let colorStyle = {
-        textColors: {
-            color: colors.textColor
-        },
-        accentColors: {
-            color: colors.accentColor
-        },
-        bannerColors: {
-            backgroundColor: colors.textColor
-        },
-        cardColors: {
-            backgroundColor: colors.backgroundColor
-        }
-    }
-    return colorStyle
+  let colorStyle = {
+    textColors: {
+      color: colors.textColor,
+    },
+    accentColors: {
+      color: colors.accentColor,
+    },
+    bannerColors: {
+      backgroundColor: colors.textColor,
+    },
+    cardColors: {
+      backgroundColor: colors.backgroundColor,
+    },
+  };
+  return colorStyle;
 }
 /**
 ## Notes
@@ -142,56 +144,76 @@ function getColors(colors) {
 - Status of topics can be changed from content prop
 **/
 export default function EarnCard(props) {
-    //-------------------------------------------------------------------
-    // 1. Destructuring the content, size and variant from props
-    //-------------------------------------------------------------------
-    let { content,asSize,asVariant,isDisabled,isHidden } = props
-    //-------------------------------------------------------------------
-    // 2. Set the component colors
-    //-------------------------------------------------------------------
-    let colors = props.withColor ? getColors(props.withColor) : {};
-    //-------------------------------------------------------------------
-    // 3. Get animation of the component
-    //-------------------------------------------------------------------
-    const animate = getAnimation(props.withAnimation);
+  //-------------------------------------------------------------------
+  // 1. Destructuring the content, size and variant from props
+  //-------------------------------------------------------------------
+  let { content, asSize, asVariant, isDisabled, isHidden } = props;
+  //-------------------------------------------------------------------
+  // 2. Set the component colors
+  //-------------------------------------------------------------------
+  let colors = props.withColor ? getColors(props.withColor) : {};
+  //-------------------------------------------------------------------
+  // 3. Get animation of the component
+  //-------------------------------------------------------------------
+  const animate = getAnimation(props.withAnimation);
 
-    // ========================= Render Function =================================
-    return (
-        <motion.div
-            initial={animate.from}
-            animate={animate.to}
-            className={`qui qui-EarnCard ${isDisabled ? 'is-Disabled' : ''} ${isHidden ? 'is-hidden' : ''}`} style={colors.cardColors}>
-            <div className="qui-leftSide" >
-            <BannerCard {...props} />
-                <div className={`qui-leftLower size-${asSize} variant-${asVariant}-text`}>
-                    <div className="qui-EarnCardIcon" style={colors.accentColors}>
-                        <i className={content?.icon}></i>
-                    </div>
-                    <div className="qui-leftLowerTop">
-                        <div className="qui-checkbox">
-                            {_.map(content?.topics, (topics, index) => {
-                                return (
-                                    <i className={`${topics.checked ? 'fas fa-check-square' : 'far fa-square'}`} key={topics.name + index} style={colors.accentColors}></i>
-                                )
-                            })}
-                        </div>
-                        <div className="qui-courseDate" style={colors.textColors}>
-                            <h2>{content?.dates.start_date}</h2>
-                            <h2>&nbsp;-&nbsp;</h2>
-                            <h2>{content?.dates.end_date}</h2>
-                        </div>
-                    </div>
-                </div>
+  // ========================= Render Function =================================
+  return (
+    <motion.div
+      initial={animate.from}
+      animate={animate.to}
+      className={`qui qui-EarnCard ${isDisabled ? "is-Disabled" : ""} ${
+        isHidden ? "is-hidden" : ""
+      }`}
+      style={colors.cardColors}
+    >
+      <div className="qui-leftSide">
+        <div className="qui-BannerDiv">
+          <BannerCard {...props} />
+        </div>
+        <div
+          className={`qui-leftLower size-${asSize} variant-${asVariant}-text`}
+        >
+          <div className="qui-EarnCardIcon" style={colors.accentColors}>
+            <i className={content?.icon}></i>
+          </div>
+          <div className="qui-leftLowerTop">
+            <div className="qui-checkbox">
+              {_.map(content?.topics, (topics, index) => {
+                return (
+                  <i
+                    className={`${
+                      topics.checked ? "fas fa-check-square" : "far fa-square"
+                    }`}
+                    key={topics.name + index}
+                    style={colors.accentColors}
+                  ></i>
+                );
+              })}
             </div>
-            <div className={`qui-rightSide size-${asSize}`}>
-                <div className={`qui-courseHeader variant-${asVariant}-text`} style={colors.textColors}>
-                    <h1>{props.content?.title}</h1>
-                    <div className={`qui-courseBanner variant-${asVariant} qui-btn`} style={colors.bannerColors}></div>
-                </div>
-                <div className="qui-courseDescription">
-                    <h3>{props.content?.description}</h3>
-                </div>
+            <div className="qui-courseDate" style={colors.textColors}>
+              <h2>{content?.dates.start_date}</h2>
+              <h2>&nbsp;-&nbsp;</h2>
+              <h2>{content?.dates.end_date}</h2>
             </div>
-        </motion.div>
-    );
+          </div>
+        </div>
+      </div>
+      <div className={`qui-rightSide size-${asSize}`}>
+        <div
+          className={`qui-courseHeader variant-${asVariant}-text`}
+          style={colors.textColors}
+        >
+          <h1>{props.content?.title}</h1>
+          <div
+            className={`qui-courseBanner variant-${asVariant} qui-btn`}
+            style={colors.bannerColors}
+          ></div>
+        </div>
+        <div className="qui-courseDescription">
+          <h3>{props.content?.description}</h3>
+        </div>
+      </div>
+    </motion.div>
+  );
 }
--- src/stories/EarnCard.stories.js
+++ src/stories/EarnCard.stories.js
@@ -2,170 +2,173 @@ import React from "react";
 import EarnCard from "../components/EarnCard/EarnCard.react";
 
 export default {
-    title: "Design System/EarnCard/EarnCard",
-    component: EarnCard,
-    argTypes: {
-        content: {
-            image: "",
-            tag: "",
-            title: "",
-            description: "",
-            icon: "",
-            dates: {
-                end_date: "",
-                start_date: "",
-            },
-            topics: []
+  title: "Design System/EarnCard/EarnCard",
+  component: EarnCard,
+  argTypes: {
+    content: {
+      table: {
+        defaultValue: {
+          image: "",
+          tag: "",
+          title: "",
+          description: "",
+          icon: "",
+          dates: {
+            end_date: "",
+            start_date: "",
+          },
+          topics: [],
         },
-        asVariant: {
-            control: "select",
-            options: ["primary", "secondary", "success", "warning", "error"],
-            table: {
-                category: "as-Flags",
-            },
-        },
-        asSize: {
-            control: "select",
-            options: ["tiny", "small", "normal", "big", "huge", "massive"],
-            table: {
-                category: "as-Flags",
-            },
-        },
-        withColor: {
-            table: {
-                category: "with-Params",
-                defaultValue: {
-                    backgroundColor: "",
-                    accentColor: "",
-                    textColor: "",
-                },
-            },
-        },
-        withAnimation: {
-            table: {
-                category: "with-Params",
-                defaultValue: {
-                    animation: "",
-                    duration: 0,
-                    delay: 0,
-                },
-            },
-        },
-        isDisabled: {
-            table: {
-                category: "is-Toggles",
-                defaultValue: false,
-            },
-        },
-        isHidden: {
-            table: {
-                category: "is-Toggles",
-                defaultValue: false,
-            },
-        },
-        onClick: {
-            table: {
-                category: "Events",
-                defaultValue: null,
-            },
-        },
+      },
     },
-    decorators: [
-        (story) => (
-            <div
-                style={{
-                    width: "100%",
-                    textAlign: "center",
-                }}
-            >
-                {story()}
-            </div>
-        ),
-    ],
-    parameters: {
-        componentSubtitle: "Displays a EarnCard with BannerCard, text and icon.",
-        a11y: { disable: true },
-        docs: {
-            iframeHeight: 600,
-        },
+    asVariant: {
+      control: "select",
+      options: ["primary", "secondary", "success", "warning", "error"],
+      table: {
+        category: "as-Flags",
+      },
     },
+    asSize: {
+      control: "select",
+      options: ["tiny", "small", "normal", "big", "huge", "massive"],
+      table: {
+        category: "as-Flags",
+      },
+    },
+    withColor: {
+      table: {
+        category: "with-Params",
+        defaultValue: {
+          backgroundColor: "",
+          accentColor: "",
+          textColor: "",
+        },
+      },
+    },
+    withAnimation: {
+      table: {
+        category: "with-Params",
+        defaultValue: {
+          animation: "",
+          duration: 0,
+          delay: 0,
+        },
+      },
+    },
+    isDisabled: {
+      table: {
+        category: "is-Toggles",
+        defaultValue: false,
+      },
+    },
+    isHidden: {
+      table: {
+        category: "is-Toggles",
+        defaultValue: false,
+      },
+    },
+    onClick: {
+      table: {
+        category: "Events",
+        defaultValue: null,
+      },
+    },
+  },
+  decorators: [
+    (story) => (
+      <div
+        style={{
+          width: "100%",
+          textAlign: "center",
+        }}
+      >
+        {story()}
+      </div>
+    ),
+  ],
+  parameters: {
+    componentSubtitle: "Displays a EarnCard with BannerCard, text and icon.",
+    a11y: { disable: true },
+    docs: {
+      iframeHeight: 600,
+    },
+  },
 };
 
 // -------------------------------------------------------------
 // Default
 // -------------------------------------------------------------
 const Template = (args) => {
-    return (
-        <EarnCard {...args} />
-    );
-}
+  return <EarnCard {...args} />;
+};
 
 export const Default = Template.bind({});
 Default.args = {
-    content: {
-        image: "static/media/Image.62bfb45a.png",
-        tag: "restricted",
-        title: 'QuoDeck Emerging Leadership Program',
-        description: 'Win a chance to apply for this exclusive opportunity for taking your career to the stars',
-        icon: 'fas fa-square',
-        dates: {
-            end_date: "3rd May",
-            start_date: "28th Feb"
-        },
-        topics: [
-            {
-                name: 'Name One',
-                contentList: [],
-                checked: true
-            },
-            {
-                name: 'Name Two',
-                contentList: [],
-                checked: false
-            },
-            {
-                name: 'Name Three',
-                contentList: [],
-                checked: false
-            },
-            {
-                name: 'Name Four',
-                contentList: [],
-                checked: false
-            },
-            {
-                name: 'Name Five',
-                contentList: [],
-                checked: true
-            },
-            {
-                name: 'Name Six',
-                contentList: [],
-                checked: false
-            },
-        ]
+  content: {
+    image: "static/media/Image.62bfb45a.png",
+    tag: "restricted",
+    title: "QuoDeck Emerging Leadership Program",
+    description:
+      "Win a chance to apply for this exclusive opportunity for taking your career to the stars",
+    icon: "fas fa-square",
+    dates: {
+      end_date: "3rd May",
+      start_date: "28th Feb",
     },
-    asVariant: "primary",
-    asSize: 'normal',
-    withColor: {
-        backgroundColor: "",
-        accentColor: "",
-        textColor: "#b60d17",
-    },
-    withAnimation: {
-        animation: "zoom",
-        duration: 0.5,
-        delay: 0,
-    },
-    isDisabled: false,
-    isHidden: false,
+    topics: [
+      {
+        name: "Name One",
+        contentList: [],
+        checked: true,
+      },
+      {
+        name: "Name Two",
+        contentList: [],
+        checked: false,
+      },
+      {
+        name: "Name Three",
+        contentList: [],
+        checked: false,
+      },
+      {
+        name: "Name Four",
+        contentList: [],
+        checked: false,
+      },
+      {
+        name: "Name Five",
+        contentList: [],
+        checked: true,
+      },
+      {
+        name: "Name Six",
+        contentList: [],
+        checked: false,
+      },
+    ],
+  },
+  asVariant: "primary",
+  asSize: "normal",
+  withColor: {
+    backgroundColor: "",
+    accentColor: "",
+    textColor: "#b60d17",
+  },
+  withAnimation: {
+    animation: "zoom",
+    duration: 0.5,
+    delay: 0,
+  },
+  isDisabled: false,
+  isHidden: false,
 };
 Default.parameters = {
-    docs: {
-        source: {
-            code: `<EarnCard {...${JSON.stringify(Default.args, null, 2)}}/>`,
-        },
+  docs: {
+    source: {
+      code: `<EarnCard {...${JSON.stringify(Default.args, null, 2)}}/>`,
     },
+  },
 };
 
 //-------------------------------------------------------------
@@ -173,22 +176,22 @@ Default.parameters = {
 // -------------------------------------------------------------
 export const ColoredEarnCard = Template.bind({});
 ColoredEarnCard.args = {
-    ...Default.args,
-    withColor: {
-        backgroundColor: "",
-        textColor: "teal",
-        accentColor: "tomato",
-    },
+  ...Default.args,
+  withColor: {
+    backgroundColor: "",
+    textColor: "teal",
+    accentColor: "tomato",
+  },
 };
 ColoredEarnCard.parameters = {
-    docs: {
-        description: {
-            story: "Use to override the standard colors of the Icon.",
-        },
-        source: {
-            code: `<EarnCard withColor={{backgroundColor: "orange", textColor: "gray",hoverBackgroundColor: "gray", hoverTextColor: "orange"}}}/>`,
-        },
+  docs: {
+    description: {
+      story: "Use to override the standard colors of the Icon.",
     },
+    source: {
+      code: `<EarnCard withColor={{backgroundColor: "orange", textColor: "gray",hoverBackgroundColor: "gray", hoverTextColor: "orange"}}}/>`,
+    },
+  },
 };
 
 //-------------------------------------------------------------
@@ -196,24 +199,24 @@ ColoredEarnCard.parameters = {
 // -------------------------------------------------------------
 export const AnimatedEarnCard = Template.bind({});
 AnimatedEarnCard.args = {
-    ...Default.args,
-    withAnimation: {
-        animation: "collapse",
-        duration: 1,
-        delay: 0,
-    },
+  ...Default.args,
+  withAnimation: {
+    animation: "collapse",
+    duration: 1,
+    delay: 0,
+  },
 };
 AnimatedEarnCard.parameters = {
-    docs: {
-        description: {
-            story: "We can animate the appearance of EarnCard",
-        },
-        source: {
-            code: `<EarnCard {...${JSON.stringify(
-                AnimatedEarnCard.args,
-                null,
-                2
-            )}}/>`,
-        },
+  docs: {
+    description: {
+      story: "We can animate the appearance of EarnCard",
     },
+    source: {
+      code: `<EarnCard {...${JSON.stringify(
+        AnimatedEarnCard.args,
+        null,
+        2
+      )}}/>`,
+    },
+  },
 };
