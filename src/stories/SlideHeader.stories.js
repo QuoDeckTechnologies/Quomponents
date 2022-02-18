fb1421745c68b5d94628b54e9a3cbcf7823a6eba
Rahul Verma <rahul.verma@quodeck.com>
2022-02-18 18:18:35 +0530

    Added SliderHeader Component

---
+++ src/stories/SlideHeader.stories.js
@@ -0,0 +1,176 @@
+import React from "react";
+import SlideHeader from "../components/SlideHeader/SlideHeader.react";
+
+export default {
+  title: "Design System/SlideHeader/SlideHeader",
+  component: SlideHeader,
+  argTypes: {
+    content: {
+      table: {
+        category: "with-Params",
+        defaultValue: {
+          title: "",
+          subtitle: "",
+        },
+      },
+    },
+    asSize: {
+      control: "select",
+      options: ["tiny", "small", "normal", "big", "huge", "massive"],
+      table: {
+        category: "as-Flags",
+      },
+    },
+    asPadded: {
+      control: "select",
+      options: ["fitted", "compact", "normal", "relaxed"],
+      table: {
+        category: "as-Flags",
+      },
+    },
+    asFloated: {
+      control: "select",
+      options: ["left", "right", "none", "inline"],
+      table: {
+        category: "as-Flags",
+      },
+    },
+    asAligned: {
+      control: "select",
+      options: ["left", "right", "center"],
+      table: {
+        category: "as-Flags",
+      },
+    },
+    withColor: {
+      table: {
+        category: "with-Params",
+        defaultValue: {
+          accentColor: "",
+          textColor: "",
+          backgroundColor: "",
+        },
+      },
+    },
+    withAnimation: {
+      table: {
+        category: "with-Params",
+        defaultValue: {
+          animation: "",
+          duration: 0,
+          delay: 0,
+        },
+      },
+    },
+    isHidden: {
+      table: {
+        category: "is-Toggles",
+        defaultValue: false,
+      },
+    },
+  },
+  decorators: [
+    (story) => (
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        {story()}
+      </div>
+    ),
+  ],
+  parameters: {
+    componentSubtitle:
+      "Default SlideHeader for general purpose use",
+    a11y: { disable: true },
+    docs: { iframeHeight: 400 },
+  },
+};
+// -------------------------------------------------------------
+// Default
+// -------------------------------------------------------------
+const Template = (args) => <SlideHeader {...args} />;
+export const Default = Template.bind({});
+Default.args = {
+  content: {
+    title: "Neque porro quisquam est qui dolorem",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, curabitur ipsum sem",
+  },
+  asSize: "normal",
+  asFloated: "none",
+  asPadded: "normal",
+  asAligned: "center",
+  withColor: {
+    accentColor: "#AD2929",
+    textColor: "",
+    backgroundColor: "#ad292980",
+  },
+  withAnimation: {
+    animation: "collapse",
+    duration: 0.5,
+    delay: 0,
+  },
+  isHidden: false,
+};
+Default.parameters = {
+  docs: {
+    source: {
+      code: `<SlideHeader {...${JSON.stringify(Default.args, null, 2)}}/>`,
+    },
+  },
+};
+// -------------------------------------------------------------
+// Colored SlideHeader
+// -------------------------------------------------------------
+export const ColoredSlideHeader = Template.bind({});
+ColoredSlideHeader.args = {
+  ...Default.args,
+  withColor: {
+    backgroundColor: "#666666",
+    textColor: "#ffffff",
+    accentColor: "#ffbf00",
+  },
+};
+ColoredSlideHeader.parameters = {
+  docs: {
+    description: {
+      story:
+        "Use to override the standard colors of the SliderHeader.",
+    },
+    source: {
+      code: `<ColoredSlideHeader {...${JSON.stringify(
+        ColoredSlideHeader.args,
+        null,
+        2
+      )}}/>`,
+    },
+  },
+};
+// -------------------------------------------------------------
+// Animated SlideHeader
+// -------------------------------------------------------------
+export const AnimatedSlideHeader = Template.bind({});
+AnimatedSlideHeader.args = {
+  ...Default.args,
+  withAnimation: {
+    animation: "slideRight",
+    duration: 0.5,
+    delay: 0,
+  },
+};
+AnimatedSlideHeader.parameters = {
+  docs: {
+    description: {
+      story:
+        "Use to animate the entry of the InlineEdit with the standard animation options and set duration and delay. Can be used to make multiple components enter the screen in a queue.",
+    },
+    source: {
+      code: `<AnimatedSlideHeader {...${JSON.stringify(
+        AnimatedSlideHeader.args,
+        null,
+        2
+      )}}/>`,
+    },
+  },
+}; 
