77b82eb43d96087277723efeb26f1b2f4375ec08
Udhav <udhav.varma@quodeck.com>
2022-03-01 10:02:14 +0530

    Test for ButtonBank added

---
+++ src/components/__tests__/ButtonBank.test.jsx
@@ -0,0 +1,41 @@
+//--------------------------------------
+// Import from NPM
+// -------------------------------------
+import { shallow } from "enzyme";
+//--------------------------------------
+// Import Component
+// -------------------------------------
+import ButtonBank from '../ButtonBank/ButtonBank.react'
+
+describe("EarnCard", () => {
+    // -------------------------------------
+    // Setup definitions for the test suite
+    // -------------------------------------
+    let component;
+    beforeEach(() => {
+        jest.resetAllMocks();
+        component = shallow(
+            <ButtonBank 
+                content = {[
+                    "Primary Button",
+                    "Primary Button",
+                    "Primary Button",
+                    "Primary Button",
+                ]}
+                isCircular = {false}
+                asVariant = "primary"
+                asSize = "normal"
+                asFloated = "none"
+                withColor = {null}
+                withLabel = {null}
+                withAnimation = {null}
+                isHidden = {null}
+                isDisabled = {false}
+                onClick = {() => {}}
+            />
+        );
+    });
+    it("should render correctly without throwing error", () => {
+        expect(component.exists()).toBe(true);
+    });
+})
