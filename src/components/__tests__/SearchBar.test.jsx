e9b701a391f2c3e325ca52af55336be1cf7658cc
Rohit-Dhende <rohit.dhende@quodeck.com>
2022-03-02 16:13:52 +0530

    Minor changes in Search Bar Test File

--- src/components/__tests__/SearchBar.test.jsx
+++ src/components/__tests__/SearchBar.test.jsx
@@ -2,7 +2,6 @@
 // Import from NPM
 // -------------------------------------
 import { shallow } from 'enzyme';
-
 //--------------------------------------
 // Import Components
 // -------------------------------------
@@ -24,6 +23,7 @@ describe("SearchBar", () => {
 
     beforeEach(() => {
         jest.resetAllMocks();
+
         component = shallow(<SearchBar
             asSize="normal"
             asFloated="inline"
@@ -36,7 +36,7 @@ describe("SearchBar", () => {
             isClosed={null}
             isFluid={null}
             onClick={() => console.log('test')}
-            placeHolder="Search"
+            placeHolder="Search..."
         />);
     })
 
@@ -45,32 +45,32 @@ describe("SearchBar", () => {
     });
 
     it("should render correctly when passed withTranslation Props", () => {
+        expect(component.find("input").props().placeholder).toBe("Search...")
         component.setProps({
-            placeHolder: "Content",
             withTranslation: {
                 lang: "hi",
                 tgt: "SearchBar",
                 dictionary: dictionary,
             },
         });
-        expect(component.exists()).toBe(true);
+        expect(component.find("input").props().placeholder).toBe("खोजें...");
     });
 
 
-    it("should render closed searchBar", () => {
+    it("should render closed Search Bar", () => {
         component.setProps({ isClosed: true });
         component.setProps({ isFluid: false });
 
-        let searchBarContainer = component.find("div").at(3);                                  
+        let searchBarChildContainer = component.find("div").at(3);
         let inputBox = component.find("input");
         let button = component.find("button");
 
-        expect(searchBarContainer.props().className).toBe('search-bar-child-container search-bar-child-container-closed');
+        expect(searchBarChildContainer.props().className).toBe('search-bar-child-container search-bar-child-container-closed');
         expect(inputBox.props().className).toBe('search-bar-input-field search-bar-input-closed');
         expect(button.props().className).toBe('search-bar-icon search-bar-icon-closed');
     });
 
-    it("should render opened searchBar when clicked", () => {
+    it("should render opened Search Bar when clicked", () => {
         component.setProps({ isClosed: false });
 
         let searchBarContainer = component.find("div").at(2);
@@ -82,5 +82,5 @@ describe("SearchBar", () => {
         expect(searchBarChildContainer.props().className).toBe("search-bar-child-container undefined");
         expect(inputBox.props().className).toBe("search-bar-input-field undefined");
         expect(button.props().className).toBe("search-bar-icon undefined");
-    })
+    });
 });
