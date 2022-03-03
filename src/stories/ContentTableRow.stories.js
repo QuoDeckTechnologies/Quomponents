c01647e42dca7c2e678a32be5491eb5c0626f97a
Udhav <udhav.varma@quodeck.com>
2022-03-03 19:05:56 +0530

    New story added

--- src/stories/ContentTableRow.stories.js
+++ src/stories/ContentTableRow.stories.js
@@ -2,83 +2,110 @@ import React from "react";
 import ContentTableRow from "../components/ContentTableRow/ContentTableRow.react";
 
 export default {
-    title: "Design System/ContentTableRow/ContentTableRow",
-    component: ContentTableRow,
-    argTypes: {
-        content: {
-            defaultValue : {
-                fileName : ''
-            }
-        },
-        withAnimation: {
-            table: {
-                category: "with-Params",
-                defaultValue: {
-                    animation: "",
-                    duration: 0,
-                    delay: 0,
-                },
-            },
-        },
-        isDisabled: {
-            table: {
-                category: "is-Toggles",
-                defaultValue: false,
-            },
-        },
-        isHidden: {
-            table: {
-                category: "is-Toggles",
-                defaultValue: false,
-            },
-        },
-        onClick: {
-            table: {
-                category: "Events",
-                defaultValue: null,
-            },
-        },
+  title: "Design System/ContentTableRow/ContentTableRow",
+  component: ContentTableRow,
+  argTypes: {
+    content: {
+      defaultValue: {
+        fileName: "",
+      },
     },
-    decorators: [
-        (story) => (
-            <div
-                style={{
-                    width: "100%",
-                    textAlign: "center",
-                }}
-            >
-                {story()}
-            </div>
-        ),
-    ],
-    parameters: {
-        componentSubtitle: "Displays a Content table row",
-        a11y: { disable: true },
+    withAnimation: {
+      table: {
+        category: "with-Params",
+        defaultValue: {
+          animation: "",
+          duration: 0,
+          delay: 0,
+        },
+      },
     },
+    isDisabled: {
+      table: {
+        category: "is-Toggles",
+        defaultValue: false,
+      },
+    },
+    isHidden: {
+      table: {
+        category: "is-Toggles",
+        defaultValue: false,
+      },
+    },
+    onClick: {
+      table: {
+        category: "Events",
+        defaultValue: null,
+      },
+    },
+  },
+  decorators: [
+    (story) => (
+      <div
+        style={{
+          width: "100%",
+          textAlign: "center",
+        }}
+      >
+        {story()}
+      </div>
+    ),
+  ],
+  parameters: {
+    componentSubtitle: "Displays a Content table row",
+    a11y: { disable: true },
+  },
 };
 
 // -------------------------------------------------------------
 // Default
 // -------------------------------------------------------------
-const Template = (args) => <ContentTableRow {...args} /> 
+const Template = (args) => <ContentTableRow {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-    content: {
-        fileName : 'Dummy file-name.pdf'
-    },
-    withAnimation: {
-        animation: "zoom",
-        duration: 0.5,
-        delay: 0,
-    },
-    isDisabled: false,
-    isHidden: false,
+  content: {
+    fileName: "Dummy file-name.pdf",
+  },
+  withAnimation: {
+    animation: "zoom",
+    duration: 0.5,
+    delay: 0,
+  },
+  isDisabled: false,
+  isHidden: false,
 };
 Default.parameters = {
-    docs: {
-        source: {
-            code: `<ContentTableRow {...${JSON.stringify(Default.args, null, 2)}}/>`,
-        },
+  docs: {
+    source: {
+      code: `<ContentTableRow {...${JSON.stringify(Default.args, null, 2)}}/>`,
     },
+  },
+};
+
+// -------------------------------------------------------------
+// ContentTableRow
+// -------------------------------------------------------------
+const ListTemplate = (args) => {
+
+  return (
+    <div className="qui-content-table-row-list">
+      <ContentTableRow {...args} content={{fileName:'Dummy file-name.pdf'}}/>
+      <ContentTableRow {...args} content={{fileName:'Dummy file-name.zip'}}/>
+      <ContentTableRow {...args} content={{fileName:'Dummy file-name.qdf'}}/>
+      <ContentTableRow {...args} content={{fileName:'Dummy file-name.xyz'}}/>
+    </div>
+  );
+};
+export const ContentTableRowList = ListTemplate.bind({});
+ContentTableRowList.parameters = {
+  docs: {
+    source: {
+      code: `<ContentTableRowList {...${JSON.stringify(
+        ContentTableRowList.args,
+        null,
+        2
+      )}}/>`,
+    },
+  },
 };
