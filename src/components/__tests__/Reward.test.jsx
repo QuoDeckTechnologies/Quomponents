abe0c7c6d2b96fb91fd87a7b4f74f806fd153e60
Rahul Verma <rahul.verma@quodeck.com>
2022-03-04 10:59:51 +0530

    Changes in test file

--- src/components/__tests__/Reward.test.jsx
+++ src/components/__tests__/Reward.test.jsx
@@ -6,10 +6,6 @@ import { shallow, mount } from 'enzyme';
 // Import Components
 // -------------------------------------
 import Reward from '../Reward/Reward.react'
-//--------------------------------------
-// Import content's image
-// -------------------------------------
-// import coin from "../assets/icons8_coin_96px.png";
 
 describe("Reward", () => {
     // -------------------------------------
@@ -39,8 +35,8 @@ describe("Reward", () => {
                     label: "Complete to win",
                     point: "10,000",
                     image: "coin.png"
-                },
-            }),
+                }
+            })
             expect(component.exists()).toBe(true);
         });
 });
