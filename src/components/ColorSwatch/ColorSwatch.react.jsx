60c3c8fde28fd6d92bd228c8d4e574b6f699176f
Rohit-Dhende <rohit.dhende@quodeck.com>
2022-03-10 18:09:24 +0530

    Added notes in the docs

--- src/components/ColorSwatch/ColorSwatch.react.jsx
+++ src/components/ColorSwatch/ColorSwatch.react.jsx
@@ -76,10 +76,7 @@ ActionButton.defaultProps = {
 
 /**
 ## Notes
-- The design system used for this component is Material UI (@mui/material)
-- The animation system used for this component is Framer Motion (framer-motion)
-- Pass inline styles to the component to override any of the component css
-- Or add custom css in overrule.scss to override the component css
+- Pass withColor props which consists of primary, accent, secondary and page color props.
 **/
 export default function ActionButton(props) {
     //-------------------------------------------------------------------
