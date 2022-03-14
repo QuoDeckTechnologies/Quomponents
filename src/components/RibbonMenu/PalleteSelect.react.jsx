3b61b9dcdf9c032e056add5118d8d11e8f22f397
Rohit-Dhende <rohit.dhende@quodeck.com>
2022-03-14 18:22:57 +0530

    Imported ColorSwatch and CustomColor components and made  code minimal

---
+++ src/components/RibbonMenu/PalleteSelect.react.jsx
@@ -0,0 +1,49 @@
+const PaletteSet = [
+    {
+        colors: ["#F88A8A", "#EF2929", "#685555", "#FFFFFF"]
+    },
+    {
+        colors: ["#E58E72", "#EF8829", "#685555", "#FFFFFF"]
+    },
+    {
+        colors: ["#F8CC8A", "#F8CC8A", "#685555", "#FFFFFF"]
+    },
+    {
+        colors: ["#55771E", "#274708", "#685555", "#FFFFFF"]
+    },
+    {
+        colors: ["#87D9AD", "#54D040", "#685555", "#FFFFFF"]
+    },
+    {
+        colors: ["#8AF8F8", "#29B4EF", "#685555", "#FFFFFF"]
+    },
+    {
+        colors: ["#8AB6F8", "#293DEF", "#685555", "#FFFFFF"]
+    },
+    {
+        colors: ["#9A8AF8", "#4529EF", "#685555", "#FFFFFF"]
+    },
+    {
+        colors: ["#CE8AF8", "#8029EF", "#685555", "#FFFFFF"]
+    },
+    {
+        colors: ["#D955EF", "#B029EF", "#685555", "#FFFFFF"]
+    },
+    {
+        colors: ["#6B3894", "#6B3894", "#685555", "#FFFFFF"]
+    },
+    {
+        colors: ["#211F8A", "#22335F", "#685555", "#FFFFFF"]
+    },
+    {
+        colors: ["#D41717", "#D41717", "#685555", "#FFFFFF"]
+    },
+    {
+        colors: ["#9E1212", "#ED8B8B", "#685555", "#FFFFFF"]
+    },
+    {
+        colors: ["#E5DADA", "#534F4F", "#685555", "#FFFFFF"]
+    },
+];
+
+export { PaletteSet };
--- src/components/RibbonMenu/RibbonDesignMenu.react.jsx
+++ src/components/RibbonMenu/RibbonDesignMenu.react.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-
+import _ from "lodash";
 import CustomColor from "../CustomColor/CustomColor.react";
 import ColorSwatch from "../ColorSwatch/ColorSwatch.react"
+
+import { PaletteSet } from "./PalleteSelect.react";
 
 export default function RibbonToolMenu(props) {
 
@@ -9,151 +11,59 @@ export default function RibbonToolMenu(props) {
 
     return (
         <div className={`ribbon-menu-design-container`}>
-
-            <div className={`palete-container`}>
-                <div className={`swatch-container`}>
-                    <ColorSwatch asSize="small"
-                        withColor={{
-                            primaryColor: "#F88A8A",
-                            accentColor: "#EF2929",
-                            secondaryColor: "#685555",
-                            pageColor: "#FFFFFF",
-                        }}
-                    />
-                    <ColorSwatch asSize="small" withColor={{
-                        primaryColor: "#E58E72",
-                        accentColor: "#EF8829",
-                        secondaryColor: "#685555",
-                        pageColor: "#FFFFFF",
-                    }} />
-                    <ColorSwatch asSize="small" withColor={{
-                        primaryColor: "#F8CC8A",
-                        accentColor: "#EF8829",
-                        secondaryColor: "#685555",
-                        pageColor: "#FFFFFF",
-                    }} />
-                    <ColorSwatch asSize="small" withColor={{
-                        primaryColor: "#55771E",
-                        accentColor: "#274708",
-                        secondaryColor: "#685555",
-                        pageColor: "#FFFFFF",
-                    }} />
-                    <ColorSwatch asSize="small" withColor={{
-                        primaryColor: "#87D9AD",
-                        accentColor: "#87D9AD",
-                        secondaryColor: "#685555",
-                        pageColor: "#FFFFFF",
-                    }} />
-                    <ColorSwatch asSize="small" withColor={{
-                        primaryColor: "#8AF8F8",
-                        accentColor: "#29B4EF",
-                        secondaryColor: "#685555",
-                        pageColor: "#FFFFFF",
-                    }} />
-                    <ColorSwatch asSize="small" withColor={{
-                        primaryColor: "#8AB6F8",
-                        accentColor: "#293DEF",
-                        secondaryColor: "#685555",
-                        pageColor: "#FFFFFF",
-                    }} />
-                    <ColorSwatch asSize="small" withColor={{
-                        primaryColor: "#9A8AF8",
-                        accentColor: "#4529EF",
-                        secondaryColor: "#685555",
-                        pageColor: "#FFFFFF",
-                    }} />
-                    <ColorSwatch asSize="small" withColor={{
-                        primaryColor: "#CE8AF8",
-                        accentColor: "#8029EF",
-                        secondaryColor: "#685555",
-                        pageColor: "#FFFFFF",
-                    }} />
-                    <ColorSwatch asSize="small" withColor={{
-                        primaryColor: "#D955EF",
-                        accentColor: "#B029EF",
-                        secondaryColor: "#685555",
-                        pageColor: "#FFFFFF",
-                    }} />
-                    <ColorSwatch asSize="small" withColor={{
-                        primaryColor: "#6B3894",
-                        accentColor: "#6B3894",
-                        secondaryColor: "#685555",
-                        pageColor: "#FFFFFF",
-                    }} />
-                    <ColorSwatch asSize="small" withColor={{
-                        primaryColor: "#211F8A",
-                        accentColor: "#22335F",
-                        secondaryColor: "#685555",
-                        pageColor: "#FFFFFF",
-                    }} />
-                    <ColorSwatch asSize="small" withColor={{
-                        primaryColor: "#D41717",
-                        accentColor: "#D41717",
-                        secondaryColor: "#685555",
-                        pageColor: "#FFFFFF",
-                    }} />
-                    <ColorSwatch asSize="small" withColor={{
-
-                        primaryColor: "#9E1212",
-                        accentColor: "#ED8B8B",
-                        secondaryColor: "#685555",
-                    }
-                    } />
-                    <ColorSwatch asSize="small" withColor={{
-                        primaryColor: "#E5DADA",
-                        accentColor: "#534F4F",
-                        secondaryColor: "#685555",
-                        pageColor: "#FFFFFF",
-                    }} />
+            <div className="color-pallete-section">
+                <div className="color-pallete-section-child-container">
+                    <div className="color-pallete-section-child">
+                        {_.map(PaletteSet, (colors, index) => {
+                            return (
+                                <ColorSwatch
+                                    asSize="small"
+                                    withColor={{
+                                        primaryColor: colors.colors[0],
+                                        accentColor: colors.colors[1],
+                                        secondaryColor: colors.colors[2],
+                                        pageColor: colors.colors[3],
+                                    }}
+                                />
+                            )
+                        })}
+                    </div>
                 </div>
-                <div className={`label`}>
-                    Pallete
-                </div>
+                <div className="label-file">Settings</div>
             </div>
             <div className={`custom-color-container`}>
                 <CustomColor
-                    {...props}
-                    withAnimation={{
-                        animation: "slideRight",
-                        duration: 0.8,
-                        delay: 0
-                    }}
                     asSize="tiny"
-                    asPadded="fitted"
                     content={{ color: "#fff", title: "Page Color" }} />
                 <CustomColor {...props}
-                    withAnimation={{
-                        animation: "slideLeft",
-                        duration: 0.8,
-                        delay: 0
-                    }}
                     asSize="tiny"
-                    asPadded="fitted"
                     content={{ color: "#994040", title: "Primary Color" }} />
                 <CustomColor  {...props}
-                    withAnimation={{
-                        animation: "slideRight",
-                        duration: 0.8,
-                        delay: 0
-                    }}
                     asSize="tiny"
-                    asPadded="fitted"
                     content={{ color: "#F4AEAE", title: "Accent Color" }} />
                 <CustomColor  {...props}
-                    withAnimation={{
-                        animation: "slideLeft",
-                        duration: 0.8,
-                        delay: 0
-                    }}
                     asSize="tiny"
-                    asPadded="fitted"
                     content={{ color: "#685555", title: "Secondary Color" }} />
             </div>
-            <div className={`slide-background`}>
-
+            <div className="parent-vertical-line"></div>
+            <div className="overlay-background-section">
+                <div className="overlay-background-section-child-container">
+                    <div className="overlay-background-section-child">
+                        <div className="set-remove"></div>
+                        <div className="label-set-remove">Set Remove</div>
+                    </div>
+                </div>
+                <div className="label-file">Slide Background</div>
             </div>
-            <div className={`slide-background`}>
-
+            <div className="parent-vertical-line"></div>
+            <div className="slide-background-section">
+                <div className="slide-background-section-child-container">
+                    <div className="slide-background-section-child">
+                        <div className="set-remove"></div>
+                        <div className="label-set-remove">Set Remove</div>
+                    </div>
+                </div>
+                <div className="label-file">Overlay Background</div>
             </div>
         </div>
     );
