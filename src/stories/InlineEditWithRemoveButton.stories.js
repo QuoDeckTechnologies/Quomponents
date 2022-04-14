068c1af23b4467440a71895d173f285fe9075722
Udhav <udhav.varma@quodeck.com>
2022-04-14 13:51:55 +0530

    Component created

---
+++ src/stories/InlineEditWithRemoveButton.stories.js
@@ -0,0 +1,96 @@
+import React from "react";
+import InlineEditWithRemoveButton from "../components/OptionItem/InlineEditWithRemoveButton/InlineEditWithRemoveButton.react";
+
+export default {
+  title:
+    "Design System/OptionItem/InlineEditWithRemoveButton/InlineEditWithRemoveButton",
+  component: InlineEditWithRemoveButton,
+  argTypes: {
+    content: {},
+    withColor: {
+      table: {
+        category: "with-Params",
+        defaultValue: {
+          backgroundColor: "",
+          accentColor: "",
+          textColor: "",
+        },
+      },
+    },
+    withAnimation: {
+      table: {
+        category: "with-Params",
+        defaultValue: {
+          animation: "",
+          duration: 0,
+          delay: 0,
+        },
+      },
+    },
+    isDisabled: {
+      table: {
+        category: "is-Toggles",
+        defaultValue: false,
+      },
+    },
+    isHidden: {
+      table: {
+        category: "is-Toggles",
+        defaultValue: false,
+      },
+    },
+    onInput: {
+      table: {
+        category: "Events",
+        defaultValue: null,
+      },
+    },
+    onClose: {
+      table: {
+        category: "Events",
+        defaultValue: null,
+      },
+    },
+  },
+  parameters: {
+    componentSubtitle: "Displays a InlineEdit with remove button.",
+    a11y: { disable: true },
+    docs: {
+      iframeHeight: 250,
+    },
+  },
+};
+// -------------------------------------------------------------
+// Default
+// -------------------------------------------------------------
+const Template = (args) => {
+  return <InlineEditWithRemoveButton {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  content: {
+    targetName: "name one",
+    value: "",
+    placeholder: "Placeholder One",
+  },
+  withColor: {
+    backgroundColor: "#ffab000d",
+    accentColor: "",
+    textColor: "",
+  },
+  withAnimation: {
+    animation: "zoom",
+    duration: 0.5,
+    delay: 0,
+  },
+  isDisabled: false,
+  isHidden: false,
+};
+Default.parameters = {
+  docs: {
+    source: {
+      code: `<OptionItem {...${JSON.stringify(Default.args, null, 2)}}/>`,
+    },
+  },
+};
