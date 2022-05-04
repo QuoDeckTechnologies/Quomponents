adb41d87b7c6ff417b7fe9c349a9007d404ad67a
Rahul Verma <rahul.verma@quodeck.com>
2022-05-04 18:47:25 +0530

    Added Video component with test file

---
+++ src/components/Templates/Video/Video.react.jsx
@@ -0,0 +1,159 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { motion } from "framer-motion";
+import {
+    getAnimation,
+    getQuommons,
+    resolveImage,
+} from "../../../common/javascripts/helpers";
+import "../../../common/stylesheets/common.css";
+import "./Video.scss";
+import "../../../common/stylesheets/overrule.scss";
+import SlideHeader from "../../SlideHeader/SlideHeader.react";
+import Videobox from "../../Videobox/Videobox.react";
+
+Video.propTypes = {
+    //=======================================
+    // Component Specific props
+    //=======================================
+    /**
+    Video data should be passed in data field and it is a required field
+    */
+    data: PropTypes.shape({
+        title: PropTypes.string,
+        subtitle: PropTypes.string,
+        image: PropTypes.object,
+        backgroundImage: PropTypes.object,
+        video: PropTypes.string,
+
+    }).isRequired,
+    /**
+    Video can set iconlist image & backgroundImage from imageLibrary.
+    */
+    imageLibrary: PropTypes.array,
+    /**
+    slideId can be used if same template is used continueously for multiple slides.
+    */
+    slideId: PropTypes.number,
+    //=======================================
+    // Quommon props
+    //=======================================
+    /**
+    Use to override component colors and behavior
+    */
+    withColor: PropTypes.shape({
+        backgroundColor: PropTypes.string,
+        slideHeaderTextColor: PropTypes.string,
+        slideHeaderBackgroundColor: PropTypes.string,
+        slideHeaderAccentColor: PropTypes.string,
+    }),
+    /**
+    Use to define the entry animation of the component
+    */
+    withAnimation: PropTypes.shape({
+        animation: PropTypes.oneOf([
+            "zoom",
+            "collapse",
+            "fade",
+            "slideDown",
+            "slideUp",
+            "slideLeft",
+            "slideRight",
+            "",
+        ]),
+        duration: PropTypes.number,
+        delay: PropTypes.number,
+    }),
+    /**
+    Use to show/hide the component
+    */
+    isHidden: PropTypes.bool,
+};
+
+Video.defaultProps = {
+    //=======================================
+    // Component Specific props
+    //=======================================
+    data: {},
+    slideId: 0,
+    //=======================================
+    // Quommon props
+    //=======================================
+    withColor: null,
+    withAnimation: null,
+
+    isHidden: false,
+};
+/**
+## Notes
+- The design system used for this component is HTML and CSS
+- The animation system used for this component is Framer Motion (framer-motion)
+- Pass inline styles to the component to override any of the component css
+- Or add custom css in overrule.scss to override the component css
+**/
+export default function Video(props) {
+    //-------------------------------------------------------------------
+    // 1. Destructuring props
+    //-------------------------------------------------------------------
+    let { data, withColor, imageLibrary, slideId } = props;
+    //-------------------------------------------------------------------
+    // 2. Set the classes
+    //-------------------------------------------------------------------
+    let quommonClasses = getQuommons(props, "video");
+    //-------------------------------------------------------------------
+    // 3. Use to set Color in Video
+    //-------------------------------------------------------------------
+    let slideHeaderColors = {
+        textColor: withColor?.slideHeaderTextColor,
+        accentColor: withColor?.slideHeaderAccentColor,
+        backgroundColor: withColor?.slideHeaderBackgroundColor
+    }
+    //-------------------------------------------------------------------
+    // 4. Function to set background
+    //-------------------------------------------------------------------
+    const getBackground = () => {
+        if (data?.backgroundImage) {
+            return {
+                backgroundImage: `url(${resolveImage(
+                    data?.backgroundImage.id,
+                    imageLibrary
+                )})`,
+            };
+        }
+    };
+    const background = getBackground();
+    //-------------------------------------------------------------------
+    // 5. Get animation of the component
+    //-------------------------------------------------------------------
+    const animate = getAnimation(props.withAnimation);
+    // ========================= Render Function =================================
+    return (
+        <motion.div
+            initial={animate.from}
+            animate={animate.to}
+            className={`qui ${quommonClasses.parentClasses}`}
+            style={{
+                ...background,
+                backgroundColor: withColor?.backgroundColor,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+            }}
+        >
+            <div className={`qui-video-card ${quommonClasses.childClasses}`} key={"video" + slideId}>
+                {!data?.image && (data?.title || data?.subtitle) && (
+                    <SlideHeader {...props}
+                        content={{ title: data?.title, subTitle: data?.subtitle }}
+                        withColor={slideHeaderColors} />
+                )}
+                {data?.image && (
+                    <img className="qui-video-image"
+                        src={resolveImage(data?.image.id, imageLibrary)}
+                        alt="video" />
+                )}
+                <Videobox {...props}
+                    url={data?.video}
+                />
+            </div>
+        </motion.div>
+    );
+}
