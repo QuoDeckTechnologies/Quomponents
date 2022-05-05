9eff62d13d2cbc3dcb9f9b523bd0ea68ced32404
ganesh123 <ganesh.devkar@quodeck.com>
2022-05-05 18:49:47 +0530

    handle test coverage

--- src/components/__tests__/templates/PictureSingleSelect.test.jsx
+++ src/components/__tests__/templates/PictureSingleSelect.test.jsx
@@ -7,7 +7,11 @@ import { shallow } from "enzyme";
 // -------------------------------------
 import PictureSingleSelect from "../../Templates/PictureSingleSelect/PictureSingleSelect.react";
 import ClickableImage from "../../ClickableImage/ClickableImage.react"
+
 describe("PictureSingleSelect", () => {
+    // -------------------------------------
+    // Setup definitions for the test suite
+    // -------------------------------------
     let component;
     beforeEach(() => {
         jest.resetAllMocks();
@@ -16,18 +20,36 @@ describe("PictureSingleSelect", () => {
                 data={{
                     title: "This is Title",
                     subtitle: "This is Subtitle",
-                    question: "This is Question",
-                    image: "https://us.123rf.com/450wm/microone/microone1909/microone190900839/130722932-chaos-in-workplace-sleepy-lazy-unorganized-employees-in-office-bad-organization-control-business-cor.jpg",
+                    question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit? ",
+                    backgroundImage: {
+                        extention: "",
+                        id: "background-image",
+                    },
                     imageOpts: [
-                        "https://us.123rf.com/450wm/microone/microone1909/microone190900839/130722932-chaos-in-workplace-sleepy-lazy-unorganized-employees-in-office-bad-organization-control-business-cor.jpg",
-                        "https://us.123rf.com/450wm/microone/microone1909/microone190900839/130722932-chaos-in-workplace-sleepy-lazy-unorganized-employees-in-office-bad-organization-control-business-cor.jpg"
+                        {
+                            text: "<< List item 1. Replace with your own text >>",
+                            image: {
+                                id: "HyLQNLtdp",
+                                extension: ".jpeg"
+                            }
+                        },
                     ],
-                    backgroundImage: "https://us.123rf.com/450wm/microone/microone1909/microone190900839/130722932-chaos-in-workplace-sleepy-lazy-unorganized-employees-in-office-bad-organization-control-business-cor.jpg",
+                    imageLibrary: [{
+                        id: "header-image",
+                        image: "https://us.123rf.com/450wm/microone/microone1909/microone190900839/130722932-chaos-in-workplace-sleepy-lazy-unorganized-employees-in-office-bad-organization-control-business-cor.jpg"
+                    }, {
+                        id: "background-image",
+                        image: "https://us.123rf.com/450wm/microone/microone1909/microone190900839/130722932-chaos-in-workplace-sleepy-lazy-unorganized-employees-in-office-bad-organization-control-business-cor.jpg"
+                    }, {
+                        id: "image-1",
+                        image: "https://us.123rf.com/450wm/microone/microone1909/microone190900839/130722932-chaos-in-workplace-sleepy-lazy-unorganized-employees-in-office-bad-organization-control-business-cor.jpg"
+                    },],
+                    caption: "caption",
                 }}
                 slideId={0}
                 asVariant="primary"
-                asFloated="left"
                 withColor={null}
+                withAnimation={null}
                 isHidden={false}
                 isDisabled={false}
                 onClick={(e) => {
@@ -39,58 +61,96 @@ describe("PictureSingleSelect", () => {
     it("should render correctly without throwing error", () => {
         expect(component.exists()).toBe(true);
     });
-    it('should render correctly when click ClickableImage', () => {
-        component.find(ClickableImage).at(0).simulate('click');
-        component.find(ClickableImage).at(1).simulate('click');
-    });
     it("should render correctly with empty content", () => {
         component.setProps({
             data: {},
         });
         expect(component.exists()).toBe(true);
     });
-    it("should render correctly when passed withColor props if backgroundImage pass from data props", () => {
-        let data = {
-            title: "This is Title",
-            subtitle: "This is Subtitle",
-            imageOpts: [
-                "https://us.123rf.com/450wm/microone/microone1909/microone190900839/130722932-chaos-in-workplace-sleepy-lazy-unorganized-employees-in-office-bad-organization-control-business-cor.jpg",
-                "https://us.123rf.com/450wm/microone/microone1909/microone190900839/130722932-chaos-in-workplace-sleepy-lazy-unorganized-employees-in-office-bad-organization-control-business-cor.jpg"
-            ],
-            image: "https://us.123rf.com/450wm/microone/microone1909/microone190900839/130722932-chaos-in-workplace-sleepy-lazy-unorganized-employees-in-office-bad-organization-control-business-cor.jpg",
-            question: "this is question",
-            backgroundImage: "https://us.123rf.com/450wm/microone/microone1909/microone190900839/130722932-chaos-in-workplace-sleepy-lazy-unorganized-employees-in-office-bad-organization-control-business-cor.jpg",
+    it("should render correctly with image prop is passed", () => {
+        component.setProps({
+            data: {
+                image: {
+                    extention: "",
+                    id: "header-image",
+                },
+            },
+            imageLibrary: [{
+                id: "header-image",
+                image: "test.png"
+            }]
+        });
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly if click on PictureSingleSelect", () => {
+        component.setProps({
+            data: {
+                imageOpts: [
+                    {
+                        text: "<< List item 1. Replace with your own text >>",
+                        image: {
+                            id: "HyLQNLtdp",
+                            extension: ".jpeg"
+                        }
+                    },
+                    {
+                        text: "<< List item 2. Replace with your own text >>",
+                        image: {
+                            id: "SKjOfKwWN",
+                            extension: ".jpeg"
+                        }
+                    },
+                    {
+                        text: "<< List item 3. Replace with your own text >>",
+                        image: {
+                            id: "h4EX9FXyK",
+                            extension: ".jpeg"
+                        }
+                    },
+                    {
+                        text: "<< List item 4. Replace with your own text >>",
+                        image: {
+                            id: "qsZ-6w6lh",
+                            extension: ".jpeg"
+                        }
+                    }
+                ],
+            },
+            imageLibrary: [{
+                image: "https://i.pinimg.com/564x/02/58/79/025879fc48f8d5a2a30e37ad1a847c11.jpg",
+                id: "HyLQNLtdp",
+            }, {
+                image: "https://i.pinimg.com/564x/64/57/dc/6457dc8f063f284fe17e519dc28b5437.jpg",
+                id: "SKjOfKwWN",
+            }, {
+                image: "https://i.pinimg.com/564x/9a/db/19/9adb19d50115e570142a7937a00550ae.jpg",
+                id: "h4EX9FXyK",
+            }, {
+                image: "https://i.pinimg.com/564x/09/03/8c/09038c92e5e2422e369816d50b16cd3f.jpg",
+                id: "qsZ-6w6lh",
+            },]
+        });
+        component.find(ClickableImage).at(0).simulate('click')
+    });
+    it("should render correctly when passed withAnimation props", () => {
+        let animation = {
+            animation: "zoom",
+            duration: 0.5,
+            delay: 0,
         }
-        let colors = {
-            backgroundColor: "",
-            slideHeaderTextColor: "ff0000",
-            slideHeaderAccentColor: "23ff00",
-            slideHeaderBackgroundColor: "00ff00",
-            captionTextColor: "ff0000",
-            captionBackgroundColor: "00ff00",
-        }
-        component.setProps({ data: data })
-        component.setProps({ withColor: colors, data: data })
+        component.setProps({ withAnimation: animation })
         expect(component.exists()).toBe(true);
     })
-    it("should render correctly when passed withColor props if backgroundImage removed from data props", () => {
-        let data = {
-            title: "This is Title",
-            subtitle: "This is Subtitle",
-            image: "https://us.123rf.com/450wm/microone/microone1909/microone190900839/130722932-chaos-in-workplace-sleepy-lazy-unorganized-employees-in-office-bad-organization-control-business-cor.jpg",
-            question: "this is question",
-            backgroundImage: "",
-        }
+    it("should render correctly when passed withColor props", () => {
         let colors = {
-            backgroundColor: "#F4BFCB",
-            slideHeaderTextColor: "ff0000",
-            slideHeaderAccentColor: "23ff00",
-            slideHeaderBackgroundColor: "00ff00",
-            captionTextColor: "ff0000",
-            captionBackgroundColor: "00ff00",
+            slideHeaderTextColor: "#FFFFFF",
+            slideHeaderAccentColor: "#AD2929",
+            slideHeaderBackgroundColor: "#ad292980",
+            textBlockBackgroundColor: "#2d92a4",
+            textBlockTextColor: "#fff",
         }
-        component.setProps({ data: data })
-        component.setProps({ withColor: colors, data: data })
+        component.setProps({ withColor: colors })
         expect(component.exists()).toBe(true);
     })
     it("should render correctly when passed withAnimation props", () => {
@@ -102,6 +162,11 @@ describe("PictureSingleSelect", () => {
         component.setProps({ withAnimation: animation })
         expect(component.exists()).toBe(true);
     })
+    it("should render correctly when passed data as null", () => {
+        component.setProps({ data: {} })
+        expect(component.exists()).toBe(true);
+    })
+
     it("should render correctly when passed isHidden props as false", () => {
         component.setProps({ isHidden: false })
         expect(component.exists()).toBe(true);
@@ -116,40 +181,6 @@ describe("PictureSingleSelect", () => {
     })
     it("should render correctly when passed isDisabled props as true", () => {
         component.setProps({ isDisabled: true })
-        expect(component.exists()).toBe(true);
-    })
-    it("should render correctly when passed answer props as null", () => {
-        component.setProps({ answer: null })
-        expect(component.exists()).toBe(true);
-    })
-    it("should render correctly when passed asVariant prop as primary", () => {
-        component.setProps({ asVariant: "primary" })
-        expect(component.exists()).toBe(true);
-    })
-    it("should render correctly when passed asVariant prop as secondary", () => {
-        component.setProps({ asVariant: "secondary" })
-        expect(component.exists()).toBe(true);
-    })
-    it("should render correctly when passed asVariant prop as warning", () => {
-        component.setProps({ asVariant: "warning" })
-        expect(component.exists()).toBe(true);
-    })
-    it("should render correctly when passed asVariant prop as error", () => {
-        component.setProps({ asVariant: "error" })
-        expect(component.exists()).toBe(true);
-    })
-    it("should render correctly when passed asVariant prop as success", () => {
-        component.setProps({ asVariant: "success" })
-        expect(component.exists()).toBe(true);
-    })
-    it("should render correctly when passed image prop as null", () => {
-        let data = {
-            title: "This is Title",
-            subtitle: "This is Subtitle",
-            image: "",
-            quetion: "this is question"
-        }
-        component.setProps({ data: data })
         expect(component.exists()).toBe(true);
     })
 });
