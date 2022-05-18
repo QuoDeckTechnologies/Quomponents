089ccbaf9ae82f374a126ca8263ff3703a82281e
ganesh123 <ganesh.devkar@quodeck.com>
2022-05-18 10:11:05 +0530

    handle css properly

--- src/components/Templates/DeckAnalysisModal/DeckAnalysisModal.react.jsx
+++ src/components/Templates/DeckAnalysisModal/DeckAnalysisModal.react.jsx
@@ -95,258 +95,259 @@ export default function DeckAnalysisModal(props) {
     //-------------------------------------------------------------------
     const animate = getAnimation(props.withAnimation);
 
-    // getSlidesCheck = () => {
+    // let getSlidesCheck = () => {
     //     return (
     //         content.length > 9 &&
     //         content.length < 41
     //     );
     // };
 
-    // getTextCheck = () => {
-    //     let textCheck = true;
-    //     let culpritSlideText = [];
-    //     let defaultText = 0;
-    //     _.each(content, slide => {
-    //         _.each(_.toPairs(slide.data), component => {
-    //             switch (ComponentInputs[component[0]].type) {
-    //                 case "text":
-    //                 case "textarea":
-    //                     /* istanbul ignore next - Implicit check */
-    //                     if (
-    //                         component[1].indexOf("<<") !== -1 ||
-    //                         component[1].indexOf(">>") !== -1
-    //                     ) {
-    //                         textCheck = false;
-    //                         culpritSlideText.push(slide.slideSeq + 1);
-    //                         defaultText++;
-    //                     }
-    //                     break;
-    //                 case "bullets":
-    //                     _.each(component[1], li => {
-    //                         /* istanbul ignore next - Implicit check */
-    //                         if (
-    //                             li.indexOf("<<") !== -1 ||
-    //                             li.indexOf(">>") !== -1
-    //                         ) {
-    //                             textCheck = false;
-    //                             culpritSlideText.push(slide.slideSeq + 1);
-    //                             defaultText++;
-    //                         }
-    //                     });
-    //                     break;
-    //                 case "imagelist":
-    //                 case "radiolist":
-    //                 case "checklist":
-    //                     _.each(component[1], li => {
-    //                         /* istanbul ignore next - Implicit check */
-    //                         if (
-    //                             li.text.indexOf("<<") !== -1 ||
-    //                             li.text.indexOf(">>") !== -1
-    //                         ) {
-    //                             textCheck = false;
-    //                             culpritSlideText.push(slide.slideSeq + 1);
-    //                             defaultText++;
-    //                         }
-    //                     });
-    //                     break;
-    //                 default:
-    //             }
-    //         });
-    //     });
-    //     return {
-    //         status: textCheck,
-    //         errorCount: defaultText,
-    //         culprit: _.uniq(culpritSlideText)
-    //     };
-    // };
+    let getTextCheck = () => {
+        // let textCheck = true;
+        // let culpritSlideText = [];
+        // let defaultText = 0;
+        // _.each(content, slide => {
+        //     _.each(_.toPairs(slide.data), component => {
+        //         switch (ComponentInputs[component[0]].type) {
+        //             case "text":
+        //             case "textarea":
+        //                 /* istanbul ignore next - Implicit check */
+        //                 if (
+        //                     component[1].indexOf("<<") !== -1 ||
+        //                     component[1].indexOf(">>") !== -1
+        //                 ) {
+        //                     textCheck = false;
+        //                     culpritSlideText.push(slide.slideSeq + 1);
+        //                     defaultText++;
+        //                 }
+        //                 break;
+        //             case "bullets":
+        //                 _.each(component[1], li => {
+        //                     /* istanbul ignore next - Implicit check */
+        //                     if (
+        //                         li.indexOf("<<") !== -1 ||
+        //                         li.indexOf(">>") !== -1
+        //                     ) {
+        //                         textCheck = false;
+        //                         culpritSlideText.push(slide.slideSeq + 1);
+        //                         defaultText++;
+        //                     }
+        //                 });
+        //                 break;
+        //             case "imagelist":
+        //             case "radiolist":
+        //             case "checklist":
+        //                 _.each(component[1], li => {
+        //                     /* istanbul ignore next - Implicit check */
+        //                     if (
+        //                         li.text.indexOf("<<") !== -1 ||
+        //                         li.text.indexOf(">>") !== -1
+        //                     ) {
+        //                         textCheck = false;
+        //                         culpritSlideText.push(slide.slideSeq + 1);
+        //                         defaultText++;
+        //                     }
+        //                 });
+        //                 break;
+        //             default:
+        //         }
+        //     });
+        // });
+        return {
+            status: true,
+            // errorCount: defaultText,
+            // culprit: _.uniq(culpritSlideText)
+        };
+    };
 
-    // getImagesCheck = () => {
-    //     let imagesCheck = true;
-    //     let culpritSlideImage = [];
-    //     let defaultImages = 0;
-    //     _.each(content, slide => {
-    //         _.each(_.toPairs(slide.data), component => {
-    //             switch (ComponentInputs[component[0]].type) {
-    //                 case "image":
-    //                     /* istanbul ignore next - Implicit check */
-    //                     if (
-    //                         ComponentInputs[component[0]].name !==
-    //                         "Presenter Character" &&
-    //                         (component[1] === "default11" ||
-    //                             component[1] === "default43")
-    //                     ) {
-    //                         imagesCheck = false;
-    //                         culpritSlideImage.push(slide.slideSeq + 1);
-    //                         defaultImages++;
-    //                     }
-    //                     break;
-    //                 case "imagelist":
-    //                 case "optionimages":
-    //                     _.each(component[1], li => {
-    //                         /* istanbul ignore next - Implicit check */
-    //                         if (
-    //                             li.image === "default11" ||
-    //                             li.image === "default43"
-    //                         ) {
-    //                             imagesCheck = false;
-    //                             culpritSlideImage.push(slide.slideSeq + 1);
-    //                             defaultImages++;
-    //                         }
-    //                     });
-    //                     break;
-    //                 default:
-    //             }
-    //         });
-    //     });
-    //     return {
-    //         status: imagesCheck,
-    //         errorCount: defaultImages,
-    //         culprit: _.uniq(culpritSlideImage)
-    //     };
-    // };
+    let getImagesCheck = () => {
+        // let imagesCheck = true;
+        // let culpritSlideImage = [];
+        // let defaultImages = 0;
+        // _.each(content, slide => {
+        //     _.each(_.toPairs(slide.data), component => {
+        //         switch (ComponentInputs[component[0]].type) {
+        //             case "image":
+        //                 /* istanbul ignore next - Implicit check */
+        //                 if (
+        //                     ComponentInputs[component[0]].name !==
+        //                     "Presenter Character" &&
+        //                     (component[1] === "default11" ||
+        //                         component[1] === "default43")
+        //                 ) {
+        //                     imagesCheck = false;
+        //                     culpritSlideImage.push(slide.slideSeq + 1);
+        //                     defaultImages++;
+        //                 }
+        //                 break;
+        //             case "imagelist":
+        //             case "optionimages":
+        //                 _.each(component[1], li => {
+        //                     /* istanbul ignore next - Implicit check */
+        //                     if (
+        //                         li.image === "default11" ||
+        //                         li.image === "default43"
+        //                     ) {
+        //                         imagesCheck = false;
+        //                         culpritSlideImage.push(slide.slideSeq + 1);
+        //                         defaultImages++;
+        //                     }
+        //                 });
+        //                 break;
+        //             default:
+        //         }
+        //     });
+        // });
+        return {
+            status: true,
+            // errorCount: defaultImages,
+            // culprit: _.uniq(culpritSlideImage)
+        };
+    };
 
-    // getVideoCheck = () => {
-    //     let videoCheck = true;
-    //     let culpritSlideVideo = [];
-    //     let defaultVideos = 0;
-    //     _.each(content, slide => {
-    //         _.each(_.toPairs(slide.data), component => {
-    //             if (component[0] === "video")
-    //                 if (component[1].indexOf("NpEaa2P7qZI") !== -1) {
-    //                     videoCheck = false;
-    //                     culpritSlideVideo.push(slide.slideSeq + 1);
-    //                     defaultVideos++;
-    //                 }
-    //         });
-    //     });
-    //     return {
-    //         status: videoCheck,
-    //         errorCount: defaultVideos,
-    //         culprit: _.uniq(culpritSlideVideo)
-    //     };
-    // };
+    let getVideoCheck = () => {
+        // let videoCheck = true;
+        // let culpritSlideVideo = [];
+        // let defaultVideos = 0;
+        // _.each(content, slide => {
+        //     _.each(_.toPairs(slide.data), component => {
+        //         if (component[0] === "video")
+        //             if (component[1].indexOf("NpEaa2P7qZI") !== -1) {
+        //                 videoCheck = false;
+        //                 culpritSlideVideo.push(slide.slideSeq + 1);
+        //                 defaultVideos++;
+        //             }
+        //     });
+        // });
+        return {
+            status: true,
+            // errorCount: defaultVideos,
+            // culprit: _.uniq(culpritSlideVideo)
+        };
+    };
 
-    // getCommentsCheck = () => {
-    //     let commentsCheck = true;
-    //     let culpritSlideComments = [];
-    //     let openComments = 0;
-    //     _.each(content, slide => {
-    //         _.each(slide.comments, comment => {
-    //             if (!comment.status) {
-    //                 commentsCheck = false;
-    //                 culpritSlideComments.push(slide.slideSeq + 1);
-    //                 openComments++;
-    //             }
-    //         });
-    //     });
-    //     return {
-    //         status: commentsCheck,
-    //         errorCount: openComments,
-    //         culprit: _.uniq(culpritSlideComments)
-    //     };
-    // };
+    let getCommentsCheck = () => {
+        // let commentsCheck = true;
+        // let culpritSlideComments = [];
+        // let openComments = 0;
+        // _.each(content, slide => {
+        //     _.each(slide.comments, comment => {
+        //         if (!comment.status) {
+        //             commentsCheck = false;
+        //             culpritSlideComments.push(slide.slideSeq + 1);
+        //             openComments++;
+        //         }
+        //     });
+        // });
+        return {
+            status: true,
+            // errorCount: openComments,
+            // culprit: _.uniq(culpritSlideComments)
+        };
+    };
 
-    // getVoCheck = () => {
-    //     let voCheck = true;
-    //     let culpritSlideVo = [];
-    //     let missingVo = 0;
-    //     if (voEnabled)
-    //         _.each(content, slide => {
-    //             if (slide.voiceover === null) {
-    //                 voCheck = false;
-    //                 culpritSlideVo.push(slide.slideSeq + 1);
-    //                 missingVo++;
-    //             }
-    //         });
-    //     return {
-    //         status: voCheck,
-    //         errorCount: missingVo,
-    //         culprit: _.uniq(culpritSlideVo)
-    //     };
-    // };
+    let getVoCheck = () => {
+        // let voCheck = true;
+        // let culpritSlideVo = [];
+        // let missingVo = 0;
+        // if (voEnabled)
+        //     _.each(content, slide => {
+        //         if (slide.voiceover === null) {
+        //             voCheck = false;
+        //             culpritSlideVo.push(slide.slideSeq + 1);
+        //             missingVo++;
+        //         }
+        //     });
+        return {
+            status: true,
+            //     errorCount: missingVo,
+            //     culprit: _.uniq(culpritSlideVo)
+        };
+    };
 
-    // getVisualCheck = () => {
-    //     let visualTotal = 0;
-    //     let visualTemplates = _.map(
-    //         _.filter(TemplateList, tmpl => {
-    //             return tmpl.audit.visual === true;
-    //         }),
-    //         "key"
-    //     );
-    //     _.each(content, slide => {
-    //         if (visualTemplates.indexOf(slide.template) !== -1) visualTotal++;
-    //     });
-    //     let visualScore = Math.round(
-    //         (100 * visualTotal) / content.length
-    //     );
-    //     let visualCheck = visualScore > 30;
+    let getVisualCheck = () => {
+        // let visualTotal = 0;
+        // let visualTemplates = _.map(
+        //     _.filter(TemplateList, tmpl => {
+        //         return tmpl.audit.visual === true;
+        //     }),
+        //     "key"
+        // );
+        // _.each(content, slide => {
+        //     if (visualTemplates.indexOf(slide.template) !== -1) visualTotal++;
+        // });
+        // let visualScore = Math.round(
+        //     (100 * visualTotal) / content.length
+        // );
+        // let visualCheck = visualScore > 30;
 
-    //     return {
-    //         status: visualCheck,
-    //         score: visualScore
-    //     };
-    // };
+        return {
+            status: true,
+            // score: visualScore
+        };
+    };
 
-    // getInteractiveCheck = () => {
-    //     let interactiveTotal = 0;
-    //     let interactiveTemplates = _.map(
-    //         _.filter(TemplateList, tmpl => {
-    //             return tmpl.audit.interactive === true;
-    //         }),
-    //         "key"
-    //     );
-    //     _.each(content, slide => {
-    //         if (interactiveTemplates.indexOf(slide.template) !== -1)
-    //             interactiveTotal++;
-    //     });
-    //     let interactiveScore = Math.round(
-    //         (100 * interactiveTotal) / content.length
-    //     );
-    //     let interactiveCheck = interactiveScore > 30;
+    let getInteractiveCheck = () => {
+        // let interactiveTotal = 0;
+        // let interactiveTemplates = _.map(
+        //     _.filter(TemplateList, tmpl => {
+        //         return tmpl.audit.interactive === true;
+        //     }),
+        //     "key"
+        // );
+        // _.each(content, slide => {
+        //     if (interactiveTemplates.indexOf(slide.template) !== -1)
+        //         interactiveTotal++;
+        // });
+        // let interactiveScore = Math.round(
+        //     (100 * interactiveTotal) / content.length
+        // );
+        // let interactiveCheck = interactiveScore > 30;
 
-    //     return {
-    //         status: interactiveCheck,
-    //         score: interactiveScore
-    //     };
-    // };
+        return {
+            status: true,
+            // score: interactiveScore
+        };
+    };
 
-    // getVerbosityCheck = () => {
-    //     let verbosityTotal = 0;
-    //     let verboseTemplates = _.map(
-    //         _.filter(TemplateList, tmpl => {
-    //             return tmpl.audit.verbose === true;
-    //         }),
-    //         "key"
-    //     );
-    //     _.each(content, slide => {
-    //         if (verboseTemplates.indexOf(slide.template) !== -1)
-    //             verbosityTotal++;
-    //     });
-    //     let verbosityScore = Math.round(
-    //         (100 * verbosityTotal) / content.length
-    //     );
-    //     let verbosityCheck = verbosityScore < 30;
+    let getVerbosityCheck = () => {
+        // let verbosityTotal = 0;
+        // let verboseTemplates = _.map(
+        //     _.filter(TemplateList, tmpl => {
+        //         return tmpl.audit.verbose === true;
+        //     }),
+        //     "key"
+        // );
+        // _.each(content, slide => {
+        //     if (verboseTemplates.indexOf(slide.template) !== -1)
+        //         verbosityTotal++;
+        // });
+        // let verbosityScore = Math.round(
+        //     (100 * verbosityTotal) / content.length
+        // );
+        // let verbosityCheck = verbosityScore < 30;
 
-    //     return {
-    //         status: verbosityCheck,
-    //         score: verbosityScore
-    //     };
-    // };
+        return {
+            status: true,
+            //score: verbosityScore
+        };
+    };
 
-    // getAllCheck = () => {
-    //     let allCheck =
-    //         getSlidesCheck() &
-    //         getTextCheck().status &
-    //         getImagesCheck().status &
-    //         getVideoCheck().status &
-    //         getCommentsCheck().status &
-    //         getInteractiveCheck().status &
-    //         getVerbosityCheck().status &
-    //         getVisualCheck().status;
-    //     if (voEnabled)
-    //         allCheck = allCheck & getVoCheck().status;
-    //     return allCheck === 1;
-    // };
+    let getAllCheck = () => {
+        let allCheck =
+            //getSlidesCheck() &
+            getTextCheck().status &
+            getImagesCheck().status &
+            getVideoCheck().status &
+            getCommentsCheck().status &
+            getInteractiveCheck().status &
+            getVerbosityCheck().status &
+            getVisualCheck().status &
+            getVoCheck().status;
+        //     if (voEnabled)
+        //         allCheck = allCheck & getVoCheck().status;
+        return allCheck === 1;
+    };
 
     const [openUploadModal, setOpenModal] = useState(props.isOpen);
 
@@ -473,7 +474,7 @@ export default function DeckAnalysisModal(props) {
                     />
                 </div>
                 <div className="qui-deck-analysis-modal-close-icon-btn">
-                    <h2><i className="fa fa-times"></i>Status : Failed</h2>
+                    <h3><i className={getAllCheck() ? "fa fa-check" : "fa fa-times"}></i>Status : {getAllCheck() ? "OK" : "Failed"}</h3>
 
                     <Button
                         {...props}
