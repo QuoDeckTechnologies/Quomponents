19a0662fa401213ee62541af625c0ac3d2a93e63
Rahul Verma <rahul.verma@quodeck.com>
2022-05-19 19:26:06 +0530

    update

--- src/components/RewardCard/RewardCard.react.jsx
+++ src/components/RewardCard/RewardCard.react.jsx
@@ -1,7 +1,10 @@
-// Import npm packages
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { getQuommons } from "../../common/javascripts/helpers.js";
+import { motion } from "framer-motion";
+import {
+    getAnimation,
+    getQuommons,
+} from "../../common/javascripts/helpers";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../../common/stylesheets/common.css";
 import "./RewardCard.scss";
@@ -15,115 +18,220 @@ RewardCard.propTypes = {
     // Component Specific props
     //=======================================
     /**
-    Content props consist of all the data which are required for Nugget Card component
-    */
+     Content props consist of all the data which are required for RewardCard component
+   */
     content: PropTypes.shape({
         name: PropTypes.string,
         image: PropTypes.string,
+        soldImage: PropTypes.string,
         cost: PropTypes.number,
         stock: PropTypes.shape({
             left: PropTypes.number,
             total: PropTypes.number
         }),
     }),
-
+    /**
+    Use to set the state of RewardCard 
+    */
+    asEmphasis: PropTypes.oneOf(["default", "blankRewardCard", "soldOutRewardCard"]),
+    //=======================================
     // Quommon props
     //=======================================
     /**
-    Use to set Colors for accent line
+    Use to float the component in parent container
+    */
+    asFloated: PropTypes.oneOf(["left", "right", "none", "inline"]),
+    /**
+    Use to set Color in RewardCard component
     */
     withColor: PropTypes.shape({
         textColor: PropTypes.string,
         accentColor: PropTypes.string,
         backgroundColor: PropTypes.string,
+        soldTextColor: PropTypes.string,
+        soldAccentColor: PropTypes.string,
+        soldBackgroundColor: PropTypes.string,
     }),
     /**
-    Use to float the component in parent container
+    Use to define the entry animation of the component
     */
-    asFloated: PropTypes.oneOf(["left", "right", "inline", "none"]),
+    withAnimation: PropTypes.shape({
+        animation: PropTypes.oneOf([
+            "zoom",
+            "collapse",
+            "fade",
+            "slideDown",
+            "slideUp",
+            "slideLeft",
+            "slideRight",
+            "",
+        ]),
+        duration: PropTypes.number,
+        delay: PropTypes.number,
+    }),
     /**
     Use to show/hide the component
     */
     isHidden: PropTypes.bool,
+    /**
+    RewardCard component must have the onClick function passed as props
+    */
+    onClick: PropTypes.func.isRequired,
 };
 
 RewardCard.defaultProps = {
+    //=======================================
     // Component Specific props
     //=======================================
     content: {},
-
+    asEmphasis: "default",
+    //=======================================
     // Quommon props
     //=======================================
-    asFloated: "inline",
+    asFloated: "none",
+
+    withColor: null,
+    withAnimation: null,
+
     isHidden: false,
 };
-
 /**
 ## Notes
+- The design system used for this component is HTML and CSS
+- The animation system used for this component is Framer Motion (framer-motion)
 - Pass inline styles to the component to override any of the component css
 - Or add custom css in overrule.scss to override the component css
 - Pass all the required props to the component.
 **/
 export default function RewardCard(props) {
     //-------------------------------------------------------------------
+    // 1. Destructuring props
+    //-------------------------------------------------------------------
+    let { content, withColor, asEmphasis } = props;
+    //-------------------------------------------------------------------
     // 1. Set the classes
     //-------------------------------------------------------------------
     let quommonClasses = getQuommons(props, "reward-card");
-
+    //-------------------------------------------------------------------
+    // 2. Set the classes
+    //-------------------------------------------------------------------
     const [stockStyle, setStockStyle] = useState('qui-reward-card-cost-stock-container-row')
 
     useEffect(() => {
-        let cost = props.content?.cost?.toString();
-        let left = props.content?.stock?.left?.toString();
-        let total = props.content?.stock?.total?.toString();
+        let cost = content?.cost?.toString();
+        let left = content?.stock?.left?.toString();
+        let total = content?.stock?.total?.toString();
 
         cost?.length > 5 || left?.length > 6 || total?.length > 6 ? setStockStyle("qui-reward-card-cost-stock-container-column") : setStockStyle("qui-reward-card-cost-stock-container-row");
-    }, [props.content?.cost, props.content?.stock?.left, props.content?.stock?.total]);
+    }, [content?.cost, content?.stock?.left, content?.stock?.total]);
 
-    let backgroundImage = props.content?.image === "" ? defaultImage : props.content?.image;
+    let backgroundImage = content?.image === "" ? defaultImage : content?.image;
+    let soldImage = content?.soldImage === "" ? defaultImage : content?.soldImage;
     //-------------------------------------------------------------------
-    // 9. Get the RewardCard Component
+    // 2. Use to the state of RewardCard Component
     //-------------------------------------------------------------------
-    const rewardCard = () => {
-        return (
-            <div>
-                {
-                    props.content && <div className={`qui-reward-card-container`} style={{ color: props.withColor?.textColor, backgroundColor: props.withColor?.backgroundColor }}>
-                        {props.content?.name &&
-                            <div className={`qui-reward-card-name`}>
-                                {props.content?.name}
-                            </div>}
-                        {backgroundImage &&
-                            <div className={`qui-reward-card-image`} style={{ backgroundImage: `url(${backgroundImage})` }}>
-                            </div>}
-                        <div className={`qui-reward-card-cost-stock-container ${stockStyle}`}>
-                            {props.content?.cost &&
-                                <div className={`qui-reward-card-cost`}>
-                                    <Reward content={{ point: props.content?.cost?.toString() }} withColor={{ accentColor: props.withColor?.textColor }} />
-                                </div>
-                            }
-                            {props.content?.stock &&
-                                <div className={`qui-reward-card-stock`}>
-                                    <div>
-                                        {props.content?.stock?.left} / {props.content?.stock?.total}
-                                    </div>
-                                    <div>
-                                        left
-                                    </div>
-                                </div>
-                            }
-                        </div>
-                        <div className={`qui-reward-card-accent-line`}>
-                            <AccentLine withColor={{ accentColor: props.withColor?.accentColor }} />
-                        </div>
+    const getRewardCard = (asEmphasis) => {
+        if (asEmphasis === "blankRewardCard") {
+            return (
+                <div
+                    className={`qui-reward-card-container`}
+                    style={{ color: withColor?.textColor, backgroundColor: withColor?.backgroundColor }}
+                >
+                    <div>
+                        <i className="fas fa-plus-square qui-blank-reward-card-icon" onClick={props.onClick} />
                     </div>
-                }
-            </div>
-        );
-    };
+                </div >
+            )
+        }
+        if (asEmphasis === "soldOutRewardCard") {
+            return (
+                <div>
+                    {
+                        content && <div className={`qui-reward-card-container`} style={{ color: withColor?.soldTextColor, backgroundColor: withColor?.soldBackgroundColor }}>
+                            {content?.name &&
+                                <div className={`qui-reward-card-name`}>
+                                    {content?.name}
+                                </div>}
+                            {backgroundImage &&
+                                <div className={`qui-reward-card-image`} style={{ backgroundImage: `url(${soldImage})` }}>
+                                </div>}
+                            <div className={`qui-reward-card-cost-stock-container ${stockStyle}`}>
+                                {content?.cost &&
+                                    <div className={`qui-reward-card-cost`}>
+                                        <Reward content={{ point: content?.cost?.toString() }} withColor={{ accentColor: withColor?.soldTextColor }} />
+                                    </div>
+                                }
+                                {content?.stock &&
+                                    <div className={`qui-reward-card-stock`}>
+                                        <div>
+                                            {content?.stock?.left} / {content?.stock?.total}
+                                        </div>
+                                        <div>
+                                            left
+                                        </div>
+                                    </div>
+                                }
+                            </div>
+                            <div className={`qui-reward-card-accent-line`}>
+                                <AccentLine withColor={{ accentColor: withColor?.soldAccentColor }} />
+                            </div>
+                        </div>
+                    }
+                </div>
+            )
+        }
+        else {
+            return (
+                <div>
+                    {
+                        content && <div className={`qui-reward-card-container`} style={{ color: withColor?.textColor, backgroundColor: withColor?.backgroundColor }}>
+                            {content?.name &&
+                                <div className={`qui-reward-card-name`}>
+                                    {content?.name}
+                                </div>}
+                            {backgroundImage &&
+                                <div className={`qui-reward-card-image`} style={{ backgroundImage: `url(${backgroundImage})` }}>
+                                </div>}
+                            <div className={`qui-reward-card-cost-stock-container ${stockStyle}`}>
+                                {content?.cost &&
+                                    <div className={`qui-reward-card-cost`}>
+                                        <Reward content={{ point: content?.cost?.toString() }} withColor={{ accentColor: withColor?.textColor }} />
+                                    </div>
+                                }
+                                {content?.stock &&
+                                    <div className={`qui-reward-card-stock`}>
+                                        <div>
+                                            {content?.stock?.left} / {content?.stock?.total}
+                                        </div>
+                                        <div>
+                                            left
+                                        </div>
+                                    </div>
+                                }
+                            </div>
+                            <div className={`qui-reward-card-accent-line`}>
+                                <AccentLine withColor={{ accentColor: withColor?.accentColor }} />
+                            </div>
+                        </div>
+                    }
+                </div>
+            )
+        }
+    }
+    //-------------------------------------------------------------------
+    // 4. Get animation of the component
+    //-------------------------------------------------------------------
+    const animate = getAnimation(props.withAnimation);
+    // ========================= Render Function =================================
     return (
-        <div className={`qui ${quommonClasses.parentClasses}`}>
-            <div className={`${quommonClasses.childClasses}`}>{rewardCard()}</div>
-        </div>
+        <motion.div
+            initial={animate.from}
+            animate={animate.to}
+            className={`qui ${quommonClasses.parentClasses}`}
+        >
+            <div className={`${quommonClasses.childClasses}`}>
+                {getRewardCard(asEmphasis)}
+            </div>
+        </motion.div>
     );
 }
---
+++ src/components/__tests__/RewardCard.test.jsx
@@ -0,0 +1,141 @@
+import React from 'react';
+//--------------------------------------
+// Import from NPM
+// -------------------------------------
+import { shallow, mount, render } from 'enzyme';
+//--------------------------------------
+// Import Components
+// -------------------------------------
+import RewardCard from "../RewardCard/RewardCard.react"
+
+describe("RewardCard", () => {
+    // -------------------------------------
+    // Setup definitions for the test suite
+    // -------------------------------------
+    let component;
+    let onClick = jest.fn();
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+        component = mount(
+            <RewardCard
+                content={{
+                    name: "",
+                    image: "",
+                    soldImage: "",
+                    cost: 0,
+                    stock: {},
+                }}
+                asEmphasis="default"
+                asFloated="none"
+                withColor={null}
+                withAnimation={null}
+                isHidden={false}
+                onClick={onClick}
+            />
+        );
+    });
+
+    it("should render correctly without throwing error", () => {
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed content prop with values", () => {
+        let value = {
+            name: "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeATjmpNd-h_Ks3g4SsBtHhLZ5F3FURym4w7KBqmteMxBmPRLX6oFwH2g1CRT_ckAzzFw&usqp=CAU",
+            soldImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeATjmpNd-h_Ks3g4SsBtHhLZ5F3FURym4w7KBqmteMxBmPRLX6oFwH2g1CRT_ckAzzFw&usqp=CAU",
+            cost: 1000000,
+            stock: {
+                left: 1000000,
+                total: 1000000
+            }
+        }
+        component.setProps({ content: value })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed content prop with values if asEmphasis as soldOutRewardCard", () => {
+        component.setProps({ asEmphasis: "soldOutRewardCard" })
+        let value = {
+            name: "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeATjmpNd-h_Ks3g4SsBtHhLZ5F3FURym4w7KBqmteMxBmPRLX6oFwH2g1CRT_ckAzzFw&usqp=CAU",
+            soldImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeATjmpNd-h_Ks3g4SsBtHhLZ5F3FURym4w7KBqmteMxBmPRLX6oFwH2g1CRT_ckAzzFw&usqp=CAU",
+            cost: 1000000,
+            stock: {
+                left: 1000000,
+                total: 1000000
+            }
+        }
+        component.setProps({ content: value })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed asEmphasis prop as default", () => {
+        component.setProps({ asEmphasis: "default" })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed asEmphasis prop as blankRewardCard", () => {
+        component.setProps({ asEmphasis: "blankRewardCard" })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed asEmphasis prop as soldOutRewardCard", () => {
+        component.setProps({ asEmphasis: "soldOutRewardCard" })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed asFloated prop as left", () => {
+        component.setProps({ asFloated: "left" })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed asFloated prop as right", () => {
+        component.setProps({ asFloated: "right" })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed asFloated prop as inline", () => {
+        component.setProps({ asFloated: "inline" })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed asFloated prop as none", () => {
+        component.setProps({ asFloated: "none" })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed withColor props", () => {
+        let colors = {
+            textColor: "#FFFF",
+            accentColor: "#AD2929",
+            backgroundColor: "",
+            soldTextColor: "#fffff",
+            soldAccentColor: "#AD2929",
+            soldBackgroundColor: "#E8E8E8",
+        }
+        component.setProps({ withColor: colors })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed withAnimation props", () => {
+        let animation = {
+            animation: "zoom",
+            duration: 0.5,
+            delay: 0,
+        }
+        component.setProps({ withAnimation: animation })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed isHidden props as false", () => {
+        component.setProps({ isHidden: false })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed isHidden props as true", () => {
+        component.setProps({ isHidden: true })
+        expect(component.exists()).toBe(true);
+    });
+});
