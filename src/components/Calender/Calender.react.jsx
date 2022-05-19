f7984bff3933da5c1007d712d7978937fec9a4fa
ganesh123 <ganesh.devkar@quodeck.com>
2022-05-19 16:15:17 +0530

    initial commit

---
+++ src/components/Calender/Calender.react.jsx
@@ -0,0 +1,150 @@
+// Import npm packages
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { motion } from "framer-motion";
+import Calendar from 'react-calendar';
+import {
+    getAnimation,
+    getQuommons,
+} from "../../common/javascripts/helpers";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "../../common/stylesheets/common.css";
+import "./Calender.scss";
+import "../../common/stylesheets/overrule.scss";
+
+Calender.propTypes = {
+    //=======================================
+    // Quommon props
+    //=======================================
+    /**
+      Use to define standard component type
+      */
+    asVariant: PropTypes.oneOf([
+        "primary",
+        "secondary",
+        "success",
+        "warning",
+        "error",
+    ]),
+    /**
+      Use to define component size in increasing order
+      */
+    asSize: PropTypes.oneOf([
+        "tiny",
+        "small",
+        "normal",
+        "big",
+        "huge",
+        "massive",
+    ]),
+    /**
+      Use to override component colors and behavior
+      */
+    withColor: PropTypes.shape({
+        backgroundColor: PropTypes.string,
+        accentColor: PropTypes.string,
+        lineColor: PropTypes.string,
+        textColor: PropTypes.string,
+    }),
+    /**
+      Use to define the entry animation of the component
+      */
+    withAnimation: PropTypes.shape({
+        animation: PropTypes.oneOf([
+            "zoom",
+            "collapse",
+            "fade",
+            "slideDown",
+            "slideUp",
+            "slideLeft",
+            "slideRight",
+            "",
+        ]),
+        duration: PropTypes.number,
+        delay: PropTypes.number,
+    }),
+    /**
+      Use to enable/disable the component
+      */
+    isDisabled: PropTypes.bool,
+    /**
+      Use to show/hide the component
+      */
+    isHidden: PropTypes.bool,
+};
+
+Calender.defaultProps = {
+    //=======================================
+    // Component Specific props
+    //=======================================
+    content: {},
+    //=======================================
+    // Quommon props
+    //=======================================
+    asVariant: "primary",
+    asSize: "normal",
+    withColor: null,
+    withAnimation: null,
+    isDisabled: false,
+    isHidden: false,
+};
+
+function getColors(colors) {
+    let colorStyle = {
+        textColors: {
+            color: colors.textColor,
+        },
+        accentColors: {
+            backgroundColor: colors.accentColor,
+        },
+        lineColors: {
+            backgroundColor: colors.lineColor,
+        },
+        backColors: {
+            backgroundColor: colors.backgroundColor,
+        },
+    };
+    return colorStyle;
+}
+/**
+## Notes
+- The design system used for this component is Fontawesome Icon
+- The animation system used for this component is Framer Motion (framer-motion)
+- Pass inline styles to the component to override any of the component css
+- Or add custom css in overrule.scss to override the component css
+**/
+export default function Calender(props) {
+    const [currentStep, updateCurrentStep] = useState(1);
+    //-------------------------------------------------------------------
+    // 1. Destructuring content from props
+    //-------------------------------------------------------------------
+    let { content } = props;
+    //-------------------------------------------------------------------
+    // 2. Set the classes
+    //-------------------------------------------------------------------
+    let quommonClasses = getQuommons(props, "calender");
+    quommonClasses.childClasses += ` variant-${props.asVariant}-text`;
+    //-------------------------------------------------------------------
+    // 3. Set the component colors
+    //-------------------------------------------------------------------
+    let colors = props.withColor ? getColors(props.withColor) : {};
+    //-------------------------------------------------------------------
+    // 4. Get animation of the component
+    //-------------------------------------------------------------------
+    const animate = getAnimation(props.withAnimation);
+
+    // ========================= Render Function =================================
+    return (
+        <motion.div
+            initial={animate.from}
+            animate={animate.to}
+            className={`qui ${quommonClasses.parentClasses}`}
+            style={colors?.backColors}
+        >
+            <div className={`qui-calender-container ${quommonClasses.childClasses}`}>
+                <Calendar />
+
+            </div>
+        </motion.div>
+    );
+}
