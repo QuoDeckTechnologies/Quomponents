8b7c3858a420da6711c59e424f7a2f30d5ccb984
Rahul Verma <rahul.verma@quodeck.com>
2022-05-20 17:27:59 +0530

    Added OPT Field with test file

---
+++ src/components/OTPFields/OTPFields.react.jsx
@@ -0,0 +1,177 @@
+import React from "react";
+import PropTypes from "prop-types";
+import InputField from "../InputField/InputField.react"
+import { motion } from "framer-motion";
+import {
+    getAnimation,
+    getQuommons,
+} from "../../common/javascripts/helpers";
+import "../../common/stylesheets/common.css";
+import "./OTPFields.scss";
+import "../../common/stylesheets/overrule.scss";
+
+OTPFields.propTypes = {
+    //=======================================
+    // Component Specific props
+    //=======================================
+    /**
+    Use to define name's ID
+    */
+    name: PropTypes.string.isRequired,
+    //=======================================
+    // Quommon props
+    //=======================================
+    /**
+    Use to float the component in parent container
+    */
+    asFloated: PropTypes.oneOf(["left", "right", "none", "inline"]),
+    /**
+    Use to set Colors for accent line
+    */
+    withColor: PropTypes.shape({
+        accentColor: PropTypes.string,
+        backgroundColor: PropTypes.string,
+    }),
+    /**
+    Use to define the entry animation of the component
+     */
+    withAnimation: PropTypes.shape({
+        animation: PropTypes.oneOf([
+            "zoom",
+            "collapse",
+            "fade",
+            "slideDown",
+            "slideUp",
+            "slideLeft",
+            "slideRight",
+            "",
+        ]),
+        duration: PropTypes.number,
+        delay: PropTypes.number,
+    }),
+    /**
+    Use to show/hide the component
+    */
+    isHidden: PropTypes.bool,
+    /**
+    Use to enable/disable the component
+    */
+    isDisabled: PropTypes.bool,
+    /**
+    OTPFields component must have the onClick function passed as props
+    */
+    onClick: PropTypes.func.isRequired,
+};
+
+OTPFields.defaultProps = {
+    //=======================================
+    // Quommon props
+    //=======================================
+    asFloated: "none",
+
+    withColor: null,
+    withAnimation: null,
+
+    isHidden: false,
+    isDisabled: false,
+
+    onClick: null,
+};
+/**
+## Notes
+- The design system used for this component is HTML and CSS
+- The animation system used for this component is Framer Motion (framer-motion)
+- Pass inline styles to the component to override any of the component css
+- Or add custom css in overrule.scss to override the component css
+**/
+export default function OTPFields(props) {
+    //-------------------------------------------------------------------
+    // 1. Set the classes
+    //-------------------------------------------------------------------
+    let quommonClasses = getQuommons(props, "otp-fields");
+    //-------------------------------------------------------------------
+    // 2. Use to set OTPFields Color
+    //-------------------------------------------------------------------
+    let OTPFieldsColors = {
+        backgroundColor: props.withColor?.backgroundColor,
+        accentColor: props.withColor?.accentColor,
+    };
+    //-------------------------------------------------------------------
+    // 3. Get animation of the component
+    //-------------------------------------------------------------------
+    const animate = getAnimation(props.withAnimation);
+    // ========================= Render Function =================================
+    return (
+        <motion.div
+            initial={animate.from}
+            animate={animate.to}
+            className={`qui ${quommonClasses.parentClasses}`}
+        >
+            <div className={`qui-otp-fields-container ${quommonClasses.childClasses}`}>
+                <div className="qui-otp-fields">
+                    <InputField {...props}
+                        InputProps={{ inputProps: { min: 0, max: 9 } }}
+                        name={"0"}
+                        content={{
+                            value: "1"
+                        }}
+                        asEmphasis="shortField"
+                        asFloated="left"
+                        withColor={OTPFieldsColors}
+                        onClick={(name, value) =>
+                            props.onClick(name, value)
+                        }
+                    />
+                    <InputField
+                        name={"1"}
+                        content={{
+                            value: "2"
+                        }}
+                        asEmphasis="shortField"
+                        asFloated="left"
+                        withColor={OTPFieldsColors}
+                        onClick={(name, value) =>
+                            props.onClick(name, value)
+                        }
+                    />
+                    <InputField
+                        name={"2"}
+                        content={{
+                            value: "3"
+                        }}
+                        asEmphasis="shortField"
+                        asFloated="left"
+                        withColor={OTPFieldsColors}
+                        onClick={(name, value) =>
+                            props.onClick(name, value)
+                        }
+                    />
+                    <InputField
+                        name={"4"}
+                        content={{
+                            value: "4"
+                        }}
+                        asEmphasis="shortField"
+                        asFloated="left"
+                        withColor={OTPFieldsColors}
+                        onClick={(name, value) =>
+                            props.onClick(name, value)
+                        }
+                    />
+                    <InputField
+                        name={"4"}
+                        content={{
+                            value: "5"
+                        }}
+                        asEmphasis="shortField"
+                        asFloated="left"
+                        withColor={OTPFieldsColors}
+                        onClick={(name, value) =>
+                            props.onClick(name, value)
+                        }
+                    />
+                </div>
+            </div>
+        </motion.div>
+    );
+}
---
+++ src/components/__tests__/OTPFields.test.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+//--------------------------------------
+// Import from NPM
+// -------------------------------------
+import { shallow, mount, render } from 'enzyme';
+//--------------------------------------
+// Import Components
+// -------------------------------------
+import OTPFields from "../OTPFields/OTPFields.react"
+
+describe("OTPFields", () => {
+    // -------------------------------------
+    // Setup definitions for the test suite
+    // -------------------------------------
+    let component;
+    let OnClick = jest.fn();
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+        component = shallow(
+            <OTPFields
+                asFloated="none"
+                withColor={null}
+                withAnimation={null}
+                isHidden={false}
+                isDisabled={false}
+                OnClick={OnClick}
+            />
+        );
+    });
+
+    it("should render correctly without throwing error", () => {
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed asFloated prop as left", () => {
+        component.setProps({ asFloated: "left" })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed asFloated prop as right", () => {
+        component.setProps({ asFloated: "right" })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed asFloated prop as inline", () => {
+        component.setProps({ asFloated: "inline" })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed asFloated prop as none", () => {
+        component.setProps({ asFloated: "none" })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed withColor props", () => {
+        let colors = {
+            accentColor: "#065254",
+            backgroundColor: "#34e5eb"
+        }
+        component.setProps({ withColor: colors })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed withAnimation props", () => {
+        let animation = {
+            animation: "zoom",
+            duration: 0.5,
+            delay: 0,
+        }
+        component.setProps({ withAnimation: animation })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed isHidden props as false", () => {
+        component.setProps({ isHidden: false })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed isHidden props as true", () => {
+        component.setProps({ isHidden: true })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed isDisabled props as false", () => {
+        component.setProps({ isDisabled: false })
+        expect(component.exists()).toBe(true);
+    });
+
+    it("should render correctly when passed isDisabled props as true", () => {
+        component.setProps({ isDisabled: true })
+        expect(component.exists()).toBe(true);
+    });
+});
