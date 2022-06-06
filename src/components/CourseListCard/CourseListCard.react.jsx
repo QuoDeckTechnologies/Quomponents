76a7265322a05ed517410b65a0cfe7b9c46240bf
Udhav <udhav.varma@quodeck.com>
2022-06-06 18:23:28 +0530

    component created

---
+++ src/components/CourseListCard/CourseListCard.react.jsx
@@ -0,0 +1,210 @@
+// Import npm packages
+import React from "react";
+import PropTypes from "prop-types";
+import { motion } from "framer-motion";
+import {
+  getQuommons,
+  getTranslation,
+  getAnimation,
+  resolveImage,
+} from "../../common/javascripts/helpers";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "../../common/stylesheets/common.css";
+import "./CourseListCard.scss";
+import "../../common/stylesheets/overrule.scss";
+
+CourseListCard.propTypes = {
+  //=======================================
+  // Component Specific props
+  //=======================================
+  /**
+  HCard component text has to be in content props.
+  */
+  content: PropTypes.object,
+  /**
+  H Card can set background image from imageLibrary array
+  */
+  imageLibrary: PropTypes.array,
+  //=======================================
+  // Quommon props
+  //=======================================
+  /**
+  Use to float the component in parent container
+  */
+  asFloated: PropTypes.oneOf(["left", "right", "none", "inline"]),
+  /**
+  Use to override component colors and behavior
+  */
+  withColor: PropTypes.shape({
+    backgroundColor: PropTypes.string,
+    accentColor: PropTypes.string,
+    accentBackgroundColor: PropTypes.string,
+    textColor: PropTypes.string,
+    buttonBackgroundColor: PropTypes.string,
+    buttonTextColor: PropTypes.string,
+  }),
+  /**
+  Use to define the entry animation of the component
+  */
+  withAnimation: PropTypes.shape({
+    animation: PropTypes.oneOf([
+      "zoom",
+      "collapse",
+      "fade",
+      "slideDown",
+      "slideUp",
+      "slideLeft",
+      "slideRight",
+      "",
+    ]),
+    duration: PropTypes.number,
+    delay: PropTypes.number,
+  }),
+  /**
+  Use to show a translated version of the component text. Dictionary must be valid JSON. 
+  */
+  withTranslation: PropTypes.shape({
+    lang: PropTypes.string,
+    tgt: PropTypes.string,
+    dictionary: PropTypes.string,
+  }),
+  /**
+  Use to show/hide the component
+  */
+  isHidden: PropTypes.bool,
+  /**
+  Use to enable/disable the component
+  */
+  isDisabled: PropTypes.bool,
+  /**
+  CourseListCard component must have the onClick function passed as props
+  */
+  onClick: PropTypes.func.isRequired,
+};
+
+CourseListCard.defaultProps = {
+  //=======================================
+  // Component Specific props
+  //=======================================
+  content: null,
+  imageLibrary: [],
+  //=======================================
+  // Quommon props
+  //=======================================
+  asFloated: "none",
+  withColor: null,
+  withAnimation: null,
+  withTranslation: null,
+  isHidden: false,
+  isDisabled: false,
+};
+/**
+## Notes
+- The design system used for this component is Material UI (@mui/material)
+- The animation system used for this component is Framer Motion (framer-motion)
+- Pass inline styles to the component to override any of the component css
+- Or add custom css in overrule.scss to override the component css
+**/
+export default function CourseListCard(props) {
+  //-------------------------------------------------------------------
+  // 1. Destructuring props
+  //-------------------------------------------------------------------
+  const { content, withColor, imageLibrary, onClick } = props;
+  //-------------------------------------------------------------------
+  // 2. Function to handle click
+  //-------------------------------------------------------------------
+  const handleClick = () => {
+    if (window.innerWidth <= 481) {
+      onClick();
+    }
+  };
+  //-------------------------------------------------------------------
+  // 3. Set the classes
+  //-------------------------------------------------------------------
+  let quommonClasses = getQuommons(props, "course-list-card");
+  //-------------------------------------------------------------------
+  // 4. Get animation of the component
+  //-------------------------------------------------------------------
+  const animate = getAnimation(props.withAnimation);
+  //-------------------------------------------------------------------
+  // 5. Get translation of the component
+  //-------------------------------------------------------------------
+  let cardContent = {};
+  let tObj = getTranslation(props.withTranslation);
+  if (tObj) {
+    cardContent = { ...tObj };
+  } else {
+    cardContent = { ...content };
+  }
+  //-------------------------------------------------------------------
+  // 6. Function to set image of the card
+  //-------------------------------------------------------------------
+  const getBackground = () => {
+    if (content?.backgroundImage) {
+      return {
+        backgroundImage: `url(${resolveImage(
+          content?.backgroundImage.id,
+          imageLibrary
+        )})`,
+      };
+    }
+  };
+  const background = getBackground();
+
+  // ========================= Render Function =================================
+
+  return (
+    <motion.div
+      initial={animate.from}
+      animate={animate.to}
+      className={`qui ${quommonClasses.parentClasses}`}
+    >
+      <div
+        className="qui-course-list-card-container"
+        style={{
+          backgroundColor: withColor?.backgroundColor,
+          color: withColor?.textColor,
+        }}
+        onClick={handleClick}
+      >
+        <div
+          className="qui-course-list-card-image-container"
+          style={{
+            ...background,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+        >
+          <div
+            className="qui-course-list-card-checkbox-container"
+            style={{ backgroundColor: withColor?.accentBackgroundColor }}
+          >
+            <i
+              className={
+                content?.checked ? "fas fa-check-square" : "far fa-square"
+              }
+              style={{ color: withColor?.accentColor }}
+            ></i>
+          </div>
+        </div>
+        <div className="qui-course-list-card-text-container">
+          <div className="qui-course-list-card-text">
+            <h4>{cardContent?.title}</h4>
+            <p>{cardContent?.description}</p>
+          </div>
+          {/* <Button
+            {...props}
+            content={cardContent?.buttonText}
+            isFluid={false}
+            withTranslation={null}
+            withColor={{
+              backgroundColor: withColor?.buttonBackgroundColor,
+              textColor: withColor?.buttonTextColor,
+            }}
+          /> */}
+        </div>
+      </div>
+    </motion.div>
+  );
+}
---
+++ src/stories/CourseListCard.stories.js
@@ -0,0 +1,158 @@
+import React from "react";
+import CourseListCard from "../components/CourseListCard/CourseListCard.react";
+
+const dictionary = JSON.stringify({
+  hi: {
+    courseListCard: {
+      title: "बेलून बर्स्ट",
+      description:
+        "सितारों को इकट्ठा करने के लिए उन गुब्बारों को पॉप करें और इसे करने के लिए अधिक समय प्राप्त करने के लिए सवालों के जवाब दें।",
+      buttonText: "प्रयत्न करें",
+    },
+  },
+});
+
+export default {
+  title: "Design System/CourseListCard/CourseListCard",
+  component: CourseListCard,
+  argTypes: {
+    content: {},
+    asEmphasis: {
+      control: "select",
+      options: ["text", "outlined", "contained"],
+      table: {
+        category: "as-Flags",
+      },
+    },
+    isCircular: {
+      table: {
+        category: "is-Toggles",
+        defaultValue: false,
+      },
+    },
+    asVariant: {
+      control: "select",
+      options: ["primary", "secondary", "success", "warning", "error"],
+      table: {
+        category: "as-Flags",
+      },
+    },
+    asFloated: {
+      control: "select",
+      options: ["left", "right", "none", "inline"],
+      table: {
+        category: "as-Flags",
+      },
+    },
+    withColor: {
+      table: {
+        category: "with-Params",
+        defaultValue: {
+          backgroundColor: "",
+          accentColor: "",
+          accentBackgroundColor: "",
+          textColor: "",
+          buttonBackgroundColor: "",
+          buttonTextColor: "",
+        },
+      },
+    },
+    withAnimation: {
+      table: {
+        category: "with-Params",
+        defaultValue: {
+          animation: "",
+          duration: 0,
+          delay: 0,
+        },
+      },
+    },
+    withTranslation: {
+      table: {
+        category: "with-Params",
+        defaultValue: {
+          lang: "",
+          tgt: "",
+          dictionary: "",
+        },
+      },
+    },
+    isHidden: {
+      table: {
+        category: "is-Toggles",
+        defaultValue: false,
+      },
+    },
+    isDisabled: {
+      table: {
+        category: "is-Toggles",
+        defaultValue: false,
+      },
+    },
+    onClick: {
+      table: {
+        category: "Events",
+        defaultValue: null,
+      },
+    },
+  },
+  parameters: {
+    componentSubtitle:
+      "Displays a basic CourseListCard for general-purpose use",
+    a11y: { disable: true },
+  },
+};
+
+// -------------------------------------------------------------
+// Default
+// -------------------------------------------------------------
+const Template = (args) => <CourseListCard {...args} />;
+export const Default = Template.bind({});
+Default.args = {
+  content: {
+    title: "BALLOON BURST",
+    description:
+      "Pop those balloons to collect stars and answer questions to gain more time to do it in.",
+    buttonText: "try game",
+    checked: true,
+    backgroundImage: { id: "background-image", extention: "" },
+  },
+  imageLibrary: [
+    {
+      id: "background-image",
+      image:
+        "https://images.unsplash.com/photo-1653844124305-6606b561dee3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+  ],
+  asEmphasis: "contained",
+  isCircular: false,
+  asVariant: "warning",
+  asFloated: "none",
+  withColor: {
+    backgroundColor: "",
+    accentColor: "",
+    accentBackgroundColor: "",
+    textColor: "",
+    buttonBackgroundColor: "",
+    buttonTextColor: "",
+  },
+  withAnimation: {
+    animation: "zoom",
+    duration: 0.5,
+    delay: 0,
+  },
+  withTranslation: {
+    lang: "en",
+    tgt: "courseListCard",
+    dictionary: dictionary,
+  },
+  isDisabled: false,
+  isHidden: false,
+};
+Default.parameters = {
+  docs: {
+    source: {
+      code: `<CourseListCard {...${JSON.stringify(Default.args, null, 2)}}/>`,
+    },
+  },
+};
