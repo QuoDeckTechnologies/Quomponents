72f4f011a02e828bce18a2c47d211be471d9504b
Rohit-Dhende <rohit.dhende@quodeck.com>
2022-06-13 18:52:44 +0530

    Fixed NuggetBlock test file

--- src/components/NuggetBlock/NuggetBlock.react.jsx
+++ src/components/NuggetBlock/NuggetBlock.react.jsx
@@ -135,7 +135,7 @@ export default function NuggetBlock(props) {
             className={`qui ${quommonClasses.parentClasses}`}>
             <div className={`${quommonClasses.childClasses}`}>
                 <div className={`qui-nugget-status`} style={{ backgroundColor: color, display: display }}></div>
-                <div style={{ backgroundImage: `url(${image})` }} alt="nuggetimage" className="qui-nugget-block-image" onMouseDown={() => { return props.onClick() }} />
+                <div style={{ backgroundImage: `url(${image})` }} alt="nuggetimage" className="qui-nugget-block-image" onClick={() => { return props.onClick() }} />
             </div>
         </motion.div>
     );
