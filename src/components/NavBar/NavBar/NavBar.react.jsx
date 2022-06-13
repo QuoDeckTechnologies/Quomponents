7d3d1df5df265788ca019b85821ed62956780511
Udhav <udhav.varma@quodeck.com>
2022-06-13 16:22:47 +0530

    Component created not done

--- src/components/NavBar/NavBar/NavBar.react.jsx
+++ src/components/NavBar/NavBar/NavBar.react.jsx
@@ -1,111 +1,110 @@
 // Import npm packages
 import React from "react";
 import PropTypes from "prop-types";
-import AppMenu from "../../AppMenu/AppMenu/AppMenu.react";
+import { motion } from "framer-motion";
+import {
+  getQuommons,
+  getTranslation,
+  getAnimation,
+} from "../../../common/javascripts/helpers";
+import _ from "lodash";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../../../common/stylesheets/common.css";
 import "./NavBar.scss";
 import "../../../common/stylesheets/overrule.scss";
-import { motion } from "framer-motion";
-import _ from "lodash";
 import IconLink from "../../Buttons/IconLink/IconLink.react";
-import { getQuommons, getTranslation, getAnimation, } from "../../../common/javascripts/helpers";
+import AppMenu from "../../AppMenu/AppMenu/AppMenu.react";
 
 NavBar.propTypes = {
-
-    //=======================================
-    // Component specific prop
-    //=======================================
-    /**
-    NavBar Text has to be in content or passed as children to the component.
-    */
-    content: PropTypes.shape({
-        title: PropTypes.string,
-        logoimg: PropTypes.string,
-        iconlink: PropTypes.arrayOf(
-            PropTypes.shape({
-                icon: PropTypes.string,
-                link: PropTypes.string,
-            })
-        ),
-    }).isRequired,
-
-    /**
-    Use to define component user image
-    */
-    withUser: PropTypes.string,
-
-    //=======================================
-    // Quommon props
-    //=======================================
-
-    /**
-    Use to define standard component type
-    */
-    asVariant: PropTypes.oneOf([
-        "primary",
-        "secondary",
-        "success",
-        "warning",
-        "error",
+  //=======================================
+  // Component specific prop
+  //=======================================
+  /**
+  NavBar Text has to be in content or passed as children to the component.
+  */
+  content: PropTypes.object,
+  /**
+  Use to show search icon in NavBar component.
+  */
+  isSearchLogo: PropTypes.bool,
+  /**
+  Use to show AppMenu Component if user is logged in.
+  */
+  isLoggedIn: PropTypes.bool,
+  /**
+  Use to define component user image
+  */
+  withUser: PropTypes.string,
+  //=======================================
+  // Quommon props
+  //=======================================
+  /**
+  Use to define standard component type
+  */
+  asVariant: PropTypes.oneOf([
+    "primary",
+    "secondary",
+    "success",
+    "warning",
+    "error",
+  ]),
+  /**
+  Use to add a heading label, a footer caption or a title popover to the component
+  */
+  withLabel: PropTypes.shape({
+    content: PropTypes.string,
+  }),
+  /**
+  Use to define the entry animation of the component
+  */
+  withAnimation: PropTypes.shape({
+    animation: PropTypes.oneOf([
+      "zoom",
+      "collapse",
+      "fade",
+      "slideDown",
+      "slideUp",
+      "slideLeft",
+      "slideRight",
+      "",
     ]),
-    /**
-    Use to add a heading label, a footer caption or a title popover to the component
-    */
-    withLabel: PropTypes.shape({
-        content: PropTypes.string,
-    }),
-    /**
-    Use to define the entry animation of the component
-    */
-    withAnimation: PropTypes.shape({
-        animation: PropTypes.oneOf([
-            "zoom",
-            "collapse",
-            "fade",
-            "slideDown",
-            "slideUp",
-            "slideLeft",
-            "slideRight",
-            ""
-        ]),
-        duration: PropTypes.number,
-        delay: PropTypes.number,
-    }),
-    /**
+    duration: PropTypes.number,
+    delay: PropTypes.number,
+  }),
+  /**
   Use to show a translated version of the component text. Dictionary must be valid JSON. 
   */
-    withTranslation: PropTypes.shape({
-        lang: PropTypes.string,
-        tgt: PropTypes.string,
-        dictionary: PropTypes.string,
-    }),
-    /**
-    Use to show/hide the component
-    */
-    isHidden: PropTypes.bool,
-    /**
-    Use to enable/disable the component
-    */
-    isDisabled: PropTypes.bool,
-    /**
-    NavBar component must have the onClick function passed as props
-    */
-    onClick: PropTypes.func.isRequired,
+  withTranslation: PropTypes.shape({
+    lang: PropTypes.string,
+    tgt: PropTypes.string,
+    dictionary: PropTypes.string,
+  }),
+  /**
+  Use to show/hide the component
+  */
+  isHidden: PropTypes.bool,
+  /**
+  Use to enable/disable the component
+  */
+  isDisabled: PropTypes.bool,
+  /**
+  NavBar component must have the onClick function passed as props
+  */
+  onClick: PropTypes.func.isRequired,
 };
 
 NavBar.defaultProps = {
-    //=======================================
-    // Component Specific props
-    //=======================================
-    content: {},
-    //=======================================
-    // Quommon props
-    //=======================================
-    asVariant: "primary",
-    isHidden: false,
-    isDisabled: false,
-    withTranslation: null,
+  //=======================================
+  // Component Specific props
+  //=======================================
+  content: {},
+  //=======================================
+  // Quommon props
+  //=======================================
+  asVariant: "primary",
+  isHidden: false,
+  isDisabled: false,
+  withTranslation: null,
 };
 /**
 ## Notes
@@ -114,65 +113,104 @@ NavBar.defaultProps = {
 - Or add custom css in overrule.scss to override the component css
 **/
 export default function NavBar(props) {
-    //-------------------------------------------------------------------
-    // 1. Destructuring content from props
-    //-------------------------------------------------------------------
-    let { content } = props;
-    // 2. Set the classes
-    //-------------------------------------------------------------------
-    let quommonClasses = getQuommons(props, "NavBar");
-    quommonClasses.childClasses += ` variant-${props.asVariant}-text`;
-    //-------------------------------------------------------------------
-    // 3. Translate the text objects in case their is a dictionary provided
-    //-------------------------------------------------------------------
-    let labelContent = {
-        title: content?.title,
-    };
-    let tObj = null;
-    if (
-        props.withTranslation?.lang &&
-        props.withTranslation.lang !== "" &&
-        props.withTranslation.lang !== "en"
-    ) {
-        tObj = getTranslation(props.withTranslation);
-        if (labelContent && tObj) {
-            labelContent.title = tObj.title;
-        }
+  //-------------------------------------------------------------------
+  // 1. Destructuring content from props
+  //-------------------------------------------------------------------
+  let { content, isSearchLogo, isLoggedIn } = props;
+  // 2. Set the classes
+  //-------------------------------------------------------------------
+  let quommonClasses = getQuommons(props, "NavBar");
+  quommonClasses.childClasses += ` variant-${props.asVariant}-text`;
+  //-------------------------------------------------------------------
+  // 3. Translate the text objects in case their is a dictionary provided
+  //-------------------------------------------------------------------
+  let labelContent = {
+    title: content?.title,
+  };
+  let tObj = null;
+  if (
+    props.withTranslation?.lang &&
+    props.withTranslation.lang !== "" &&
+    props.withTranslation.lang !== "en"
+  ) {
+    tObj = getTranslation(props.withTranslation);
+    if (labelContent && tObj) {
+      labelContent.title = tObj.title;
     }
-    //-------------------------------------------------------------------
-    // 4. Get animation of the component
-    //-------------------------------------------------------------------
-    const animate = getAnimation(props.withAnimation);
+  }
+  //-------------------------------------------------------------------
+  // 4. Get animation of the component
+  //-------------------------------------------------------------------
+  const animate = getAnimation(props.withAnimation);
 
-    // ========================= Render Function =================================
+  const getLogoImage = (title) => {
+    let image;
+    if (title) {
+      image = content?.shortLogo;
+    } else {
+      image = content?.fullLogo;
+    }
     return (
-        <motion.div
-            initial={animate.from}
-            animate={animate.to}
-            className={`qui ${quommonClasses.parentClasses}`}
-        >
-            <div className={`qui-navbar-container ${quommonClasses.childClasses}`} >
-                <div className="qui-left-navbar">
-                    {_.map(content?.iconlink, (icon, index) => {
-                        return (
-                            <IconLink
-                                {...props}
-                                key={index}
-                                content={{ link: icon.link }}
-                                withIcon={{ icon: icon.icon }}
-                            />
-                        );
-                    })}
-                    <img src={props.content?.logoimg} alt="Logo" className="logoimg" />
-                    <div className="qui-content">
-                        {labelContent?.title}
-                    </div>
-                </div>
-                <div className="qui-right-navbar" >
-                    <div className="qui-searching" onClick={props.onClick}><i class="fas fa-search"></i></div>
-                    <AppMenu {...props} withIcon={{ icon: 'fas fa-ellipsis-v' }} />
-                </div>
-            </div>
-        </motion.div>
+      <img
+        src={image}
+        className={`qui-logo-img ${
+          title ? "qui-short-logo-img" : "qui-full-logo-img"
+        }`}
+        alt="Logo"
+      />
     );
-}   
+  };
+
+  // ========================= Render Function =================================
+  return (
+    <motion.div
+      initial={animate.from}
+      animate={animate.to}
+      className={`qui ${quommonClasses.parentClasses}`}
+    >
+      <div className={`qui-navbar-container ${quommonClasses.childClasses}`}>
+        <div className="qui-left-navbar">
+          {_.map(content?.iconlink, (icon, index) => {
+            return (
+              <IconLink
+                {...props}
+                key={index}
+                content={{ link: icon.link }}
+                withIcon={{ icon: icon.icon }}
+                asPadded="fitted"
+              />
+            );
+          })}
+          {/* {getLogoImage(content?.title)} */}
+          <img src={content?.shortLogo} className="qui-logo-img" alt="Logo" />
+          {!content?.title && (
+            <img
+              src={content?.fullLogo}
+              className="qui-full-logo-img"
+              alt="Logo"
+            />
+          )}
+          <div className="qui-content">
+            <h4 className="qui-nav-bar-title">{labelContent?.title}</h4>
+          </div>
+        </div>
+        <div className="qui-right-navbar">
+          {isSearchLogo && (
+            <div className="qui-searching" onClick={props.onClick}>
+              <i className="fas fa-search"></i>
+            </div>
+          )}
+          {isLoggedIn ? (
+            <AppMenu {...props} withIcon={{ icon: "fas fa-ellipsis-v" }} />
+          ) : (
+            <div className="qui-nav-bar-menu-icon">
+              <div className="qui-nav-bar-menu-icon-element qui-nav-bar-menu-element-top"></div>
+              <div className="qui-nav-bar-menu-icon-element qui-nav-bar-menu-element-middle"></div>
+              <div className="qui-nav-bar-menu-icon-element qui-nav-bar-menu-element-bottom"></div>
+            </div>
+          )}
+        </div>
+      </div>
+    </motion.div>
+  );
+}
--- src/stories/NavBar.stories.js
+++ src/stories/NavBar.stories.js
@@ -1,279 +1,330 @@
 import React from "react";
 import NavBar from "../components/NavBar/NavBar/NavBar.react";
-import ShortLogo from "../assets/amplayfier-logo-short.png"
+import ShortLogo from "../assets/amplayfier-logo-short.png";
+import FullLogo from "../assets/FullLogo.png";
+
 const dictionary = JSON.stringify({
-    hi: {
-        navbar: {
-            title: "कमाये",
-        },
+  hi: {
+    navbar: {
+      title: "कमाये",
     },
+  },
 });
 
 export default {
-    title: "Design System/NavBar/NavBar",
-    component: NavBar,
-    argTypes: {
-        content: {
-            table: {
-                defaultValue: {
-                    title: "",
-                    logoimg: "",
-                    iconlink: [],
-                },
-            },
+  title: "Design System/NavBar/NavBar",
+  component: NavBar,
+  argTypes: {
+    content: {
+      table: {
+        defaultValue: {
+          title: "",
+          shortLogo: "",
+          fullLogo: "",
+          iconlink: [],
         },
-        withUser: {
-            table: {
-                category: "with-Params",
-                defaultValue: "",
-            },
-        },
-        asVariant: {
-            control: "select",
-            options: ["primary", "secondary", "success", "warning", "error"],
-            table: {
-                category: "as-Flags",
-            },
-        },
-        withAnimation: {
-            table: {
-                category: "with-Params",
-                defaultValue: {
-                    animation: "",
-                    duration: 0,
-                    delay: 0,
-                },
-            },
-        },
-        withTranslation: {
-            table: {
-                category: "with-Params",
-                defaultValue: {
-                    lang: "",
-                    tgt: "",
-                    dictionary: "",
-                },
-            },
-        },
-        isHidden: {
-            table: {
-                category: "is-Toggles",
-                defaultValue: false,
-            },
-        },
-        isDisabled: {
-            table: {
-                category: "is-Toggles",
-                defaultValue: false,
-            },
-        },
-        onClick: {
-            table: {
-                category: "Events",
-                defaultValue: null,
-            },
-        },
+      },
     },
-    decorators: [
-        (story) => (
-            <div
-                style={{
-                    width: "100%",
-                    textAlign: "center",
-                    fontSize: "1.25em",
-                }}
-            >
-                {story()}
-            </div>
-        ),
-    ],
-    parameters: {
-        componentSubtitle: "Displays NavBar with AppMenu, Icons and Image for general-purpose use",
-        a11y: { disable: true },
-        docs: { iframeHeight: 700 },
+    isSearchLogo: {
+      table: {
+        defaultValue: false,
+      },
     },
+    isLoggedIn: {
+      table: {
+        defaultValue: false,
+      },
+    },
+    withUser: {
+      table: {
+        category: "with-Params",
+        defaultValue: "",
+      },
+    },
+    asVariant: {
+      control: "select",
+      options: ["primary", "secondary", "success", "warning", "error"],
+      table: {
+        category: "as-Flags",
+      },
+    },
+    withAnimation: {
+      table: {
+        category: "with-Params",
+        defaultValue: {
+          animation: "",
+          duration: 0,
+          delay: 0,
+        },
+      },
+    },
+    withTranslation: {
+      table: {
+        category: "with-Params",
+        defaultValue: {
+          lang: "",
+          tgt: "",
+          dictionary: "",
+        },
+      },
+    },
+    isHidden: {
+      table: {
+        category: "is-Toggles",
+        defaultValue: false,
+      },
+    },
+    isDisabled: {
+      table: {
+        category: "is-Toggles",
+        defaultValue: false,
+      },
+    },
+    onClick: {
+      table: {
+        category: "Events",
+        defaultValue: null,
+      },
+    },
+  },
+  decorators: [
+    (story) => (
+      <div
+        style={{
+          width: "100%",
+          textAlign: "center",
+          fontSize: "1.25em",
+        }}
+      >
+        {story()}
+      </div>
+    ),
+  ],
+  parameters: {
+    componentSubtitle:
+      "Displays NavBar with AppMenu, Icons and Image for general-purpose use",
+    a11y: { disable: true },
+    docs: { iframeHeight: 700 },
+  },
 };
 
 const Template = (args) => <NavBar {...args} />;
 //----------------------------------------------------------
 // Default
-//---------------------------------------------------------
+//----------------------------------------------------------
 export const Default = Template.bind({});
 Default.args = {
-    content: {
-        title: "earn",
-        logoimg: ShortLogo,
-        iconlink: [{
-            icon: "fas fa-angle-left",
-            link: "https://www.google.com/",
-        }],
-    },
-    asVariant: "primary",
-    withUser: "",
-    withLabel: {
-        content: "Catalog",
-    },
-    withAnimation: {
-        animation: "zoom",
-        duration: 0.5,
-        delay: 0,
-    },
-    withTranslation: {
-        lang: "en",
-        tgt: "navbar",
-        dictionary: dictionary,
-    },
-    isDisabled: false,
-    isHidden: false,
+  content: {
+    title: "earn",
+    shortLogo: ShortLogo,
+    fullLogo: FullLogo,
+    iconlink: [
+      {
+        icon: "fas fa-angle-left",
+        link: "https://www.google.com/",
+      },
+    ],
+  },
+  isSearchLogo: true,
+  isLoggedIn: true,
+  withUser: "",
+  withLabel: {
+    content: "Catalog",
+  },
+  asVariant: "primary",
+  withAnimation: {
+    animation: "zoom",
+    duration: 0.5,
+    delay: 0,
+  },
+  withTranslation: {
+    lang: "en",
+    tgt: "navbar",
+    dictionary: dictionary,
+  },
+  isDisabled: false,
+  isHidden: false,
 };
 Default.parameters = {
-    docs: {
-        description: {
-            story:
-                "Any free fontawesome icon can be used as the NavBar icon definition."
-        },
-        source: {
-            code: `<NavBar {...${JSON.stringify(Default.args, null, 2)}}/>`,
-        },
+  docs: {
+    description: {
+      story:
+        "Any free fontawesome icon can be used as the NavBar icon definition.",
     },
+    source: {
+      code: `<NavBar {...${JSON.stringify(Default.args, null, 2)}}/>`,
+    },
+  },
 };
+//----------------------------------------------------------
 // With UserNavBar
-//---------------------------------------------------------
+//----------------------------------------------------------
 export const UserNavbar = Template.bind({});
 UserNavbar.args = {
-    content: {
-        title: "earn",
-        logoimg: ShortLogo,
-        iconlink: [{
-            icon: "fas fa-angle-left",
-            link: "https://www.google.com/",
-        }],
-    },
-    withUser: "https://dp-client.com/CMS-NEW/assets/images/user/user11605616227.png",
-    withLabel: {
-        content: "",
-    },
-    withAnimation: {
-        animation: "zoom",
-        duration: 0.5,
-        delay: 0,
-    },
-    isDisabled: false,
-    isHidden: false,
+  content: {
+    title: "earn",
+    shortLogo: ShortLogo,
+    fullLogo: FullLogo,
+    iconlink: [
+      {
+        icon: "fas fa-angle-left",
+        link: "https://www.google.com/",
+      },
+    ],
+  },
+  isSearchLogo: true,
+  isLoggedIn: true,
+  withUser:
+    "https://dp-client.com/CMS-NEW/assets/images/user/user11605616227.png",
+  withLabel: {
+    content: "",
+  },
+  withAnimation: {
+    animation: "zoom",
+    duration: 0.5,
+    delay: 0,
+  },
+  isDisabled: false,
+  isHidden: false,
 };
 UserNavbar.parameters = {
-    docs: {
-        description: {
-            story:
-                "Any image can be used as the NavBar image definition.",
-        },
-        source: {
-            code: `<NavBar {...${JSON.stringify(UserNavbar.args, null, 2)}}/>`,
-        },
+  docs: {
+    description: {
+      story: "Any image can be used as the NavBar image definition.",
     },
+    source: {
+      code: `<NavBar {...${JSON.stringify(UserNavbar.args, null, 2)}}/>`,
+    },
+  },
+};
+//----------------------------------------------------------
+// With Amplayfier Header
+//----------------------------------------------------------
+export const AmplayfierHeader = Template.bind({});
+AmplayfierHeader.args = {
+  ...Default.args,
+  content: {
+    shortLogo: ShortLogo,
+    fullLogo: FullLogo,
+    iconlink: [
+      {
+        icon: "fas fa-angle-left",
+        link: "https://www.google.com/",
+      },
+    ],
+  },
+  isSearchLogo: false,
+  isLoggedIn: false,
+  isDisabled: false,
+  isHidden: false,
+};
+AmplayfierHeader.parameters = {
+  docs: {
+    description: {
+      story: "Use this to display Amplayfier Header component.",
+    },
+    source: {
+      code: `<NavBar {...${JSON.stringify(AmplayfierHeader.args, null, 2)}}/>`,
+    },
+  },
 };
 // -------------------------------------------------------------
 // AllVariants
 // -------------------------------------------------------------
 const AllVariantsTemplate = (args) => {
-    const baseObj = {
-        ...Object.assign({}, Default.args, args, {
-
-        }),
-    };
-    return (
-        <div>
-            <NavBar
-                {...Object.assign({}, baseObj, {
-                    asVariant: "primary",
-                })}
-            />
-            <NavBar
-                {...Object.assign({}, baseObj, {
-                    asVariant: "secondary",
-                })}
-            />
-            <NavBar
-                {...Object.assign({}, baseObj, {
-                    asVariant: "success",
-                })}
-            />
-            <NavBar
-                {...Object.assign({}, baseObj, {
-                    asVariant: "warning",
-                })}
-            />
-            <NavBar
-                {...Object.assign({}, baseObj, {
-                    asVariant: "error",
-                })}
-            />
-        </div>
-    );
+  const baseObj = {
+    ...Object.assign({}, Default.args, args, {}),
+  };
+  return (
+    <div>
+      <NavBar
+        {...Object.assign({}, baseObj, {
+          asVariant: "primary",
+        })}
+      />
+      <NavBar
+        {...Object.assign({}, baseObj, {
+          asVariant: "secondary",
+        })}
+      />
+      <NavBar
+        {...Object.assign({}, baseObj, {
+          asVariant: "success",
+        })}
+      />
+      <NavBar
+        {...Object.assign({}, baseObj, {
+          asVariant: "warning",
+        })}
+      />
+      <NavBar
+        {...Object.assign({}, baseObj, {
+          asVariant: "error",
+        })}
+      />
+    </div>
+  );
 };
 export const AllVariants = AllVariantsTemplate.bind({});
 AllVariants.parameters = {
-    docs: {
-        description: {
-            story: "All variants are supported. Use as per purpose noted here.",
-        },
-        source: {
-            code: `<NavBar asVariant="primary"/>`,
-        },
+  docs: {
+    description: {
+      story: "All variants are supported. Use as per purpose noted here.",
     },
-}
+    source: {
+      code: `<NavBar asVariant="primary"/>`,
+    },
+  },
+};
 
 //-------------------------------------------------------------
 // Animated Navbar
 // -------------------------------------------------------------
 export const AnimatedNavbar = Template.bind({});
 AnimatedNavbar.args = {
-    ...Default.args,
-    withAnimation: {
-        animation: "fade",
-        duration: 1,
-        delay: 0,
-    },
+  ...Default.args,
+  withAnimation: {
+    animation: "fade",
+    duration: 1,
+    delay: 0,
+  },
 };
 AnimatedNavbar.parameters = {
-    docs: {
-        description: {
-            story: "We can animate the appearance of Navbar",
-        },
-        source: {
-            code: `<NavbarDark {...${JSON.stringify(
-                AnimatedNavbar.args,
-                null,
-                2
-            )}}/>`,
-        },
+  docs: {
+    description: {
+      story: "We can animate the appearance of Navbar",
     },
+    source: {
+      code: `<NavbarDark {...${JSON.stringify(
+        AnimatedNavbar.args,
+        null,
+        2
+      )}}/>`,
+    },
+  },
 };
 // -------------------------------------------------------------
 // Translated NavBar
 // -------------------------------------------------------------
 export const TranslatedNavBar = Template.bind({});
 TranslatedNavBar.args = {
-    ...Default.args,
-    withTranslation: {
-        lang: "hi",
-        tgt: "navbar",
-        dictionary: dictionary,
-    },
+  ...Default.args,
+  withTranslation: {
+    lang: "hi",
+    tgt: "navbar",
+    dictionary: dictionary,
+  },
 };
 TranslatedNavBar.parameters = {
-    docs: {
-        description: {
-            story:
-                "We can translate the language of NavBar if dictionary is provided",
-        },
-        source: {
-            code: `<TranslatedNavBar {...${JSON.stringify(
-                TranslatedNavBar.args,
-                {
-                    navbar: { content: "कमाये" }
-                }
-            )}}/>`,
-        },
+  docs: {
+    description: {
+      story:
+        "We can translate the language of NavBar if dictionary is provided",
     },
+    source: {
+      code: `<TranslatedNavBar {...${JSON.stringify(TranslatedNavBar.args, {
+        navbar: { content: "कमाये" },
+      })}}/>`,
+    },
+  },
 };
