7129d1f9689c2ecdca845870e3b6e23953098126
Udhav <udhav.varma@quodeck.com>
2022-06-13 16:51:14 +0530

    checkbox test changes

--- src/components/__tests__/CheckBox.test.jsx
+++ src/components/__tests__/CheckBox.test.jsx
@@ -17,10 +17,8 @@ describe("CheckBox", () => {
     jest.resetAllMocks();
     component = shallow(
       <CheckBox
-        content={{ label: "Default Label", checked: false }}
-        onClick={(e) => {
-          console.log(e);
-        }}
+        content={{ name: "Default Label", checked: false }}
+        onClick={() => {}}
       />
     );
   });
@@ -50,22 +48,22 @@ describe("CheckBox", () => {
     expect(component.exists()).toBe(true);
   });
 
-  // it("should render correctly without throwing error", () => {
-  //   component
-  //     .find("#qui-check-box-element")
-  //     .at(0)
-  //     .simulate("change", {
-  //       target: { value: "Enable Checkbox", checked: true },
-  //     });
-  // });
+  it("should render correctly without throwing error", () => {
+    component.setProps({
+      content: {
+        name: "default-name",
+      },
+    });
+    component.find("#qui-check-box-element-default-name").simulate("change", {
+      target: { value: "Enable Checkbox", checked: true },
+    });
+  });
 
   it("should render correctly without throwing error", () => {
     let wrapper = mount(
       <CheckBox
-        content={{ label: "Default Label", checked: false }}
-        onClick={(e) => {
-          console.log(e);
-        }}
+        content={{ name: "Default Label", checked: false }}
+        onClick={() => {}}
       />
     );
     expect(wrapper.exists()).toBe(true);
