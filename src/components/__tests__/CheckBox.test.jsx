2f79a45b7c5a0d7c3f509a8e5460f41c6a3b075b
Udhav <udhav.varma@quodeck.com>
2022-06-13 18:35:47 +0530

    checkbox and upload testing

--- src/components/__tests__/CheckBox.test.jsx
+++ src/components/__tests__/CheckBox.test.jsx
@@ -48,6 +48,62 @@ describe("CheckBox", () => {
     expect(component.exists()).toBe(true);
   });
 
+  it("should render correctly when asFloated is left", () => {
+    component.setProps({
+      asFloated: "left",
+    });
+    expect(component.exists()).toBe(true);
+  });
+
+  it("should render correctly when asFloated is right", () => {
+    component.setProps({
+      asFloated: "right",
+    });
+    expect(component.exists()).toBe(true);
+  });
+
+  it("should render correctly when asFloated is none", () => {
+    component.setProps({
+      asFloated: "none",
+    });
+    expect(component.exists()).toBe(true);
+  });
+
+  it("should render correctly when asFloated is inline", () => {
+    component.setProps({
+      asFloated: "inline",
+    });
+    expect(component.exists()).toBe(true);
+  });
+
+  it("should render correctly when passed isHidden props is false", () => {
+    component.setProps({
+      isHidden: false,
+    });
+    expect(component.exists()).toBe(true);
+  });
+
+  it("should render correctly when passed isCircular props is true", () => {
+    component.setProps({
+      isHidden: true,
+    });
+    expect(component.exists()).toBe(true);
+  });
+
+  it("should render correctly when passed isDisabled props is false", () => {
+    component.setProps({
+      isDisabled: false,
+    });
+    expect(component.exists()).toBe(true);
+  });
+
+  it("should render correctly when passed isDisabled props is true", () => {
+    component.setProps({
+      isDisabled: true,
+    });
+    expect(component.exists()).toBe(true);
+  });
+
   it("should render correctly without throwing error", () => {
     component.setProps({
       content: {
