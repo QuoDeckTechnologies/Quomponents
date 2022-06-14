37e356612b235d7e4fc2a98a4792d3e68e12dd72
Rahul Verma <rahul.verma@quodeck.com>
2022-06-14 15:04:44 +0530

    changes in  Banner component

--- src/components/Carousel/Banner/Banner.react.jsx
+++ src/components/Carousel/Banner/Banner.react.jsx
@@ -1,11 +1,13 @@
 import React, { useRef } from "react";
 import PropTypes from "prop-types";
+import { motion } from "framer-motion";
 import _ from "lodash";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import {
     getQuommons,
+    getAnimation,
 } from "../../../common/javascripts/helpers";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../../../common/stylesheets/common.css";
@@ -25,14 +27,74 @@ Banner.propTypes = {
         image: PropTypes.string,
         header: PropTypes.string,
         content: PropTypes.string,
-        props: PropTypes.object
     })).isRequired,
+    //=======================================
+    // Quommon props
+    //=======================================
+    /**
+    Use to define standard component type
+    */
+    asVariant: PropTypes.oneOf([
+        "primary",
+        "secondary",
+        "success",
+        "warning",
+        "error",
+    ]),
+
+    /**
+    Use to override component colors and behavior
+    */
+    withColor: PropTypes.shape({
+        backgroundColor: PropTypes.string,
+        accentColor: PropTypes.string,
+        textColor: PropTypes.string,
+    }),
+    /**
+    Use to define the entry animation of the component
+    */
+    withAnimation: PropTypes.shape({
+        animation: PropTypes.oneOf([
+            "zoom",
+            "collapse",
+            "fade",
+            "slideDown",
+            "slideUp",
+            "slideLeft",
+            "slideRight",
+            ""
+        ]),
+        duration: PropTypes.number,
+        delay: PropTypes.number,
+    }),
+    /**
+    Use to enable/disable the component
+    */
+    isDisabled: PropTypes.bool,
+    /**
+    Use to show/hide the component
+    */
+    isHidden: PropTypes.bool,
+    /**
+    Button component must have the onClick function passed as props
+    */
+    onClick: PropTypes.func.isRequired,
 };
 
 Banner.defaultProps = {
     // Component Specific props
     //=======================================
     content: [],
+    //=======================================
+    // Quommon props
+    //=======================================
+    asVariant: "primary",
+
+    withColor: null,
+    withAnimation: null,
+
+    isDisabled: false,
+    isHidden: false
 };
 /**
 ## Notes
@@ -43,9 +105,30 @@ Banner.defaultProps = {
 - MUI props are not being passed to the button. Please speak to the admin to handle any new MUI prop.
 **/
 export default function Banner(props) {
-    const sliderRef = useRef();
-    let { content } = props;
+    //-------------------------------------------------------------------
+    // 1. Destructuring props
+    //-------------------------------------------------------------------
+    let { content, withColor, asVariant } = props;
+    //-------------------------------------------------------------------
+    // 2. Set the classes
+    //-------------------------------------------------------------------
     let quommonClasses = getQuommons(props, "banner");
+    //-------------------------------------------------------------------
+    // 3. Use to set Color in Banner
+    //-------------------------------------------------------------------
+    let Color = {
+        backgroundColor: props.withColor?.backgroundColor,
+        color: props.withColor?.textColor,
+        borderBottomColor: `${props.withColor?.accentColor}`,
+    };
+    //-------------------------------------------------------------------
+    // 4. Get animation of the component
+    //-------------------------------------------------------------------
+    const animate = getAnimation(props.withAnimation);
+    //-------------------------------------------------------------------
+    // 5. Use to Set the settings for slider in the Component
+    //-------------------------------------------------------------------u
+    const sliderRef = useRef();
 
     var settings = {
         dots: true,
@@ -62,9 +145,12 @@ export default function Banner(props) {
         swipeToSlide: true,
     };
     // ========================= Render Function =================================
-
     return (
-        <div className={`qui ${quommonClasses.parentClasses}`}>
+        <motion.div
+            initial={animate.from}
+            animate={animate.to}
+            className={`qui ${quommonClasses.parentClasses}`}
+        >
             <Slider ref={sliderRef} {...settings}>
                 {_.map(content, (slide, index) => {
                     return (
@@ -81,6 +167,6 @@ export default function Banner(props) {
                     );
                 })}
             </Slider>
-        </div>
+        </motion.div>
     );
 }
