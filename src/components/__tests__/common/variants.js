c9365257d1026abeca3b179c03ca6fe3c2c2ac8c
kp978 <krishna.pant@quodeck.com>
2022-07-07 18:30:51 +0530

    Updated variants.js , added toBeGreaterThanEqual(1)

--- src/components/__tests__/common/variants.js
+++ src/components/__tests__/common/variants.js
@@ -4,38 +4,38 @@ const sharedSpecs = (args) => {
     let component;
     beforeEach(() => {
         jest.resetAllMocks();
-        component = shallow(<args.target {...args.required} />);
+        component = mount(<args.target {...args.required} />);
     });
 
     describe("Valid Variants", () => {
         it("should render correctly when primary variant used", () => {
             component.setProps({ asVariant: "primary" });
-            expect(component.find(".variant-primary").length).toBe(1);
-            expect(component.find(".variant-primary-text").length).toBe(1);
+            expect(component.find(".variant-primary").length).toBeGreaterThanOrEqual(1);
+            expect(component.find(".variant-primary-text").length).toBeGreaterThanOrEqual(1);
         });
 
         it("should render correctly when secondary variant used", () => {
             component.setProps({ asVariant: "secondary" });
-            expect(component.find(".variant-secondary").length).toBe(1);
-            expect(component.find(".variant-secondary-text").length).toBe(1);
+            expect(component.find(".variant-secondary").length).toBeGreaterThanOrEqual(1);
+            expect(component.find(".variant-secondary-text").length).toBeGreaterThanOrEqual(1);
         });
 
         it("should render correctly when warning variant used", () => {
             component.setProps({ asVariant: "warning" });
-            expect(component.find(".variant-warning").length).toBe(1);
-            expect(component.find(".variant-warning-text").length).toBe(1);
+            expect(component.find(".variant-warning").length).toBeGreaterThanOrEqual(1);
+            expect(component.find(".variant-warning-text").length).toBeGreaterThanOrEqual(1);
         });
 
         it("should render correctly when error variant used", () => {
             component.setProps({ asVariant: "error" });
-            expect(component.find(".variant-error").length).toBe(1);
-            expect(component.find(".variant-error-text").length).toBe(1);
+            expect(component.find(".variant-error").length).toBeGreaterThanOrEqual(1);
+            expect(component.find(".variant-error-text").length).toBeGreaterThanOrEqual(1);
         });
 
         it("should render correctly when success variant used", () => {
             component.setProps({ asVariant: "success" });
-            expect(component.find(".variant-success").length).toBe(1);
-            expect(component.find(".variant-success-text").length).toBe(1);
+            expect(component.find(".variant-success").length).toBeGreaterThanOrEqual(1);
+            expect(component.find(".variant-success-text").length).toBeGreaterThanOrEqual(1);
         });
     });
 };
