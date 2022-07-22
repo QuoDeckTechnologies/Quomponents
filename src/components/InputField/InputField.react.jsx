f05fede0b365b600da3e01a713a4846229cff8c5
Rahul Verma <rahul.verma@quodeck.com>
2022-07-22 17:10:56 +0530

    Changes in InputField removed unused code

--- src/components/InputField/InputField.react.jsx
+++ src/components/InputField/InputField.react.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import TextField from '@mui/material/TextField';
 import { motion } from "framer-motion";
