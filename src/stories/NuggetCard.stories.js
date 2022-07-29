89b10728839a56175aea03e110c337c6d0723d52
kp978 <krishna.pant@quodeck.com>
2022-07-29 17:15:55 +0530

    docCode Changes in NuggetCard

--- src/stories/NuggetCard.stories.js
+++ src/stories/NuggetCard.stories.js
@@ -5,17 +5,17 @@ export default {
 	title: "Design System/NuggetCard",
 	component: NuggetCard,
 	argTypes: {
-			published: false,
-			tags: [],
-			category: "",
-			name: "",
-			description: "",
-			image: "",
-			points: "",
-			identifier: "",
+		published: false,
+		tags: [],
+		category: "",
+		name: "",
+		description: "",
+		image: "",
+		points: "",
+		identifier: "",
 		asFloated: {
 			control: "select",
-			options: ["left", "right","none", "inline"],
+			options: ["left", "right", "none", "inline"],
 			table: {
 				category: "as-Flags",
 			},
@@ -44,7 +44,6 @@ export default {
 			<div
 				style={{
 					width: "100%",
-					textAlign: "center",
 					fontSize: "1.25em",
 				}}
 			>
@@ -65,36 +64,59 @@ export default {
 const Template = (args) => <NuggetCard {...args} />;
 export const Default = Template.bind({});
 Default.args = {
-    published: false,
-    tags: ["Sales"],
-    category: "profiler",
-    name: "Measure your sales readiness",
-    description:
-      "Take this quick profile test to check how well you are prepared for a sales job",
-    image: "https://topkit.org/wp-content/uploads/2018/07/Sample-Course.png",
-    points: "200",
-    identifier: "XrPmy_OAK",
-    menuContent: [
-      {
-        list: [
-          { title: "EDIT DETAILS", func: () => {} },
-          { title: "EDIT CONTENT", func: () => {} },
-          { title: "MANAGE LEARNERS", func: () => {} },
-          { title: "VIEW ANALYTICS", func: () => {} },
-          { title: "DELETE COURSE", func: () => {} },
-        ],
-      },
-    ],
-  asFloated: "inline",
-  isDisabled: false,
-  isHidden: false,
+	published: false,
+	tags: ["Sales"],
+	category: "profiler",
+	name: "Measure your sales readiness",
+	description:
+		"Take this quick profile test to check how well you are prepared for a sales job",
+	image: "https://topkit.org/wp-content/uploads/2018/07/Sample-Course.png",
+	points: "200",
+	identifier: "XrPmy_OAK",
+	menuContent: [
+		{
+			list: [
+				{ title: "EDIT DETAILS", func: () => { } },
+				{ title: "EDIT CONTENT", func: () => { } },
+				{ title: "MANAGE LEARNERS", func: () => { } },
+				{ title: "VIEW ANALYTICS", func: () => { } },
+				{ title: "DELETE COURSE", func: () => { } },
+			],
+		},
+	],
+	asFloated: "inline",
+	isDisabled: false,
+	isHidden: false,
 };
 Default.parameters = {
-  docs: {
-    source: {
-      code: `<NuggetCard {...${JSON.stringify(Default.args, null, 2)}}/>`,
-    },
-  },
+	docs: {
+		source: {
+			code: `<NuggetCard 
+	  published= {false}
+	  tags= {["Sales"]}
+	  category= "profiler"
+	  name= "Measure your sales readiness"
+	  description=
+		"Take this quick profile test to check how well you are prepared for a sales job"
+	  image= "https://topkit.org/wp-content/uploads/2018/07/Sample-Course.png"
+	  points= "200"
+	  identifier= "XrPmy_OAK"
+	  menuContent= {[
+		{
+		  list: [
+			{ title: "EDIT DETAILS", func: () => {} },
+			{ title: "EDIT CONTENT", func: () => {} },
+			{ title: "MANAGE LEARNERS", func: () => {} },
+			{ title: "VIEW ANALYTICS", func: () => {} },
+			{ title: "DELETE COURSE", func: () => {} },
+		  ],
+		},
+	  ]}
+	asFloated= "inline"
+	isDisabled= {false}
+	isHidden= {false}/>`,
+		},
+	},
 };
 
 // -------------------------------------------------------------
@@ -103,14 +125,14 @@ Default.parameters = {
 export const NuggetCardWithManyTags = Template.bind({});
 NuggetCardWithManyTags.args = {
 	...Default.args,
-		published: false,
-		tags: ["Article", "Cataloing", "Returns Management", "Orders Management", "Payments and Settlements", "Customer", "Latest", "Pneumonoultramicroscopicsilicovolcanoconiosis", "Tag 1", "Tag 2", "Tag 3", "Tag 4", "Tag 5", "Tag 6", "Tag 7", "Tag 8", "Tag 9", "Tag 10", "Tag 11", "Tag 12", "Tag 13", "Tag 14", "Tag 15", "Tag 16", "Tag 17", "Tag 18", "Tag 19", "Tag 20", "Tag 21", "Tag 22", "Tag 23", "Tag 24"],
-		category: "profiler",
-		name: "Measure your sales readiness",
-		description: "Take this quick profile test to check how well you are prepared for a sales job",
-		image: "https://topkit.org/wp-content/uploads/2018/07/Sample-Course.png",
-		points: "200",
-		identifier: "XrPmy_OAK",
+	published: false,
+	tags: ["Article", "Cataloing", "Returns Management", "Orders Management", "Payments and Settlements", "Customer", "Latest", "Pneumonoultramicroscopicsilicovolcanoconiosis", "Tag 1", "Tag 2", "Tag 3", "Tag 4", "Tag 5", "Tag 6", "Tag 7", "Tag 8", "Tag 9", "Tag 10", "Tag 11", "Tag 12", "Tag 13", "Tag 14", "Tag 15", "Tag 16", "Tag 17", "Tag 18", "Tag 19", "Tag 20", "Tag 21", "Tag 22", "Tag 23", "Tag 24"],
+	category: "profiler",
+	name: "Measure your sales readiness",
+	description: "Take this quick profile test to check how well you are prepared for a sales job",
+	image: "https://topkit.org/wp-content/uploads/2018/07/Sample-Course.png",
+	points: "200",
+	identifier: "XrPmy_OAK",
 	asFloated: "inline",
 	isDisabled: false,
 	isHidden: false,
@@ -121,11 +143,30 @@ NuggetCardWithManyTags.parameters = {
 			story: "Use to override the standard colors of the component.",
 		},
 		source: {
-			code: `<NuggetCardWithManyTags {...${JSON.stringify(
-				NuggetCardWithManyTags.args,
-				null,
-				2
-			)}}/>`,
+			code: `<NuggetCard 
+			published= {false}
+			tags= {["Article", "Cataloing", "Returns Management", "Orders Management", "Payments and Settlements", "Customer", "Latest", "Pneumonoultramicroscopicsilicovolcanoconiosis", "Tag 1", "Tag 2", "Tag 3", "Tag 4", "Tag 5", "Tag 6", "Tag 7", "Tag 8", "Tag 9", "Tag 10", "Tag 11", "Tag 12", "Tag 13", "Tag 14", "Tag 15", "Tag 16", "Tag 17", "Tag 18", "Tag 19", "Tag 20", "Tag 21", "Tag 22", "Tag 23", "Tag 24"]}
+			category= "profiler"
+			name= "Measure your sales readiness"
+			description=
+			  "Take this quick profile test to check how well you are prepared for a sales job"
+			image= "https://topkit.org/wp-content/uploads/2018/07/Sample-Course.png"
+			points= "200"
+			identifier= "XrPmy_OAK"
+			menuContent= {[
+			  {
+				list: [
+				  { title: "EDIT DETAILS", func: () => {} },
+				  { title: "EDIT CONTENT", func: () => {} },
+				  { title: "MANAGE LEARNERS", func: () => {} },
+				  { title: "VIEW ANALYTICS", func: () => {} },
+				  { title: "DELETE COURSE", func: () => {} },
+				],
+			  },
+			]}
+		  asFloated= "inline"
+		  isDisabled= {false}
+		  isHidden= {false}/>`,
 		},
 	},
 };
@@ -135,14 +176,14 @@ NuggetCardWithManyTags.parameters = {
 export const PublishedNuggetCard = Template.bind({});
 PublishedNuggetCard.args = {
 	...Default.args,
-		published: true,
-		tags: ["Sales1", "Sales2"],
-		category: "profiler",
-		name: "Measure your sales readiness",
-		description: "Take this quick profile test to check how well you are prepared for a sales job",
-		image: "https://topkit.org/wp-content/uploads/2018/07/Sample-Course.png",
-		points: "200",
-		identifier: "XrPmy_OAK",
+	published: true,
+	tags: ["Sales1", "Sales2"],
+	category: "profiler",
+	name: "Measure your sales readiness",
+	description: "Take this quick profile test to check how well you are prepared for a sales job",
+	image: "https://topkit.org/wp-content/uploads/2018/07/Sample-Course.png",
+	points: "200",
+	identifier: "XrPmy_OAK",
 	asFloated: "inline",
 	isDisabled: false,
 	isHidden: false,
@@ -153,11 +194,30 @@ PublishedNuggetCard.parameters = {
 			story: "Use to override the standard colors of the component.",
 		},
 		source: {
-			code: `<PublishedNuggetCard {...${JSON.stringify(
-				PublishedNuggetCard.args,
-				null,
-				2
-			)}}/>`,
+			code: `<NuggetCard 
+			published= {true}
+			tags= {["Article", "Cataloing", "Returns Management", "Orders Management", "Payments and Settlements", "Customer", "Latest", "Pneumonoultramicroscopicsilicovolcanoconiosis", "Tag 1", "Tag 2", "Tag 3", "Tag 4", "Tag 5", "Tag 6", "Tag 7", "Tag 8", "Tag 9", "Tag 10", "Tag 11", "Tag 12", "Tag 13", "Tag 14", "Tag 15", "Tag 16", "Tag 17", "Tag 18", "Tag 19", "Tag 20", "Tag 21", "Tag 22", "Tag 23", "Tag 24"]}
+			category= "profiler"
+			name= "Measure your sales readiness"
+			description=
+			  "Take this quick profile test to check how well you are prepared for a sales job"
+			image= "https://topkit.org/wp-content/uploads/2018/07/Sample-Course.png"
+			points= "200"
+			identifier= "XrPmy_OAK"
+			menuContent= {[
+			  {
+				list: [
+				  { title: "EDIT DETAILS", func: () => {} },
+				  { title: "EDIT CONTENT", func: () => {} },
+				  { title: "MANAGE LEARNERS", func: () => {} },
+				  { title: "VIEW ANALYTICS", func: () => {} },
+				  { title: "DELETE COURSE", func: () => {} },
+				],
+			  },
+			]}
+		  asFloated= "inline"
+		  isDisabled= {false}
+		  isHidden= {false}/>`,
 		},
 	},
 };
@@ -167,14 +227,14 @@ PublishedNuggetCard.parameters = {
 export const NuggetCardWithExtraContent = Template.bind({});
 NuggetCardWithExtraContent.args = {
 	...Default.args,
-		published: true,
-		tags: ["Sales1", "Sales2"],
-		category: "profiler",
-		name: "Measure your sales readiness Measure your sales readiness",
-		description: "Take this quick profile test to check how well you are prepared for a sales job. Take this quick profile test to check how well you are prepared for a sales job",
-		image: "https://topkit.org/wp-content/uploads/2018/07/Sample-Course.png",
-		points: "200",
-		identifier: "XrPmy_OAK",
+	published: true,
+	tags: ["Sales1", "Sales2"],
+	category: "profiler",
+	name: "Measure your sales readiness Measure your sales readiness",
+	description: "Take this quick profile test to check how well you are prepared for a sales job. Take this quick profile test to check how well you are prepared for a sales job",
+	image: "https://topkit.org/wp-content/uploads/2018/07/Sample-Course.png",
+	points: "200",
+	identifier: "XrPmy_OAK",
 	asFloated: "inline",
 	isDisabled: false,
 	isHidden: false,
@@ -185,11 +245,30 @@ NuggetCardWithExtraContent.parameters = {
 			story: "Use to override the standard colors of the component.",
 		},
 		source: {
-			code: `<NuggetCardWithExtraContent {...${JSON.stringify(
-				NuggetCardWithExtraContent.args,
-				null,
-				2
-			)}}/>`,
+			code: `<NuggetCard 
+			published= {false}
+			tags= {["Article", "Cataloing", "Returns Management", "Orders Management", "Payments and Settlements", "Customer", "Latest", "Pneumonoultramicroscopicsilicovolcanoconiosis", "Tag 1", "Tag 2", "Tag 3", "Tag 4", "Tag 5", "Tag 6", "Tag 7", "Tag 8", "Tag 9", "Tag 10", "Tag 11", "Tag 12", "Tag 13", "Tag 14", "Tag 15", "Tag 16", "Tag 17", "Tag 18", "Tag 19", "Tag 20", "Tag 21", "Tag 22", "Tag 23", "Tag 24"]}
+			category= "profiler"
+			name= "Measure your sales readiness Measure your sales readiness"
+			description=
+			  "Take this quick profile test to check how well you are prepared for a sales job. Take this quick profile test to check how well you are prepared for a sales job"
+			image= "https://topkit.org/wp-content/uploads/2018/07/Sample-Course.png"
+			points= "200"
+			identifier= "XrPmy_OAK"
+			menuContent= {[
+			  {
+				list: [
+				  { title: "EDIT DETAILS", func: () => {} },
+				  { title: "EDIT CONTENT", func: () => {} },
+				  { title: "MANAGE LEARNERS", func: () => {} },
+				  { title: "VIEW ANALYTICS", func: () => {} },
+				  { title: "DELETE COURSE", func: () => {} },
+				],
+			  },
+			]}
+		  asFloated= "inline"
+		  isDisabled= {false}
+		  isHidden= {false}/>`,
 		},
 	},
 };
