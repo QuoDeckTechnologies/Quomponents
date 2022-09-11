4916c9e8211982a59354a5d23791d12342b4fbaf
Arijit Lahiri <arijit@ptotem.com>
2022-09-12 02:17:15 +0530

    Updated Field Button

--- src/components/OptionItem/OptionItemOne/OptionItemOne.react.jsx
+++ src/components/OptionItem/OptionItemOne/OptionItemOne.react.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import { motion } from "framer-motion";
 import {
-  getAnimation,
-  getQuommons,
-  getTranslation,
+    getAnimation,
+    getQuommons,
+    getTranslation,
 } from "../../../common/javascripts/helpers";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../../../common/stylesheets/common.css";
@@ -14,84 +14,84 @@ import "../../../common/stylesheets/overrule.scss";
 import InputField from "../../InputField/InputField.react";
 
 OptionItemOne.propTypes = {
-  //=======================================
-  // Component Specific props
-  //=======================================
-  /**
+    //=======================================
+    // Component Specific props
+    //=======================================
+    /**
   OptionItemOne targetName, value, placeholder should be passed in content object
   */
-  content: PropTypes.shape({
-    targetName: PropTypes.string,
-    value: PropTypes.string,
+    name: PropTypes.string,
+    value: PropTypes.string.isRequired,
     placeholder: PropTypes.string,
-    maxLength: PropTypes.number,
-  }),
-  //=======================================
-  // Quommon props
-  //=======================================
-  /**
+    maxLength: PropTypes.number.isRequired,
+    removable: PropTypes.bool,
+    //=======================================
+    // Quommon props
+    //=======================================
+    /**
   Use to override component colors
   */
-  withColor: PropTypes.shape({
-    backgroundColor: PropTypes.string,
-    accentColor: PropTypes.string,
-  }),
-  /**
+    withColor: PropTypes.shape({
+        backgroundColor: PropTypes.string,
+        accentColor: PropTypes.string,
+    }),
+    /**
   Use to define the entry animation of the component
   */
-  withAnimation: PropTypes.shape({
-    animation: PropTypes.oneOf([
-      "zoom",
-      "collapse",
-      "fade",
-      "slideDown",
-      "slideUp",
-      "slideLeft",
-      "slideRight",
-      "",
-    ]),
-    duration: PropTypes.number,
-    delay: PropTypes.number,
-  }),
-  /**
+    withAnimation: PropTypes.shape({
+        animation: PropTypes.oneOf([
+            "zoom",
+            "collapse",
+            "fade",
+            "slideDown",
+            "slideUp",
+            "slideLeft",
+            "slideRight",
+            "",
+        ]),
+        duration: PropTypes.number,
+        delay: PropTypes.number,
+    }),
+    /**
   Use to show a translated version of the component text. Dictionary must be valid JSON. 
   */
-  withTranslation: PropTypes.shape({
-    lang: PropTypes.string,
-    tgt: PropTypes.string,
-    dictionary: PropTypes.string,
-  }),
-  /**
+    withTranslation: PropTypes.shape({
+        lang: PropTypes.string,
+        tgt: PropTypes.string,
+        dictionary: PropTypes.string,
+    }),
+    /**
   Use to enable/disable the component
   */
-  isDisabled: PropTypes.bool,
-  /**
+    isDisabled: PropTypes.bool,
+    /**
   Use to show/hide the component
   */
-  isHidden: PropTypes.bool,
-  /**
-  OptionItemOne component must have the onInput function passed as props
+    isHidden: PropTypes.bool,
+    /**
+  OptionItemOne component must have the onsubmit function passed as props
   */
-  onInput: PropTypes.func.isRequired,
-  /**
-  OptionItemOne component must have the onClick function passed as props
+    onSubmit: PropTypes.func.isRequired,
+    /**
+  OptionItemOne component can have the onRemove function passed as props
   */
-  onClick: PropTypes.func.isRequired,
+    onRemove: PropTypes.func,
 };
 
 OptionItemOne.defaultProps = {
-  //=======================================
-  // Component Specific props
-  //=======================================
-  content: {},
-  //=======================================
-  // Quommon props
-  //=======================================
-  withColor: null,
-  withAnimation: null,
-  withTranslation: null,
-  isDisabled: false,
-  isHidden: false,
+    //=======================================
+    // Component Specific props
+    //=======================================
+    placeholder: "Please enter the value",
+    removable: false,
+    //=======================================
+    // Quommon props
+    //=======================================
+    withColor: null,
+    withAnimation: null,
+    withTranslation: null,
+    isDisabled: false,
+    isHidden: false,
 };
 /**
 ## Notes
@@ -101,49 +101,47 @@ OptionItemOne.defaultProps = {
 - Or add custom css in overrule.scss to override the component css
 **/
 export default function OptionItemOne(props) {
-  //-------------------------------------------------------------------
-  // 1. Destructuring content prop
-  //-------------------------------------------------------------------
-  const { content } = props;
-  //-------------------------------------------------------------------
-  // 2. Set the classes
-  //-------------------------------------------------------------------
-  let quommonClasses = getQuommons(props, "option-item-one");
-  //-------------------------------------------------------------------
-  // 3. Translate the text objects in case their is a dictionary provided
-  //-------------------------------------------------------------------
-  let tObj = getTranslation(props.withTranslation);
-  //-------------------------------------------------------------------
-  // 4. Get animation of the component
-  //-------------------------------------------------------------------
-  const animate = getAnimation(props);
+    //-------------------------------------------------------------------
+    // 2. Set the classes
+    //-------------------------------------------------------------------
+    let quommonClasses = getQuommons(props, "option-item-one");
+    //-------------------------------------------------------------------
+    // 3. Translate the text objects in case their is a dictionary provided
+    //-------------------------------------------------------------------
+    let tObj = getTranslation(props.withTranslation);
+    //-------------------------------------------------------------------
+    // 4. Get animation of the component
+    //-------------------------------------------------------------------
+    const animate = getAnimation(props);
 
-  // ========================= Render Function =================================
+    // ========================= Render Function =================================
 
-  return (
-    <motion.div
-      initial={animate.from}
-      animate={animate.to}
-      className={`qui ${quommonClasses.parentClasses}`}
-    >
-      <div className="qui-inline-option-container">
-        <InputField
-          name={content?.targetName || "default-target-name"}
-          value={content?.value}
-          placeholder={tObj?.placeholder || content?.placeholder}
-          maxLength={content?.maxLength}
-          asEmphasis="listInput"
-          withColor={props.withColor}
-          onSubmit={(name, value) => props.onInput(name, value)}
-        />
-        <div className="qui-inline-edit-with-remove-button-close-icon">
-          <i
-            className="qui-inline-edit-with-remove-button-icon fas fa-times"
-            data-id={content?.targetName}
-            onClick={(e) => props.onClick(e.target.dataset.id)}
-          ></i>
-        </div>
-      </div>
-    </motion.div>
-  );
+    return (
+        <motion.div
+            initial={animate.from}
+            animate={animate.to}
+            className={`qui ${quommonClasses.parentClasses}`}
+        >
+            <div className="qui-inline-option-container">
+                <InputField
+                    name={props.name}
+                    value={props.value}
+                    placeholder={props.placeholder}
+                    maxLength={props.maxLength}
+                    asEmphasis="listInput"
+                    withColor={props.withColor}
+                    onSubmit={(value) => props.onSubmit(value)}
+                />
+                {props.removable && (
+                    <div className="qui-inline-edit-with-remove-button-close-icon">
+                        <i
+                            className="qui-inline-edit-with-remove-button-icon fas fa-times"
+                            data-id={props.name}
+                            onClick={(e) => props.onRemove(props.name)}
+                        ></i>
+                    </div>
+                )}
+            </div>
+        </motion.div>
+    );
 }
--- src/stories/InputField.stories.js
+++ src/stories/InputField.stories.js
@@ -2,114 +2,113 @@ import React from "react";
 import InputField from "../components/InputField/InputField.react";
 
 const dictionary = JSON.stringify({
-  hi: {
-    inputField: {
-      label: "इनपुट नाम",
-      placeholder: "विकल्प",
-    }
-  },
-  en: {
-    inputField: {
-      label: "Input Name",
-      placeholder: "Options",
-    }
-  }
+    hi: {
+        inputField: {
+            label: "इनपुट नाम",
+            placeholder: "विकल्प",
+        },
+    },
+    en: {
+        inputField: {
+            label: "Input Name",
+            placeholder: "Options",
+        },
+    },
 });
 
 export default {
-  title: "Design System/InputField",
-  component: InputField,
-  argTypes: {
-    name: "",
-    label: "",
-    value: "",
-    placeholder: "",
-    maxLength: 0,
-    type: "text",
-    multiline: false,
-    asEmphasis: {
-      control: "select",
-      options: ["filled", "charLimited", "listInput", "shortField"],
-      table: {
-        category: "as-Flags",
-      },
-    },
-    asFloated: {
-      control: "select",
-      options: ["left", "right", "none", "inline"],
-      table: {
-        category: "as-Flags",
-      },
-    },
-    withColor: {
-      table: {
-        category: "with-Params",
-        defaultValue: {
-          textColor: "",
-          accentColor: "",
-          backgroundColor: "",
-          onSelectTextColor: "",
-          onSelectAccentColor: "",
-          onSelectBackgroundColor: "",
+    title: "Design System/InputField",
+    component: InputField,
+    argTypes: {
+        name: "",
+        label: "",
+        value: "",
+        placeholder: "",
+        maxLength: 0,
+        type: "text",
+        multiline: false,
+        asEmphasis: {
+            control: "select",
+            options: ["filled", "charLimited", "listInput", "shortField"],
+            table: {
+                category: "as-Flags",
+            },
         },
-      },
-    },
-    withAnimation: {
-      table: {
-        category: "with-Params",
-        defaultValue: {
-          animation: "",
-          duration: 0,
-          delay: 0,
+        asFloated: {
+            control: "select",
+            options: ["left", "right", "none", "inline"],
+            table: {
+                category: "as-Flags",
+            },
         },
-      },
-    },
-    withTranslation: {
-      table: {
-        category: "with-Params",
-        defaultValue: {
-          lang: "",
-          tgt: "",
-          dictionary: "",
+        withColor: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    textColor: "",
+                    accentColor: "",
+                    backgroundColor: "",
+                    onSelectTextColor: "",
+                    onSelectAccentColor: "",
+                    onSelectBackgroundColor: "",
+                },
+            },
         },
-      },
+        withAnimation: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    animation: "",
+                    duration: 0,
+                    delay: 0,
+                },
+            },
+        },
+        withTranslation: {
+            table: {
+                category: "with-Params",
+                defaultValue: {
+                    lang: "",
+                    tgt: "",
+                    dictionary: "",
+                },
+            },
+        },
+        isHidden: {
+            table: {
+                category: "is-Toggles",
+                defaultValue: false,
+            },
+        },
+        isDisabled: {
+            table: {
+                category: "is-Toggles",
+                defaultValue: false,
+            },
+        },
+        onSubmit: {
+            table: {
+                category: "Events",
+                defaultValue: null,
+            },
+        },
     },
-    isHidden: {
-      table: {
-        category: "is-Toggles",
-        defaultValue: false,
-      },
+    decorators: [
+        (story) => (
+            <div
+                style={{
+                    textAlign: "center",
+                }}
+            >
+                {story()}
+            </div>
+        ),
+    ],
+    parameters: {
+        componentSubtitle: "Default InputField for general purpose use",
+        a11y: { disable: true },
+        docs: { iframeHeight: 100 },
     },
-    isDisabled: {
-      table: {
-        category: "is-Toggles",
-        defaultValue: false,
-      },
-    },
-    onSubmit: {
-      table: {
-        category: "Events",
-        defaultValue: null,
-      },
-    },
-  },
-  decorators: [
-    (story) => (
-      <div
-        style={{
-          textAlign: "center",
-        }}
-      >
-        {story()}
-      </div>
-    ),
-  ],
-  parameters: {
-    componentSubtitle:
-      "Default InputField for general purpose use",
-    a11y: { disable: true },
-    docs: { iframeHeight: 100 },
-  },
 };
 // -------------------------------------------------------------
 // Default
@@ -117,203 +116,197 @@ export default {
 const Template = (args) => <InputField {...args} />;
 export const Default = Template.bind({});
 Default.args = {
-  label: "Input Name",
-  value: "Please input your text here",
-  placeholder: "Options",
-  maxLength: 300,
-  type: "text",
-  multiline: false,
-  name: "testing_id",
-  asEmphasis: "filled",
-  asFloated: "none",
-  withColor: {
-    textColor: "#666666",
-    accentColor: "#ffab00",
-    backgroundColor: "#ffab000d",
-    onSelectTextColor: "",
-    onSelectAccentColor: "",
-    onSelectBackgroundColor: "",
-  },
-  withAnimation: {
-    animation: "collapse",
-    duration: 0.5,
-    delay: 0,
-  },
-  withTranslation: {
-    lang: "en",
-    tgt: "inputField",
-    dictionary: dictionary,
-  },
-  isHidden: false,
-  isDisabled: false,
+    label: "Input Name",
+    value: "Please input your text here",
+    placeholder: "Options",
+    maxLength: 300,
+    type: "text",
+    multiline: false,
+    name: "testing_id",
+    asEmphasis: "filled",
+    asFloated: "none",
+    withColor: {
+        textColor: "#666666",
+        accentColor: "#ffab00",
+        backgroundColor: "#ffab000d",
+        onSelectTextColor: "",
+        onSelectAccentColor: "",
+        onSelectBackgroundColor: "",
+    },
+    withAnimation: {
+        animation: "collapse",
+        duration: 0.5,
+        delay: 0,
+    },
+    withTranslation: {
+        lang: "en",
+        tgt: "inputField",
+        dictionary: dictionary,
+    },
+    isHidden: false,
+    isDisabled: false,
 };
 Default.parameters = {
-  docs: {
-    source: {
-      code: `<InputField {...${JSON.stringify(Default.args, null, 2)}}/>`,
+    docs: {
+        source: {
+            code: `<InputField {...${JSON.stringify(Default.args, null, 2)}}/>`,
+        },
     },
-  },
 };
 // -------------------------------------------------------------
 // charLimited InputField
 // -------------------------------------------------------------
 export const charLimitedField = Template.bind({});
 charLimitedField.args = {
-  ...Default.args,
-  asEmphasis: "charLimited",
+    ...Default.args,
+    asEmphasis: "charLimited",
 };
 charLimitedField.parameters = {
-  docs: {
-    description: {
-      story:
-        "Use to show the charLimited editing state for the InputField.",
+    docs: {
+        description: {
+            story: "Use to show the charLimited editing state for the InputField.",
+        },
+        source: {
+            code: `<charLimitedField {...${JSON.stringify(
+                charLimitedField.args,
+                null,
+                2
+            )}}/>`,
+        },
     },
-    source: {
-      code: `<charLimitedField {...${JSON.stringify(
-        charLimitedField.args,
-        null,
-        2
-      )}}/>`,
-    },
-  },
 };
 // -------------------------------------------------------------
 // listInput InputField
 // -------------------------------------------------------------
 export const listInputField = Template.bind({});
 listInputField.args = {
-  ...Default.args,
-  value: "",
-  placeholder: "Options",
-  maxLength: 300,
-  type: "text",
-  multiline: false,
-  asEmphasis: "listInput",
+    ...Default.args,
+    value: "",
+    placeholder: "Options",
+    maxLength: 300,
+    type: "text",
+    multiline: false,
+    asEmphasis: "listInput",
 };
 listInputField.parameters = {
-  docs: {
-    description: {
-      story:
-        "Use to show the listInput editing state for the InputField.",
+    docs: {
+        description: {
+            story: "Use to show the listInput editing state for the InputField.",
+        },
+        source: {
+            code: `<listInputField {...${JSON.stringify(
+                listInputField.args,
+                null,
+                2
+            )}}/>`,
+        },
     },
-    source: {
-      code: `<listInputField {...${JSON.stringify(
-        listInputField.args,
-        null,
-        2
-      )}}/>`,
-    },
-  },
 };
 // -------------------------------------------------------------
 // shortField InputField
 // -------------------------------------------------------------
 export const ShortFieldField = Template.bind({});
 ShortFieldField.args = {
-  ...Default.args,
-  value: "0",
-  asEmphasis: "shortField",
+    ...Default.args,
+    value: "0",
+    asEmphasis: "shortField",
 };
 ShortFieldField.parameters = {
-  docs: {
-    description: {
-      story:
-        "Use to show the shortField editing state for the InputField.",
+    docs: {
+        description: {
+            story: "Use to show the shortField editing state for the InputField.",
+        },
+        source: {
+            code: `<ShortFieldField {...${JSON.stringify(
+                ShortFieldField.args,
+                null,
+                2
+            )}}/>`,
+        },
     },
-    source: {
-      code: `<ShortFieldField {...${JSON.stringify(
-        ShortFieldField.args,
-        null,
-        2
-      )}}/>`,
-    },
-  },
 };
 // -------------------------------------------------------------
 // Default Translated InputField
 // -------------------------------------------------------------
 export const TranslatedInputField = Template.bind({});
 TranslatedInputField.args = {
-  ...Default.args,
-  withTranslation: {
-    lang: "hi",
-    tgt: "inputField",
-    dictionary: dictionary,
-  },
+    ...Default.args,
+    withTranslation: {
+        lang: "hi",
+        tgt: "inputField",
+        dictionary: dictionary,
+    },
 };
 TranslatedInputField.parameters = {
-  docs: {
-    description: {
-      story:
-        "Use to change the language that the text appears in InputField.",
+    docs: {
+        description: {
+            story: "Use to change the language that the text appears in InputField.",
+        },
+        source: {
+            code: `<InputField {...${JSON.stringify(
+                TranslatedInputField.args,
+                null,
+                2
+            )}}/>`,
+        },
     },
-    source: {
-      code: `<InputField {...${JSON.stringify(
-        TranslatedInputField.args,
-        null,
-        2
-      )}}/>`,
-    },
-  },
 };
 // -------------------------------------------------------------
 // charLimited Translated InputField
 // -------------------------------------------------------------
 export const TranslatedCharLimited = Template.bind({});
 TranslatedCharLimited.args = {
-  ...Default.args,
-  asEmphasis: "charLimited",
-  withTranslation: {
-    lang: "hi",
-    tgt: "inputField",
-    dictionary: dictionary,
-  },
+    ...Default.args,
+    asEmphasis: "charLimited",
+    withTranslation: {
+        lang: "hi",
+        tgt: "inputField",
+        dictionary: dictionary,
+    },
 };
 TranslatedCharLimited.parameters = {
-  docs: {
-    description: {
-      story:
-        "Use to change the language that the text appears in charLimited InputField.",
+    docs: {
+        description: {
+            story: "Use to change the language that the text appears in charLimited InputField.",
+        },
+        source: {
+            code: `<InputField {...${JSON.stringify(
+                TranslatedCharLimited.args,
+                null,
+                2
+            )}}/>`,
+        },
     },
-    source: {
-      code: `<InputField {...${JSON.stringify(
-        TranslatedCharLimited.args,
-        null,
-        2
-      )}}/>`,
-    },
-  },
 };
 // -------------------------------------------------------------
 // listInput Translated InputField
 // -------------------------------------------------------------
 export const TranslatedListInput = Template.bind({});
 TranslatedListInput.args = {
-  ...Default.args,
-  content: {
-    value: "",
-    placeholder: "Options",
-    maxLength: 300,
-  },
-  asEmphasis: "listInput",
-  withTranslation: {
-    lang: "hi",
-    tgt: "inputField",
-    dictionary: dictionary,
-  },
+    ...Default.args,
+    content: {
+        value: "",
+        placeholder: "Options",
+        maxLength: 300,
+    },
+    asEmphasis: "listInput",
+    withTranslation: {
+        lang: "hi",
+        tgt: "inputField",
+        dictionary: dictionary,
+    },
 };
 TranslatedListInput.parameters = {
-  docs: {
-    description: {
-      story:
-        "Use to change the language that the text appears in listInput InputField.",
+    docs: {
+        description: {
+            story: "Use to change the language that the text appears in listInput InputField.",
+        },
+        source: {
+            code: `<InputField {...${JSON.stringify(
+                TranslatedListInput.args,
+                null,
+                2
+            )}}/>`,
+        },
     },
-    source: {
-      code: `<InputField {...${JSON.stringify(
-        TranslatedListInput.args,
-        null,
-        2
-      )}}/>`,
-    },
-  },
 };
